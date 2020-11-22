7a2e7b9bd6646e57190dcc2f13f4281b6800e2fd
skyepodium <skyepodium@gmail.com>
2020-11-22 12:48:18 +0900

    bfs

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,39 @@
-coin_types = [500, 100, 50, 10, 5, 1]
+from collections import deque
 
-money = int(input())
-remain = 1000 - money
 
+def bfs(x, y):
+    q = deque()
+    check[x][y] = True
+    q.append((x, y))
+
+    while q:
+        cx, cy = q.popleft()
+
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if not check[nx][ny] and a[nx][ny] == 0:
+                    check[nx][ny] = True
+                    q.append((nx, ny))
+
+
+n, m = map(int, input().split())
+a = []
+for i in range(n):
+    a.append(list(map(int, input())))
+
+check = [[False for _ in range(m)] for _ in range(n)]
+dx = [0, 0, 1, -1]
+dy = [-1, 1, 0, 0]
 result = 0
 
-for coin in coin_types:
-    result += remain // coin
-    remain %= coin
+
+for i in range(n):
+    for j in range(m):
+        if not check[i][j] and a[i][j] == 0:
+            result += 1
+            bfs(i, j)
 
 print(result)
