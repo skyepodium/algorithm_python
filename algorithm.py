0b33b34e54982bc90ca6accba28e91720b4e4673
skyepodium <skyepodium@gmail.com>
2020-11-29 15:37:52 +0900

    baekjoon 5046 by for loop

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,32 @@
-n = int(input())
-result = ""
+n, b, h, w = map(int, input().split())
 
-while n > 0:
-    num = n % 2
-    result += str(num)
-    n = n // 2
+h_list = []
+w_list = []
+max_val = 10001 * 200
+result = max_val
 
-print(result[::-1])
+for i in range(h):
+    pay = int(input())
+    h_list.append(pay)
+
+    w_arr = list(map(int, input().split()))
+    w_list.append(w_arr)
+
+for i in range(h):
+    if h_list[i] * n > b:
+        continue
+
+    check = False
+    for room_cnt in w_list[i]:
+        if room_cnt >= n:
+            check = True
+
+    if not check:
+        continue
+
+    result = min(result, h_list[i] * n)
+
+if result != max_val:
+    print(result)
+else:
+    print("stay home")
