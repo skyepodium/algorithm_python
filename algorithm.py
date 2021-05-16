7e00222e94ac92c0a17295afa0a333369dd44b78
skyepodium <skyepodium@gmail.com>
2021-05-16 21:18:36 +0900

    linear regression implementation

--- algorithm.py
+++ algorithm.py
@@ -1,33 +1,56 @@
-from tensorflow import keras
-import matplotlib.pyplot as plt
+import numpy as np
 
-# 데이터를 불러오는 코드를 작성해주세요.
-mnist = keras.datasets.mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# 데이터의 크기를 shape 함수를 통해 출력해주세요.
-print(f"train_images: {train_images.shape}")
-print(f"train_labels.shape: {train_labels.shape}")
+class LinearRegression:
+    def __init__(self, learning_rate=0.01, epochs=5000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.w = 0.0
+        self.b = 0.0
 
-print(f"test_images.shape: {test_images.shape}")
-print(f"test_labels.shape: {test_labels.shape}")
+    def fit(self, x_train, y_train):
+        w = self.w
+        b = self.b
+        learning_rate = self.learning_rate
+        epochs = self.epochs
 
-"""
-train_images: (60000, 28, 28)
-train_labels.shape: (60000,)
-test_images.shape: (10000, 28, 28)
-test_labels.shape: (10000,)
-"""
-#(BHW) - 1. batch, 2. height, 3. width
-"""
-이미지 데이터는 3가지의 속성을가집니다.
-순서대로 1. batch, 2. height, 3. width
-"""
+        n_data = len(x_train)
 
-# 28x28의 흑백 이미지를 plt를 이용하여 출력하는 코드를 작성해보세요.
-plt.figure()
-plt.imshow(train_images[1])
-plt.colorbar()
-plt.grid(True)
-plt.show()
-print(train_labels[1])
+        for i in range(epochs):
+            hypothesis = x_train * w + b
+            cost = np.sum((hypothesis - y_train) ** 2) / n_data
+            gradient_w = np.sum((w * x_train - y_train + b) * 2 * x_train) / n_data
+            gradient_b = np.sum((w * x_train - y_train + b) * 2) / n_data
+
+            w -= learning_rate * gradient_w
+            b -= learning_rate * gradient_b
+
+            if i % 100 == 0:
+                print('Epoch ({:10d}/{:10d}) cost: {:10f}, W: {:10f}, b:{:10f}'.format(i, epochs, cost, w, b))
+
+        self.w = w
+        self.b = b
+
+    def predict(self, test_x):
+        w = self.w
+        b = self.b
+
+        y_pred = test_x * w + b
+        return y_pred
+
+
+lr = LinearRegression()
+
+train_x = np.array([1., 2., 3., 4., 5., 6.])
+train_y = np.array([9., 16., 23., 30., 37., 44.])
+test_x = np.array([2, 3, 4, 5, 6, 7])
+print(lr)
+lr.fit(train_x, train_y)
+
+result = lr.predict(test_x)
+print('result', result)
+from sklearn.linear_model import LinearRegression as real
+lo = real()
+lo.fit(train_x.reshape(-1, 1), train_y)
+real_pred = lo.predict(test_x.reshape(-1, 1))
+print("real_pred", real_pred)
