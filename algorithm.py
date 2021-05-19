e2ff63b5d50be8b0c70979a478f08061d7fa53bf
skyepodium <skyepodium@gmail.com>
2021-05-19 20:14:20 +0900

    keras save model

--- algorithm.py
+++ algorithm.py
@@ -1,59 +1,23 @@
-# and gate
-def and_gate(x1, x2):
-    # 입력값 x1에 대한 가중치 w1, x2에 대한 가중치 w2
-    w1, w2 = 0.6, 0.6
-
-    # bias 편향값
-    b = -0.7
-
-    # 연산 결과
-    result = x1*w1 + x2*w2 + b
-
-    # 결과가 0보다 작거나 같으면 0, 0보다 크면 1을 반환합니다.
-    return 0 if result <= 0 else 1
+from tensorflow import keras
+from keras.layers import SimpleRNN, Dense, Input
+from keras.models import Model
 
 
-# or gate
-def or_gate(x1, x2):
-    # 입력값 x1에 대한 가중치 w1, x2에 대한 가중치 w2
-    w1, w2 = 0.6, 0.6
+# 모델 생성
+Inputs = Input(shape=(28, 28))
+x1 = SimpleRNN(64, activation="tanh")(Inputs)
+x2 = Dense(10, activation="softmax")(x1)
+model = Model(Inputs, x2)
 
-    # bias 편향값
-    b = -0.5
+# 모델 저장 경로
+SAVE_PATH = "./params.h5"
 
-    # 연산 결과
-    result = x1*w1 + x2*w2 + b
+# 모델 저장
+# model.save(SAVE_PATH)
+model.save_weights(SAVE_PATH)
 
-    # 결과가 0보다 작거나 같으면 0, 0보다 크면 1을 반환합니다.
-    return 0 if result <= 0 else 1
-
-
-def nand_gate(x1, x2):
-    # 입력값 x1에 대한 가중치 w1, x2에 대한 가중치 w2
-    w1, w2 = 0.6, 0.6
-
-    # bias 편향값
-    b = -0.7
-    # 연산 결과
-    result = x1*w1 + x2*w2 + b
-
-    # 결과가 0보다 작거나 같으면 1, 0보다 크면 0을 반환합니다.
-    return 1 if result <= 0 else 0
-
-
-# 입력값 목록을 튜플 리스트로 만듭니다.
-input_list = [(0, 0), (0, 1), (1, 0), (1, 1)]
-
-
-# 반복문을 순회하면서 입력값을 가져옵니다.
-for x1, x2 in input_list:
-    # or gate의 결과를 저장하고s
-    or_result = or_gate(x1, x2)
-
-    # nand gate의 결과를 저장하고
-    nand_result = nand_gate(x1, x2)
-
-    xor_result = and_gate(or_result, nand_result)
-
-    # 출력합니다.
-    print(f"입력값: {x1} {x2} / 결과: {xor_result}")
+# 모델 불러오기
+# new_model = keras.models.load_model(SAVE_PATH)
+#
+# # 모델 요약 정보 확인
+# new_model.summary()
