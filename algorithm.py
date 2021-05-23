faaf5e953d5a8d5744ea453ad53f87784a2277c8
skyepodium <skyepodium@gmail.com>
2021-05-23 18:34:49 +0900

    logloss 구현 예제

--- algorithm.py
+++ algorithm.py
@@ -1,84 +1,41 @@
-import pandas as pd
-from sklearn.metrics import log_loss
-from sklearn.ensemble import RandomForestClassifier
-
-a = [1, 2, 3, 4, 1, 2]
-b = [1, 2, 3, 4, 5, 5]
-
-train_data = {
-    'data': a,
-    'target': b
-}
-
-train = pd.DataFrame(
-            data=train_data
-        )
-
-a = [1, 2, 3, 4, 1, 2]
-b = [1, 2, 3, 4, 5, 5]
-
-test_data = {
-    'data': a,
-    'target': b
-}
-
-test = pd.DataFrame(
-            data=test_data
-        )
-
-
-train_x = train.drop(columns=["target"])
-train_y = train["target"]
-test_x = train.drop(columns=["target"])
-test_y = train["target"]
-
-
-rf = RandomForestClassifier()
-rf.fit(train_x.values.reshape(-1, 1), train_y)
-preds_proba = rf.predict_proba(test_x.values.reshape(-1, 1))
-
-print(test_y)
-print(test_y.shape)
-print(preds_proba)
-print(preds_proba.shape)
-
-log_loss_result = log_loss(test_y, preds_proba)
-
-print(f"log_loss_result: {log_loss_result}")
-
+# numpy를 불러옵니다.
 import numpy as np
-print(-1 * np.log(1))
 
-result1 = -1 * np.log(0.99)
-result2 = -1 * np.log(0.5)
-result3 = -1 * np.log(0.1)
+# 직접 logloss를 계산할 함수를 생성합니다.
+# answer_array는 정답 array
+# proba_array는 확률 array
+def my_logloss(answer_array, proba_array):
 
-total_result = result1 + result2 + result3
-print("total logloss", total_result / 3)
+    # array의 크기를 가져옵니다.
+    size = answer_array.shape[0]
 
-a = np.array([1, 3, 2])
-print(a)
-b = np.array([[0.99, 0, 0.01], [0, 0.5, 0.5], [0.9, 0.1, 0]])
-print(b)
+    # 반복문을 사용해서 logloss의 합을 계산합니다.
+    logloss_sum = 0
+    # zip함수로 묶으면 함께 순회할 수 있습니다.
+    for answer, arr in zip(answer_array, proba_array):
+        proba = arr[answer - 1]
+        # 음의 로그함수에 넣어서 logloss 계산
+        logloss_sum += -np.log(proba)
 
-log_loss_result = log_loss(a, b)
-print(f"log_loss_result: {log_loss_result}")
+    # logloss의 평균 계산
+    result = logloss_sum / size
 
-a = np.array([1, 2, 3])
-print(a)
-b = np.array([[0, 0, 1], [1, 0, 0], [1, 0, 0]])
-print(b)
+    # 반환
+    return result
 
-log_loss_result = log_loss(a, b)
-print(f"log_loss_result: {log_loss_result}")
+# answer_array는 정답 array
+# proba_array는 확률 array
+answer_list = np.array([1, 3, 2])
+proba_list = np.array([[0.99, 0, 0.01], [0, 0.5, 0.5], [0.9, 0.1, 0]])
 
-a = 1e-15
-print(-np.log(a))
+# my_logloss 결과 출력
+my_logloss_result = my_logloss(answer_list, proba_list)
+print('my_logloss', my_logloss_result)
 
-a = np.array([1, 3, 2])
-print(a)
-b = np.array([[0.99, 0, 0.01], [0, 0.5, 0.5], [0.9, 0.1, 0]])
-print(b)
 
-res = (-np.log(0.99) + -np.log(0.5) + -np.log(0.1)) / 3
-print(res)
+# 비교를 위해 사이킷런의 log_loss 함수를 불러옵니다.
+from sklearn.metrics import log_loss
+
+# 사이킷런 결과 확인
+sklearn_reslt = log_loss(answer_list, proba_list)
+print("sklearn_reslt", sklearn_reslt)
