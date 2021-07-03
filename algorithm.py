d539f8ddb904ee9dabaa057e0506d4e2fe054594
skyepodium <skyepodium@gmail.com>
2021-07-03 16:11:04 +0900

    devmatching tooth brush

--- algorithm.py
+++ algorithm.py
@@ -1,50 +1,50 @@
-def solution(rows, columns, queries):
-    answer = []
+def solution(enroll, referral, seller, amount):
 
-    a = [[0 for _ in range(columns)] for _ in range(rows)]
+    size = len(enroll)
+    answer = [0 for _ in range(size)]
+    v = [0 for _ in range(size + 1)]
 
-    dx = 1, 0, -1, 0
-    dy = 0, 1, 0, -1
+    # 1. name_dict
+    name_dict = {'-': 0}
+    idx_dict = {0: '-'}
+    idx = 1
+    for en_name in enroll:
+        name_dict[en_name] = idx
+        idx_dict[idx] = en_name
+        idx += 1
 
-    num = 1
-    for i in range(rows):
-        for j in range(columns):
-            a[i][j] = num
-            num += 1
+    for num in range(size):
+        c = enroll[num]
+        p = referral[num]
+        v[name_dict[c]] = name_dict[p]
 
-    for sx, sy, ex, ey in queries:
-        sx, sy, ex, ey = sx - 1, sy - 1, ex - 1, ey - 1
-        points = [(sx, sy), (ex, sy), (ex, ey), (sx, ey)]
-        height, width = ex - sx + 1, ey - sy + 1
-        first_num = a[sx][sy]
-        min_num = 10001
-        min_num = min(min_num, first_num)
+    sell_size = len(seller)
+    for num in range(sell_size):
+        node = name_dict[seller[num]]
+        price = amount[num] * 100
+        while node != 0:
+            remain_price = int(price * 0.1)
+            get_price = price - remain_price
 
-        for dir in range(4):
-            cx, cy = points[dir]
-            range_size = height if dir % 2 == 0 else width
-            range_size -= 1
+            if remain_price < 1:
+                get_price = price
+            answer[node-1] += get_price
+            if get_price == price:
+                break
 
-            for idx in range(range_size):
-
-                bx, by = cx + dx[dir], cy + dy[dir]
-                a[cx][cy] = a[bx][by]
-                if dir == 3 and idx == range_size - 1:
-                    a[cx][cy] = first_num
-                min_num = min(min_num, a[cx][cy])
-                cx, cy = bx, by
-
-        answer.append(min_num)
-
+            price = remain_price
+            node = v[node]
     return answer
 
 
 
 
-r = 6
-c = 6
-q = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
-# q = [[2,2,5,4]]
 
-res = solution(r, c, q)
-print("res", res)
+enroll = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
+referral = ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]
+seller = ["young", "john", "tod", "emily", "mary"]
+amount = [12, 4, 2, 5, 10]
+
+res = solution(enroll, referral, seller, amount)
+
+print('res', res)
