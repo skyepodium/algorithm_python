8e9b6aa9cbff6d72b148b0e048bdc59d30bc03a1
skyepodium <skyepodium@gmail.com>
2021-08-07 23:45:48 +0900

    leet code most common prefix

--- algorithm.py
+++ algorithm.py
@@ -1,52 +1,30 @@
-import collections
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
 
+        base = strs[0]
+        res = ""
+        size = 0
 
-def riverSizes(matrix):
+        for i in range(0, len(base) + 1):
+            cur = base[:i]
+            notSame = False
+            for word in strs:
+                if word[:i] != cur:
+                    notSame = True
+                    break
+            if notSame:
+                continue
 
-    n = len(matrix)
-    m = len(matrix[0])
-    d = (0, -1), (0, 1), (1, 0), (-1, 0)
+            if len(cur) > size:
+                size = len(cur)
+                res = cur
 
-    check = [[-1 for _ in range(m)] for _ in range(n)]
-    res = []
+        return res
 
-    def bfs(i, j):
-        check[i][j] = 0
-        cnt = 1
-        q = collections.deque()
-        q.append((i, j))
+sl = Solution()
 
-        while q:
-            x, y = q.popleft()
+strs = ["flower","flow","flight"]
+strs = ["a"]
+res = sl.longestCommonPrefix(strs)
 
-            for dx, dy in d:
-                nx = x + dx
-                ny = y + dy
-                if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                    continue
-
-                if matrix[nx][ny] == 1 and check[nx][ny] == -1 :
-                    check[nx][ny] = 0
-                    cnt += 1
-                    q.append((nx, ny))
-
-        return cnt
-
-    for i in range(n):
-        for j in range(m):
-            if matrix[i][j] == 1 and check[i][j] == -1:
-                bfs_cnt = bfs(i, j)
-                res.append(bfs_cnt)
-
-    return res
-
-
-m = [
-    [1, 0, 0, 1, 0],
-    [1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 0]
-  ]
-
-print(riverSizes(m))
+print(res)
