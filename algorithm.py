46ca8f1616839b2cb93a4c6a16933b1efa8b9ee2
skyepodium <skyepodium@gmail.com>
2021-08-09 18:42:30 +0900

    max-area-of-island

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,52 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-from typing import Optional
-
-from Cython.Compiler.ExprNodes import ListNode
+import collections
 
 
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        result = []
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
 
-        node = head
+        d = (0, -1), (0, 1), (1, 0), (-1, 0)
 
-        while node:
-            result.append(node.val)
-            node = node.next
+        n = len(grid)
+        m = len(grid[0])
 
-        return result == result[::-1]
+        check = [[False for _ in range(m)] for _ in range(n)]
+        result = 0
+
+        def bfs(x, y):
+            q = collections.deque()
+            check[x][y] = True
+            q.append((x, y))
+            cnt = 0
+
+            while q:
+                x, y, = q.popleft()
+                cnt += 1
+                for dx, dy in d:
+                    nx, ny = x + dx, y + dy
+
+                    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                        continue
+
+                    if grid[nx][ny] == 1 and not check[nx][ny]:
+                        check[nx][ny] = True
+                        q.append((nx, ny))
+
+            return cnt
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1 and not check[i][j]:
+                    result = max(result, bfs(i, j))
+
+        return result
+
+
+
+
+sl = Solution()
+
+grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+grid = [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]
+res = sl.maxAreaOfIsland(grid)
+
+print(res)
