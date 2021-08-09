4c127b47f26ef32dc82b958b9a5145612beaf073
skyepodium <skyepodium@gmail.com>
2021-08-09 23:33:20 +0900

    letter-combinations-of-a-phone-number

--- algorithm.py
+++ algorithm.py
@@ -2,16 +2,16 @@ class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
 
         nums = [
-            [],
-            [],
-            ["a", "b", "c"],
-            ["d", "e", "f"],
-            ["g", "h", "i"],
-            ["j", "k", "l"],
-            ["m", "n", "o"],
-            ["p", "q", "r", "s"],
-            ["t", "u", "v"],
-            ["w", "x", "y", "z"]
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
         ]
 
         result = []
