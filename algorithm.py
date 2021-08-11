6a0f8966cffc5bb81f8b27926d0e366900711bed
skyepodium <skyepodium@gmail.com>
2021-08-11 20:47:18 +0900

    baekjoon 1926 by dijkstra

--- algorithm.py
+++ algorithm.py
@@ -1,38 +1,38 @@
-def solution(scores):
-    answer = ""
-
-    def get_grade(val):
-        res = "F"
-        if val >= 90: res = "A"
-        elif val >= 80: res = "B"
-        elif val >= 70: res = "C"
-        elif val >= 50: res = "D"
-        print(res)
-        return res
-
-    size = len(scores)
-    for j in range(size):
-        cur_list = []
-        cur_val = scores[j][j]
-        for i in range(size):
-            cur_list.append(scores[i][j])
-
-        cur_list.sort()
-
-        if cur_list[0] == cur_val and cur_list[0] != cur_list[1]:
-            answer += get_grade(sum(cur_list[1:]) / len(cur_list[1:]))
-            continue
-        if cur_list[size-1] == cur_val and cur_list[size-1] != cur_list[size-2]:
-            answer += get_grade(sum(cur_list[:size-1]) / len(cur_list[:size-1]))
-            continue
-        answer += get_grade(sum(cur_list) / len(cur_list))
-
-    return answer
+import heapq
 
 
+def dijkstra(node):
+    d[node] = 0
+    pq = []
+    heapq.heappush(pq, (d[node], node))
 
-s = [[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]]
-s = [[70,49,90],[68,50,38],[73,31,100]]
-res = solution(s)
+    while pq:
+        cost, node = heapq.heappop(pq)
 
-print(res)
+        if cost > d[node]: continue
+
+        for n_cost, n_node in v[node]:
+            if d[n_node] > d[node] + n_cost:
+                d[n_node] = d[node] + n_cost
+                heapq.heappush(pq, (d[n_node], n_node))
+
+
+n = int(input())
+m = int(input())
+
+max_val = 100000 * 1000
+v = [[] for _ in range(n+1)]
+d = [max_val for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    v[a].append((c, b))
+
+start_node, end_node = map(int, input().split())
+
+dijkstra(start_node)
+
+print(d[end_node])
+
+
+
