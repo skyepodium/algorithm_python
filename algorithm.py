148ed858865a36bed23da72f8ab523ce8d826079
skyepodium <skyepodium@gmail.com>
2021-08-11 10:23:12 +0900

    concatenation-of-array

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,3 @@
 class Solution:
-    def subsets(self, nums: list[int]) -> list[list[int]]:
-
-        result = []
-        size = len(nums)
-
-        def go(idx, stack):
-            result.append(stack[:])
-
-            for i in range(idx, size):
-                stack.append(nums[i])
-                go(i + 1, stack)
-                stack.pop()
-
-        go(0, [])
-
-        return result
-
-sl = Solution()
-
-nums = [1, 2, 3]
-
-res = sl.subsets(nums)
-
-print(res)
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
