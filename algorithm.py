b611a025a7c2d575efdd3960c070d8cf6f8b9361
skyepodium <skyepodium@gmail.com>
2021-08-14 09:53:40 +0900

    programmers - cesar

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,22 @@
-def solution(array, commands):
-    answer = []
-
-    for s, e, n in commands:
-        answer.append(sorted(array[s - 1:e])[n - 1])
-
+def solution(s, n):
+    answer = ""
+    for c in s:
+        if c == " ":
+            answer += c
+        elif c.islower():
+            n_ord = ord(c) + n
+            answer += chr(n_ord - 26) if n_ord > ord('z') else chr(n_ord)
+        else:
+            n_ord = ord(c) + n
+            answer += chr(n_ord - 26) if n_ord > ord('Z') else chr(n_ord)
     return answer
+
+s = "AB"
+n = 1
+s = "a B z"
+n = 4
+res = solution(s, n)
+
+print(res)
+
+print(chr(97))
