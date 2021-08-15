41f97aea497ba25e1e5ba7e2cc981f84c29142a8
skyepodium <skyepodium@gmail.com>
2021-08-15 22:20:14 +0900

    programmer star

--- algorithm.py
+++ algorithm.py
@@ -1,22 +1,3 @@
-import collections
+a, b = map(int, input().strip().split(' '))
 
-
-def solution(n, lost, reserve):
-    answer = 0
-
-    remain_diff = collections.Counter(reserve) - collections.Counter(lost)
-    remains = list(remain_diff.keys())
-
-    lost_diff = collections.Counter(lost) - collections.Counter(reserve)
-    losts = list(lost_diff.keys())
-
-    cnt = 0
-    for val in losts:
-        if val + 1 in remains:
-            cnt += 1
-            remains.remove(val + 1)
-        elif val - 1 in remains:
-            cnt += 1
-            remains.remove(val - 1)
-
-    return n - len(losts) + cnt
+print(("*" * a + "\n") * b)
