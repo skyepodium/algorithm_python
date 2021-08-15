76d718ec38984ec2a89b22309e78d71cad8eebae
skyepodium <skyepodium@gmail.com>
2021-08-15 23:39:46 +0900

    min max value

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,2 @@
 def solution(s):
-    stack = []
-
-    for c in s:
-        if c == "(":
-            stack.append(c)
-        else:
-            if not stack:
-                return False
-
-            if stack[len(stack) - 1] == "(":
-                stack.pop()
-            else:
-                return False
-
-    if len(stack) > 0:
-        return False
-
-    return True
+    return f"{min([int(x) for x in s.split(' ')])} {max([int(x) for x in s.split(' ')])}"
