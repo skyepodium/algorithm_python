7dc451b1b8c0ac0f83de8efc80debda941729a99
skyepodium <skyepodium@gmail.com>
2021-08-15 22:29:43 +0900

    programmers num of factors

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,8 @@
 def solution(left, right):
     answer = 0
-
-    def get_cnt(num):
-        res = 0
-        for i in range(1, num + 1):
-            if num % i == 0:
-                res += 1
-        return res & 1 != 1
-
-    for i in range(left, right + 1):
-        if get_cnt(i):
-            answer += i
-        else:
+    for i in range(left,right+1):
+        if int(i**0.5) == i**0.5:
             answer -= i
-
+        else:
+            answer += i
     return answer
