5ca6ec3e7c428dcb4783c52ba86604347ce246d3
skyepodium <skyepodium@gmail.com>
2021-08-15 23:33:31 +0900

    programmer stack

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,19 @@
-def solution(A,B):
-    return sum(x*y for x, y in zip(sorted(A), sorted(B, reverse=True)))
+def solution(s):
+    stack = []
+
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        else:
+            if not stack:
+                return False
+
+            if stack[len(stack) - 1] == "(":
+                stack.pop()
+            else:
+                return False
+
+    if len(stack) > 0:
+        return False
+
+    return True
