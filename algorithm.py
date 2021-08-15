61d893ef7f7bf293f084a8e6c9cf75ef84f74543
skyepodium <skyepodium@gmail.com>
2021-08-15 22:42:42 +0900

    programmers phoneketmon

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,2 @@
-def solution(left, right):
-    answer = 0
-    for i in range(left,right+1):
-        if int(i**0.5) == i**0.5:
-            answer -= i
-        else:
-            answer += i
-    return answer
+def solution(nums):
+    return min(len(set(nums)), len(nums)//2)
