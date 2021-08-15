6b2211dafebba6475f8238ed5de5cb123ff21082
skyepodium <skyepodium@gmail.com>
2021-08-15 22:25:59 +0900

    programmers num of factors

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,17 @@
-a, b = map(int, input().strip().split(' '))
+def solution(left, right):
+    answer = 0
 
-print(("*" * a + "\n") * b)
+    def get_cnt(num):
+        res = 0
+        for i in range(1, num + 1):
+            if num % i == 0:
+                res += 1
+        return res & 1 != 1
+
+    for i in range(left, right + 1):
+        if get_cnt(i):
+            answer += i
+        else:
+            answer -= i
+
+    return answer
