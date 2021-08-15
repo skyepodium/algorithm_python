8fea7a6cbe1e1925bf947842ab3cac0cfe7cd40e
skyepodium <skyepodium@gmail.com>
2021-08-15 23:07:37 +0900

    programmers tuple

--- algorithm.py
+++ algorithm.py
@@ -1,29 +1,25 @@
-def solution(answers):
-    cnt = [0, 0, 0]
-    size = len(answers)
-    one = [1, 2, 3, 4, 5]
-    two = [2, 1, 2, 3, 2, 4, 2, 5]
-    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+import re
 
-    one = (size // len(one) + 1) * one
-    two = (size // len(two) + 1) * two
-    three = (size // len(three) + 1) * three
 
-    for i in range(len(answers)):
-        val = answers[i]
+def solution(s):
+    s_list = s.split("},{")
 
-        if val == one[i]:
-            cnt[0] += 1
-        if val == two[i]:
-            cnt[1] += 1
-        if val == three[i]:
-            cnt[2] += 1
+    tuple_list = []
+    for cur in s_list:
+        cur = re.sub("{{|}}", "", cur)
 
-    max_val = max(cnt)
+        cur_list = cur.split(",")
+        tuple_list.append(cur_list)
 
+    tuple_list.sort(key=lambda x: len(x))
+
+    s = set()
     res = []
-    for i in range(len(cnt)):
-        if cnt[i] == max_val:
-            res.append(i + 1)
+    for t in tuple_list:
+        for val in t:
+            if val not in s:
+                res.append(int(val))
+                s.add(val)
+                break
 
     return res
