25b6f239ffa243b7d2bad86bebc48454619c5426
skyepodium <skyepodium@gmail.com>
2021-08-15 23:14:30 +0900

    programmers tuple

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,7 @@
 import re
-
+import collections
 
 def solution(s):
-    s_list = s.split("},{")
 
-    tuple_list = []
-    for cur in s_list:
-        cur = re.sub("{{|}}", "", cur)
-
-        cur_list = cur.split(",")
-        tuple_list.append(cur_list)
-
-    tuple_list.sort(key=lambda x: len(x))
-
-    s = set()
-    res = []
-    for t in tuple_list:
-        for val in t:
-            if val not in s:
-                res.append(int(val))
-                s.add(val)
-                break
-
-    return res
+    s = collections.Counter(re.findall("\d+", s))
+    return [int(key) for key, cnt in sorted(s.items(), key=lambda x: x[1], reverse=True)]
