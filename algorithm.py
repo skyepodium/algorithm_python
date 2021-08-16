84beebfbd04b4cf2ed5c78666ae7bd9483ba6324
skyepodium <skyepodium@gmail.com>
2021-08-16 11:31:36 +0900

    programmers 3sum prime

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,25 @@
-n, k = map(int, input().split())
+def solution(nums):
+    # 1. init
+    answer = 0
 
-d = [True for _ in range(n+1)]
-cnt = 0
+    # 2. max_val
+    max_int = max(nums) * 3
+    d = [True for _ in range(max_int + 1)]
 
-for i in range(2, n+1):
-    for j in range(i, n+1, i):
-        if d[j]:
-            d[j] = False
-            cnt += 1
-            if cnt == k:
-                print(j)
-                break
+    # 3. eratos
+    def eratos():
+        for i in range(2, max_int + 1):
+            for j in range(i+i, max_int+1, i):
+                if d[j]:
+                    d[j] = False
+
+    eratos()
+
+    # 4. nested loop
+    for i in range(len(nums) - 2):
+        for j in range(i + 1, len(nums) - 1):
+            for k in range(j + 1, len(nums)):
+                if d[nums[i] + nums[j] + nums[k]]:
+                    answer += 1
+
+    return answer
