e6d20cb3cd19c0178fa1a0cf079fd8f5b2521948
skyepodium <skyepodium@gmail.com>
2021-08-16 12:37:52 +0900

    괄호 회전하기

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,37 @@
-def solution(x, n):
-    return [i * x for i in range(1, n+1)]
+def solution(s):
+    # 1. shift with slicing
+    def shift(s):
+        return s[-1] + s[:len(s) - 1]
+
+    # 2. check valid
+    def is_valid(s):
+        d = {
+            "{": "}",
+            "[": "]",
+            "(": ")"
+        }
+        stack = []
+        for c in s:
+            if c not in d:
+                if not stack:
+                    return False
+
+                top = stack.pop()
+                if d[top] != c:
+                    return False
+            else:
+                stack.append(c)
+
+        if stack:
+            return False
+
+        return True
+
+    # 3. shift and update cnt
+    answer = 0
+    for i in range(0, len(s)):
+        if is_valid(s):
+            answer += 1
+        s = shift(s)
+
+    return answer
