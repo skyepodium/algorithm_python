344b9ab2cc76b4838524f3acb7e760d1d4ce97d1
skyepodium <skyepodium@gmail.com>
2021-08-16 14:16:28 +0900

    regex number

--- algorithm.py
+++ algorithm.py
@@ -1,37 +1,29 @@
-def solution(s):
-    # 1. shift with slicing
-    def shift(s):
-        return s[-1] + s[:len(s) - 1]
+import re
 
-    # 2. check valid
-    def is_valid(s):
-        d = {
-            "{": "}",
-            "[": "]",
-            "(": ")"
-        }
-        stack = []
-        for c in s:
-            if c not in d:
-                if not stack:
-                    return False
 
-                top = stack.pop()
-                if d[top] != c:
-                    return False
-            else:
-                stack.append(c)
+def main(amountText):
+    # 1. 숫자 콤마만 있는지 확인
+    # match를 쓰면 안된다. match는 문자열의 시작부터 패턴이 일치하는지 찾는다.
+    # 25,000,123원은 앞에서 숫자를 이미 만족했기 때문에 '원'은 매칭되지 않는다고 나온다.
+    if re.search("[^0-9,]", amountText):
+        print("error: contain non digit or non comma", s)
+        return False
 
-        if stack:
-            return False
+    # 2. 0으로 시작 검사
+    # .은 글자 1개를 대체합니다.
+    if re.fullmatch("0.+", amountText):
+        print("error: start with 0", s)
+        return False
 
-        return True
+    # 3, 3자리씩 콤마가 있거나 없는지 확인합니다.
+    if not re.fullmatch("\d{1,3}(,?\d{3})+", amountText):
+        print("error: wrong comma", s)
+        return False
 
-    # 3. shift and update cnt
-    answer = 0
-    for i in range(0, len(s)):
-        if is_valid(s):
-            answer += 1
-        s = shift(s)
+    return True
 
-    return answer
+s_list = ["Hello", "-123", "10,000원", "0123", "0", "1", ",123", "1,123", "1234"]
+
+for s in s_list:
+    if main(s):
+        print("correct", s)
