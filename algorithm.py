a7dcf6eb8334db8896438d5eb3bd5f5646a945af
skyepodium <skyepodium@gmail.com>
2021-08-16 15:04:53 +0900

    regex money comma

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,11 @@
 import re
 
 
-def main(amountText):
-    if not re.fullmatch("\d{1,3}(,\d{3})+|[1-9]{1}\d*|0", amountText):
-        return False
-
-    return True
+def main(money):
+    return re.fullmatch("\d{1,3}(,\d{3})+|\d|[1-9]{1}\d*", money)
 
 s_list = ["Hello", "-123", "10,000원", "0123", "0", "1", ",123", "1,123", "1234"]
 
 for s in s_list:
-    if main(s):
-        print("correct", s)
-    else:
-        print("error", s)
+    res = "correct" if main(s) else "error"
+    print(res, s)
