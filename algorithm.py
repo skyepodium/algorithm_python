378f8fa16a9d2d92e9918c7f52d9280352e4fe37
skyepodium <skyepodium@gmail.com>
2021-08-16 18:12:51 +0900

    pair delete

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,16 @@
-from heapq import heappush, heappop, heapify
+def solution(s):
+    stack = []
 
+    for c in s:
+        if stack:
+            if stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+        else:
+            stack.append(c)
 
-def solution(scoville, K):
-    # 1. init
-    answer = 0
+    if stack:
+        return 0
 
-    # 2. list to heap
-    heapify(scoville)
-
-    # 3. loop
-    while len(scoville) >= 2 and scoville[0] < K:
-        a = heappop(scoville)
-        b = heappop(scoville)
-
-        heappush(scoville, a + b * 2)
-        answer += 1
-
-    return answer if scoville[0] >= K else -1
+    return 1
