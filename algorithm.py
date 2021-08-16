0d4dbdc1a05d28953a5ecdd155aa3401e07b9fc5
skyepodium <skyepodium@gmail.com>
2021-08-16 11:58:17 +0900

    programmers x만큼 간격이 있는 n개의 숫자

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,2 @@
-def solution(nums):
-    # 1. init
-    answer = 0
-
-    # 2. max_val
-    max_int = max(nums) * 3
-    d = [True for _ in range(max_int + 1)]
-
-    # 3. eratos
-    def eratos():
-        for i in range(2, max_int + 1):
-            for j in range(i+i, max_int+1, i):
-                if d[j]:
-                    d[j] = False
-
-    eratos()
-
-    # 4. nested loop
-    for i in range(len(nums) - 2):
-        for j in range(i + 1, len(nums) - 1):
-            for k in range(j + 1, len(nums)):
-                if d[nums[i] + nums[j] + nums[k]]:
-                    answer += 1
-
-    return answer
+def solution(x, n):
+    return [i * x for i in range(1, n+1)]
