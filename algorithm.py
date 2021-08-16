98a22813d1fb436409ef1a4dc57ce4da0165591b
skyepodium <skyepodium@gmail.com>
2021-08-16 10:52:28 +0900

    programmers 3sum prime

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,22 @@
-def solution(s):
-    return f"{min([int(x) for x in s.split(' ')])} {max([int(x) for x in s.split(' ')])}"
+def solution(nums):
+    # 0. init
+    answer = 0
+
+    # 1. sort
+    nums.sort()
+
+    # 2. prime
+    def is_prime(num):
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    # 3 two pointer
+    for i in range(len(nums) - 2):
+        for j in range(i + 1, len(nums) - 1):
+            for k in range(j + 1, len(nums)):
+                if is_prime(nums[i] + nums[j] + nums[k]):
+                    answer += 1
+
+    return answer
