ae4f0c58e0e3ea7d0ee96319a376be5400cf43de
skyepodium <skyepodium@gmail.com>
2021-08-18 00:30:55 +0900

    intersection-of-two-arrays

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,6 @@
+from collections import Counter
+
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # 0. init
-        size = len(numbers)
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-        # 1. binary_search
-        def binary_search(l, r, remain):
-            while l <= r:
-                mid = l + (r - l) // 2
-
-                if numbers[mid] > remain:
-                    r = mid - 1
-                elif numbers[mid] < remain:
-                    l = mid + 1
-                else:
-                    return mid
-            return -1
-
-        # 2. loop
-        for i in range(size - 1):
-            l = i + 1
-            r = size - 1
-
-            res = binary_search(l, r, target - numbers[i])
-            if res != -1:
-                return [i + 1, res + 1]
+        return list((Counter(nums1) - (Counter(nums1) - Counter(nums2))).keys())
