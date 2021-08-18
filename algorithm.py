0bf20a08d7d0d38a5c3740043d6b961e7c996037
skyepodium <skyepodium@gmail.com>
2021-08-18 18:45:32 +0900

    search-a-2d-matrix

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,24 @@
-from collections import Counter
-
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # 1. exception
+        if not matrix:
+            return -1
 
-        return list((Counter(nums1) - (Counter(nums1) - Counter(nums2))).keys())
+        # 2. init
+        i = 0
+        j = len(matrix[0]) - 1
+        n = len(matrix)
+        m = len(matrix[0])
+
+        # 3. loop
+        while i < n and j >= 0:
+            cur = matrix[i][j]
+
+            if cur > target:
+                j -= 1
+            elif cur < target:
+                i += 1
+            else:
+                return True
+
+        return False
