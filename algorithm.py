9893dfa8c64cea3fb345e29e004798161784ab7c
skyepodium <skyepodium@gmail.com>
2021-08-18 21:36:55 +0900

    leetcode tabulation

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,16 @@
-a = [[1, 2, 3], [4, 5, 6]]
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # 0. exception        
+        if len(nums) <= 2:
+            return max(nums)
 
-print(any(-1 in x for x in a))
+        # 1. init
+        d = [x for x in nums]
+        d[0] = nums[0]
+        d[1] = max(nums[0], nums[1])
+
+        # 2. tabulation
+        for i in range(2, len(nums)):
+            d[i] = max(d[i - 1], d[i - 2] + nums[i])
+
+        return d[len(nums) - 1]
