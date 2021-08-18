36d3fd2f5e6a596032273d8dcf46652bc609862e
skyepodium <skyepodium@gmail.com>
2021-08-18 18:56:12 +0900

    python any

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,3 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # 1. exception
-        if not matrix:
-            return -1
+a = [[1, 2, 3], [4, 5, 6]]
 
-        # 2. init
-        i = 0
-        j = len(matrix[0]) - 1
-        n = len(matrix)
-        m = len(matrix[0])
-
-        # 3. loop
-        while i < n and j >= 0:
-            cur = matrix[i][j]
-
-            if cur > target:
-                j -= 1
-            elif cur < target:
-                i += 1
-            else:
-                return True
-
-        return False
+print(any(-1 in x for x in a))
