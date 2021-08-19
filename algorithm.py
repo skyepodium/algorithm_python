3fcb49158db185bab9e917ee73961ede29ceef59
skyepodium <skyepodium@gmail.com>
2021-08-19 21:52:15 +0900

    leet code maximum-subarray

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,15 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        # 0. exception        
-        if len(nums) <= 2:
-            return max(nums)
+from typing import List
 
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
         # 1. init
-        d = [x for x in nums]
-        d[0] = nums[0]
-        d[1] = max(nums[0], nums[1])
+        d = [0 for _ in range(len(nums))]
+        res = d[0] = nums[0]
 
         # 2. tabulation
-        for i in range(2, len(nums)):
-            d[i] = max(d[i - 1], d[i - 2] + nums[i])
+        for i in range(1, len(nums)):
+            d[i] = max(nums[i], d[i - 1] + nums[i])
+            res = max(res, d[i])
 
-        return d[len(nums) - 1]
+        return res
