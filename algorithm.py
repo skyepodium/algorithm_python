e07b4abf4952003ff09b8e5f5f8abc3defe8c7c2
skyepodium <skyepodium@gmail.com>
2021-08-19 23:23:02 +0900

    squares-of-a-sorted-array

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,3 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-
-        d = [0 for _ in range(n + 1)]
-
-        def go(i):
-            if i <= 2:
-                return i
-
-            if d[i] > 0:
-                return d[i]
-
-            d[i] = go(i - 2) + go(i - 1)
-
-            return d[i]
-
-        return go(n)
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([x * x for x in nums])
