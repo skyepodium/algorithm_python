d483a8a7d17b835c502c6d07f2f0149cf71b40bd
skyepodium <skyepodium@gmail.com>
2021-08-21 16:41:58 +0900

    merge-two-sorted-lists

--- algorithm.py
+++ algorithm.py
@@ -4,12 +4,13 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # 1. init
-        rev = None
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        # 2. loop
-        while head:
-            rev, rev.next, head = head, rev, head.next
+        if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
 
-        return rev
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+
+        return l1
+
