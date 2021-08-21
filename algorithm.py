a0863cfd45203055312c950b31144c78958b624b
skyepodium <skyepodium@gmail.com>
2021-08-22 08:15:59 +0900

    swap-nodes-in-pairs

--- algorithm.py
+++ algorithm.py
@@ -4,24 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = head
 
-        res = node = ListNode(0)
+        while head and head.next:
+            head.val, head.next.val = head.next.val, head.val
+            head = head.next.next
 
-        carry = 0
-        while l1 or l2 or carry:
-            sum_val = 0
-            if l1:
-                sum_val += l1.val
-                l1 = l1.next
-
-            if l2:
-                sum_val += l2.val
-                l2 = l2.next
-
-            carry, val = divmod(sum_val + carry, 10)
-
-            node.next = ListNode(val)
-            node = node.next
-
-        return res.next
+        return res
