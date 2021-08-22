4c7205e5799877bec6f43629f7c273804e0c2439
skyepodium <skyepodium@gmail.com>
2021-08-22 23:07:32 +0900

    linked-list-cycle

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,15 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        root = node = ListNode(0)
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
 
-        while head:
-            if head.val != val:
-                node.next = ListNode(head.val, None)
-                node = node.next
-            head = head.next
+        slow = fast = head
 
-        return root.next
+        while fast and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
