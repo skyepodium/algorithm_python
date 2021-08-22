d71bc15b7feb69130275b20eca4f4f521f5f250f
skyepodium <skyepodium@gmail.com>
2021-08-22 09:18:19 +0900

    implement-stack-using-queues

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,19 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        res = head
+class MyStack:
 
-        while head and head.next:
-            head.val, head.next.val = head.next.val, head.val
-            head = head.next.next
+    def __init__(self):
+        self.q = collections.deque()
 
-        return res
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
