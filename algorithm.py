ea621a5edf372befd54e55eb7fe1d1cdf4f6caf9
skyepodium <skyepodium@gmail.com>
2021-08-22 22:50:07 +0900

    remove-linked-list-elements

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        root = node = ListNode(0)
 
-        stack, counter, seen = [], Counter(s), set()
+        while head:
+            if head.val != val:
+                node.next = ListNode(head.val, None)
+                node = node.next
+            head = head.next
 
-        for c in s:
-            counter[c] -= 1
-
-            if c in seen:
-                continue
-
-            while stack and c < stack[-1] and counter[stack[-1]] > 0:
-                seen.remove(stack.pop())
-
-            stack.append(c)
-            seen.add(c)
-
-        return "".join(stack)
+        return root.next
