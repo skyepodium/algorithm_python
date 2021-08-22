337b1b90413d271c8ae831becfefac24e8f6715d
skyepodium <skyepodium@gmail.com>
2021-08-22 11:05:25 +0900

    remove-duplicate-letters

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,10 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # 1. init
-        stack = []
-        res = [0 for _ in range(len(temperatures))]
+    def removeDuplicateLetters(self, s: str) -> str:
 
-        # 2. loop
-        for i in range(len(temperatures)):
-            cur = temperatures[i]
+        for c in sorted(set(s)):
+            suffix = s[s.index(c):]
 
-            while stack and temperatures[stack[-1]] < cur:
-                top = stack.pop()
-                res[top] = i - top
+            if set(s) == set(suffix):
+                return c + self.removeDuplicateLetters(suffix.replace(c, ""))
 
-            stack.append(i)
-
-        return res
+        return ""
