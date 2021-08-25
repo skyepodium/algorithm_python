bf6a5c264207d3c12b9c636e8a5f2c324dfb6223
skyepodium <skyepodium@gmail.com>
2021-08-25 19:58:13 +0900

    add strings

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,31 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 1. init        
-        l = 0
-        r = len(nums) - 1
+    def addStrings(self, num1: str, num2: str) -> str:
+        # 1. init
+        num1 = num1[::-1]
+        num2 = num2[::-1]
 
-        # 2. sort
-        d = [(idx, val) for idx, val in enumerate(nums)]
-        d.sort(key=lambda x: x[1])
+        # 2. swap with length
+        if len(num1) < len(num2):
+            num1, num2 = num2, num1
 
-        # 3. two pointer
-        while l < r:
-            cur = d[l][1] + d[r][1]
-            if cur > target:
-                r -= 1
-            elif cur < target:
-                l += 1
-            else:
-                return d[l][0], d[r][0]
+        n = len(num1)
+        m = len(num2)
+        carry = 0
+        res = ""
+
+        # 3. loop
+        for i in range(n):
+            cur = 0
+
+            cur += ord(num1[i]) - ord('0')
+
+            if i < m:
+                cur += ord(num2[i]) - ord('0')
+
+            carry, val = divmod(cur + carry, 10)
+            res += str(val)
+
+        if carry > 0:
+            res += str(carry)
+
+        return str(res)[::-1]
