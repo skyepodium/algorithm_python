4a402af4d747d663f48e26e23c14c4e6fbf2675f
skyepodium <skyepodium@gmail.com>
2021-08-25 21:31:33 +0900

    maximum-depth-of-binary-tree

--- algorithm.py
+++ algorithm.py
@@ -1,33 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         # 1. exception
         if not root:
-            return False
+            return 0
 
         # 2. init
-        res = []
+        self.res = 0
 
         # 3. dfs
-        def dfs(node, val):
+        def dfs(node, cnt):
             if not node:
                 return
 
-            if not node.left and not node.right and val + node.val == targetSum:
-                res.append(True)
+            if not node.left and not node.right:
+                self.res = max(self.res, cnt)
                 return
 
-            if node.left:
-                dfs(node.left, val + node.val)
+            dfs(node.left, cnt + 1)
+            dfs(node.right, cnt + 1)
 
-            if node.right:
-                dfs(node.right, val + node.val)
+        dfs(root, 1)
 
-        dfs(root, 0)
-
-        return any(res)
+        return self.res   
