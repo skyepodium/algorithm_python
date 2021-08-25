1f76afc20f1b643a2fd056c4161f223e53e6762e
skyepodium <skyepodium@gmail.com>
2021-08-25 18:37:11 +0900

    Making A Large Island

--- algorithm.py
+++ algorithm.py
@@ -1,47 +1,59 @@
-from heapq import heappop, heappush
+from collections import deque
 
-def solution(n, k, cmd):
-    # 1. init
-    left, right, removed = [], [], []
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        # 1. init
+        n = len(grid)
+        check = [[-1 for _ in range(n)] for _ in range(n)]
+        d = [(0, -1), (0, 1), (1, 0), (-1, 0)]
 
-    # 2. make heap
-    for i in range(k):
-        heappush(left, -i)
-    for i in range(k, n):
-        heappush(right, i)
+        # 2. bfs
+        def bfs(start_x, start_y, idx):
+            check[start_x][start_y] = idx
+            q = deque()
+            q.append((start_x, start_y))
+            cnt = 0
 
-    # 3. cmd
-    for c in cmd:
-        if c[0] == 'U':
-            cnt = int(c.split(" ")[1])
-            while left and cnt > 0:
-                heappush(right, -heappop(left))
-                cnt -= 1
-        elif c[0] == 'D':
-            cnt = int(c.split(" ")[1])
-            while right and cnt > 0:
-                heappush(left, -heappop(right))
-                cnt -= 1
-        elif c[0] == 'C':
-            removed.append(heappop(right))
+            while q:
+                x, y = q.popleft()
+                cnt += 1
+                for dx, dy in d:
+                    nx, ny = x + dx, y + dy
+                    if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                        continue
+                    if check[nx][ny] == -1 and grid[nx][ny] == 1:
+                        check[nx][ny] = idx
+                        q.append((nx, ny))
+            return cnt
 
-            if not right:
-                heappush(right, -heappop(left))
-        elif c[0] == 'Z':
-            top = removed.pop()
-            if top < right[0]:
-                heappush(left, -top)
-            else:
-                heappush(right, top)
+        # 3. loop
+        idx = 0
+        cnt_dict = {}
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1 and check[i][j] == -1:
+                    cnt = bfs(i, j, idx)
+                    cnt_dict[idx] = cnt
+                    idx += 1
 
-    res_left = [-x for x in left]
-    total_set = set(res_left + right)
+        # 4. loop
+        res = 0
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    cur_cnt = 0
+                    seen = set()
+                    for dx, dy in d:
+                        nx, ny = i + dx, j + dy
 
-    result = ""
-    for i in range(n):
-        val = "X"
-        if i in total_set:
-            val = "O"
-        result += val
+                        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                            continue
 
-    return result
+                        if check[nx][ny] > -1 and grid[nx][ny] == 1 and check[nx][ny] not in seen:
+                            n_idx = check[nx][ny]
+                            n_cnt = cnt_dict[n_idx]
+                            cur_cnt += n_cnt
+                            seen.add(n_idx)
+                    res = max(res, cur_cnt + 1)
+
+        return res if res > 0 else n * n
