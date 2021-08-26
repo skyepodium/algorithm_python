370a1d03c7d66f3615879b06245b4021bea4986b
Kim Jung yoon <skyepodium@gmail.com>
2021-08-26 09:27:58 +0900

    leet code path sum2

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,34 @@
 class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
-        # 1. exception
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        # 1. init
+        res = []
+        
+        # 2. exception
         if not root:
-            return 0
-
-        # 2. init
-        self.res = 100001
-
+            return res
+        
         # 3. dfs
-        def dfs(node, cnt):
+        def dfs(node, val, stack):
             if not node:
                 return
-
+            
             if not node.left and not node.right:
-                self.res = min(self.res, cnt)
-                return
-
-            dfs(node.left, cnt + 1)
-            dfs(node.right, cnt + 1)
-
-        dfs(root, 1)
-
-        return self.res 
+                if val + node.val == targetSum:
+                    stack.append(node.val)
+                    res.append(stack[:])
+                    stack.pop()
+                    return
+            
+            
+            stack.append(node.val)
+            dfs(node.left, val + node.val, stack)
+            stack.pop()
+            
+            stack.append(node.val)
+            dfs(node.right, val + node.val, stack)
+            stack.pop()
+        
+        dfs(root, 0, [])
+        
+        return res
+            
