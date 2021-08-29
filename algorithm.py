0d339e2f8d92104c4295e78f04142125ae0dc50f
skyepodium <skyepodium@gmail.com>
2021-08-29 11:54:27 +0900

    reverse-bits

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,13 @@
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        # 1. init
-        s = set()
+    def reverseBits(self, n: int) -> int:
+        def bit_to_str(num):
+            res = ""
+            while num > 0:
+                res += str(num % 2)
+                num //= 2
 
-        # 2. squared_sum
-        def squared_sum(num: int) -> int:
-            num = str(num)
-            res = 0
+            return res[::-1].zfill(32)[::-1]
 
-            for c in num:
-                res += (ord(c) - ord('0')) ** 2
+        val = bit_to_str(n)
 
-            return res
-
-        # 3. loop
-        while n not in s:
-            s.add(n)
-            n = squared_sum(n)
-            if n == 1:
-                return True
-
-        return False
+        return int(val, 2)
