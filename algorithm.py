15e3d8dffe9d60f8ad32f9accdccfce8b5b681f9
skyepodium <skyepodium@gmail.com>
2021-08-29 13:05:47 +0900

    cheapest-flights-within-k-stops

--- algorithm.py
+++ algorithm.py
@@ -2,38 +2,40 @@ from heapq import heappush, heappop
 
 
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+
         # 1. init
-        max_val = 10001
-        res = 0
-        d = [max_val for _ in range(n + 1)]
+        max_val = 1000001
+        res = max_val
+        d = [[max_val for _ in range(k + 2)] for _ in range(n + 1)]
         v = [[] for _ in range(n + 1)]
 
-        # 2. make graph
-        for s, e, c in times:
-            v[s].append((c, e))
+        # 2. make_graph
+        for s, e, c in flights:
+            v[s].append((c, 0, e))
 
-        # 3. dijkstra
         def dijkstra(start_node):
+            d[start_node][0] = 0
             pq = []
-            d[start_node] = 0
-            heappush(pq, (d[start_node], start_node))
+            heappush(pq, (d[start_node][0], 0, start_node))
 
             while pq:
-                cost, node = heappop(pq)
+                cost, cnt, node = heappop(pq)
 
-                if d[node] < cost:
+                if d[node][cnt] < cost:
                     continue
 
-                for n_cost, n_node in v[node]:
-                    if d[n_node] > d[node] + n_cost:
-                        d[n_node] = d[node] + n_cost
-                        heappush(pq, (d[n_node], n_node))
+                for n_cost, n_cnt, n_node in v[node]:
+                    if cnt + 1 <= k + 1 and d[n_node][cnt + 1] > d[node][cnt] + n_cost:
+                        d[n_node][cnt + 1] = d[node][cnt] + n_cost
+                        heappush(pq, (d[n_node][cnt + 1], cnt + 1, n_node))
 
-        dijkstra(k)
+        dijkstra(src)
 
-        # 4. check
-        for i in range(1, n + 1):
-            res = max(res, d[i])
+        # 3. check loop
+        for i in range(0, k + 2):
+            res = min(res, d[dst][i])
 
-        return -1 if res == max_val else res
+        return res if res != max_val else -1
+
+
