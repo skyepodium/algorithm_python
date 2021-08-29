f2bbc00075fbec98e918df913d289132197c5cd8
skyepodium <skyepodium@gmail.com>
2021-08-29 12:36:00 +0900

    network-delay-time

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,39 @@
+from heapq import heappush, heappop
+
+
 class Solution:
-    def reverseBits(self, n: int) -> int:
-        def bit_to_str(num):
-            res = ""
-            while num > 0:
-                res += str(num % 2)
-                num //= 2
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # 1. init
+        max_val = 10001
+        res = 0
+        d = [max_val for _ in range(n + 1)]
+        v = [[] for _ in range(n + 1)]
 
-            return res[::-1].zfill(32)[::-1]
+        # 2. make graph
+        for s, e, c in times:
+            v[s].append((c, e))
 
-        val = bit_to_str(n)
+        # 3. dijkstra
+        def dijkstra(start_node):
+            pq = []
+            d[start_node] = 0
+            heappush(pq, (d[start_node], start_node))
 
-        return int(val, 2)
+            while pq:
+                cost, node = heappop(pq)
+
+                if d[node] < cost:
+                    continue
+
+                for n_cost, n_node in v[node]:
+                    if d[n_node] > d[node] + n_cost:
+                        d[n_node] = d[node] + n_cost
+                        heappush(pq, (d[n_node], n_node))
+
+        dijkstra(k)
+
+        # 4. check
+        for i in range(1, n + 1):
+            res = max(res, d[i])
+
+        return -1 if res == max_val else res
