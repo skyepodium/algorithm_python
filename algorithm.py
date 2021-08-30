c448a0455044111486dcd7cfd39ae0d8ba006e40
skyepodium <skyepodium@gmail.com>
2021-08-30 19:44:04 +0900

    programmers 순위검색

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,63 @@
-def solution(people, limit):
+from collections import defaultdict
+import re
+import itertools
+
+def solution(info, query):
     # 1. init
-    l = 0
-    r = len(people) - 1
-    cnt = 0
+    info_dict = defaultdict()
+    res = []
 
-    # 2. sort
-    people.sort()
+    # 2. set info
+    def set_info(info):
+        for x in info:
+            l, p, c, f, s = x.split(" ")
 
-    # 3. two pointer
-    while l <= r:
-        cnt += 1
-        if people[l] + people[r] <= limit:
-            l += 1
-        r -= 1
+            l_list = [l, '-']
+            p_list = [p, '-']
+            c_list = [c, '-']
+            f_list = [f, '-']
+            s_list = [int(s)]
+            lpcf = list(itertools.product(l_list,p_list,c_list,f_list,s_list))
 
-    return cnt
+            for q, w, e, r, t in lpcf:
+                key = f"{q}{w}{e}{r}"
+                if key in info_dict:
+                    info_dict[key].append(t)
+                else:
+                    info_dict[key] = [t]
+
+    set_info(info)
+
+    # 3. sort
+    for val in info_dict.values():
+        val.sort()
+
+    # 4. lower bound
+    def lower_bound(key, score):
+        s_list = info_dict[key]
+        l = 0
+        r = len(s_list)
+
+        while l < r:
+            mid = l + (r - l) // 2
+
+            if s_list[mid] < score:
+                l = mid + 1
+            else:
+                r = mid
+
+        return r
+
+    # 5 query
+    for q in query:
+        l, p, c, f, s = re.compile(" and | ").split(q)
+        key = f"{l}{p}{c}{f}"
+        score = int(s)
+
+        cnt = 0
+        if key in info_dict:
+            idx = lower_bound(key, score)
+            cnt = len(info_dict[key]) - idx
+        res.append(cnt)
+
+    return res
