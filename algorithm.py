86540cdbe96c9c9a2815af8653e9b22255b080f2
skyepodium <skyepodium@gmail.com>
2021-08-30 18:16:08 +0900

    programmers 구명보트

--- algorithm.py
+++ algorithm.py
@@ -1,36 +1,17 @@
-import re
-
-def solution(files):
+def solution(people, limit):
     # 1. init
-    f_list = []
+    l = 0
+    r = len(people) - 1
+    cnt = 0
 
-    # 2. split_file with regex
-    def split_file(s):
-        # 1) head
-        head = re.search("[^0-9]+", s).group(0)
+    # 2. sort
+    people.sort()
 
-        # 2) number
-        number = re.search("[0-9]{1,5}", s).group(0)
+    # 3. two pointer
+    while l <= r:
+        cnt += 1
+        if people[l] + people[r] <= limit:
+            l += 1
+        r -= 1
 
-        return [head, number]
-
-    # 3. loop
-    for idx, f in enumerate(files):
-        head, number = split_file(f)
-        f_list.append([head, number, idx, f])
-
-    # 4. sort
-    f_list.sort(key=lambda x: (
-        x[0].lower(),
-        int(x[1]),
-        x[2]
-    ))
-
-    return [x[3] for x in f_list]
-
-files = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
-
-res = solution(files)
-
-print('res', res)
-
+    return cnt
