33a12a5b1ebfde4b0f1d52d9ffc401dfa49f4302
skyepodium <skyepodium@gmail.com>
2021-08-30 21:21:03 +0900

    programmers 스킬 트리

--- algorithm.py
+++ algorithm.py
@@ -1,22 +1,14 @@
-def solution(number, k):
+import re
+
+def solution(skill, skill_trees):
     # 1. init
-    stack = []
+    pattern = f"[^{skill}]"
+    res = 0
 
     # 2. loop
-    for c in number:
-        while k > 0 and stack and ord(stack[-1]) < ord(c):
-            stack.pop()
-            k -= 1
+    for s in skill_trees:
+        r = re.sub(pattern, "", s)
+        if skill.startswith(r):
+            res += 1
 
-        stack.append(c)
-
-    # 3. exception
-    while k > 0:
-        stack.pop()
-        k -= 1
-
-    return "".join(stack)
-
-res = solution("321", 2)
-
-print('res', res)
+    return res
