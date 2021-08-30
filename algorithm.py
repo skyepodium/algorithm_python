fd35dd4d0c232edd45c582662ea4da543955e4de
skyepodium <skyepodium@gmail.com>
2021-08-30 21:44:23 +0900

    invert-binary-tree

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,6 @@
-import re
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
 
-def solution(skill, skill_trees):
-    # 1. init
-    pattern = f"[^{skill}]"
-    res = 0
-
-    # 2. loop
-    for s in skill_trees:
-        r = re.sub(pattern, "", s)
-        if skill.startswith(r):
-            res += 1
-
-    return res
+        return root
