486a6a6d1d1b3cd4682a719341914d64144b8677
Kim Jung yoon <skyepodium@gmail.com>
2021-08-31 11:42:47 +0900

    프로그래머스 다음 큰 숫자
    프로그래머스 다음 큰 숫자

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,22 @@
-def solution(s):
+def solution(n):
     # 1. init
-    cnt = 0
-    removed_cnt = 0
+    max_int = 1000001
+    one_cnt = 0
     
-    # 2. len_to_binary
-    def len_to_binary(n):
+    # 2. to_binary
+    def to_binary(n):
         res = ""
-        
         while n > 0:
-            res += str(n % 2)
+            res += str(n%2);
             n //= 2
-        
         return res[::-1]
     
-    # 3. loop
-    while s != "1":
-        # 1) remove zero
-        zero_removed = s.replace("0", "")
-        removed_cnt += len(s) - len(zero_removed)
-        
-        # 2) length to binary
-        s = len_to_binary(len(zero_removed))
-        
-        # 3) update
-        cnt += 1
-
-    return [cnt, removed_cnt]
+    # 3. update_one_cnt
+    one_cnt = to_binary(n).count("1")
+    
+    # 4. loop
+    for i in range(n+1, max_int):
+        if one_cnt == to_binary(i).count("1"):
+            return i
+    
+    return -1
