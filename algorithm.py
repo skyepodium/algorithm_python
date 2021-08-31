75af04d50246e21d26b9170a279e7d574582d2e6
Kim Jung yoon <skyepodium@gmail.com>
2021-08-31 11:06:14 +0900

    programmers 이진 변환 반복하기
    programmers 이진 변환 반복하기

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,28 @@
-class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root:
-            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+def solution(s):
+    # 1. init
+    cnt = 0
+    removed_cnt = 0
+    
+    # 2. len_to_binary
+    def len_to_binary(n):
+        res = ""
+        
+        while n > 0:
+            res += str(n % 2)
+            n //= 2
+        
+        return res[::-1]
+    
+    # 3. loop
+    while s != "1":
+        # 1) remove zero
+        zero_removed = s.replace("0", "")
+        removed_cnt += len(s) - len(zero_removed)
+        
+        # 2) length to binary
+        s = len_to_binary(len(zero_removed))
+        
+        # 3) update
+        cnt += 1
 
-        return root
+    return [cnt, removed_cnt]
