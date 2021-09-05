f055c129bac86439318745d86c223ef787e26402
skyepodium <skyepodium@gmail.com>
2021-09-05 12:46:51 +0900

    binary-tree-inorder-traversal

--- algorithm.py
+++ algorithm.py
@@ -1,22 +1,21 @@
-def solution(n):
-    # 1. init
-    max_int = 1000001
-    one_cnt = 0
-    
-    # 2. to_binary
-    def to_binary(n):
-        res = ""
-        while n > 0:
-            res += str(n%2);
-            n //= 2
-        return res[::-1]
-    
-    # 3. update_one_cnt
-    one_cnt = to_binary(n).count("1")
-    
-    # 4. loop
-    for i in range(n+1, max_int):
-        if one_cnt == to_binary(i).count("1"):
-            return i
-    
-    return -1   
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # 1. exception
+        if not root:
+            return []
+
+        # 2. init
+        res = []
+
+        # 3. inorder
+        def go(node):
+            if not node:
+                return
+
+            go(node.left)
+            res.append(node.val)
+            go(node.right)
+
+        go(root)
+
+        return res
