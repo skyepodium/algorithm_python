446645f7160bbf86dfae88bb93c3a519b9a36b59
skyepodium <skyepodium@gmail.com>
2021-09-05 13:33:05 +0900

    add-binary

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,10 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        return str(bin(n)).count('1')
+    def addBinary(self, a: str, b: str) -> str:
+        c = int(a, 2) + int(b, 2)
+
+        res = ""
+        while c > 0:
+            res += str(c % 2)
+            c //= 2
+
+        return "0" if res == "" else res[::-1]        
