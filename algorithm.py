fc0a7542bda6c790dafa39d5407fb1e0f648a440
skyepodium <skyepodium@gmail.com>
2021-09-05 13:23:05 +0900

    number-of-1-bits

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,3 @@
 class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # 1. init
-        res = []
-
-        # 2. exception
-        if not root:
-            return res
-
-        # 3. post_order
-        def post_order(node):
-            if not node:
-                return
-
-            post_order(node.left)
-            post_order(node.right)
-            res.append(node.val)
-
-        post_order(root)
-
-        return res
+    def hammingWeight(self, n: int) -> int:
+        return str(bin(n)).count('1')
