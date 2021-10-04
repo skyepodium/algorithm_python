4a2ee9b0d4593e5a5cb27984fdd0709050b1f227
skyepodium <skyepodium@gmail.com>
2021-10-04 19:30:29 +0900

    island-perimeter

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,54 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+from typing import List
+from collections import deque
 
 class Solution:
-    def deleteNode(self, node):
-        node.val = node.next.val
-        node.next = node.next.next
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # 1. init
+        n, m = len(grid), len(grid[0])
+        check = [[False for _ in range(m)] for _ in range(n)]
+        d = (0, -1), (0, 1), (1, 0), (-1, 0)
+        result = 0
+
+        # 2. bfs
+        def bfs(x, y):
+            check[x][y] = True
+            q = deque()
+            cnt = 0
+            link_cnt = 0
+            q.append((x, y))
+
+            while q:
+                x, y = q.popleft()
+                cnt += 1
+                for dx, dy in d:
+                    nx = x + dx
+                    ny = y + dy
+                    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                        continue
+
+                    if grid[nx][ny] == 1:
+                        link_cnt += 1
+                        if not check[nx][ny]:
+                            check[nx][ny] = True
+                            q.append((nx, ny))
+            return cnt * 4 - link_cnt
+
+        # 3. loop
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1 and not check[i][j]:
+                    result += bfs(i, j)
+
+
+        return result
+
+sl = Solution()
+
+grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+# grid = [[1]]
+# grid = [[1, 0]]
+# grid = [[0, 1]]
+# grid = [[1,1],[1,1]]
+res = sl.islandPerimeter(grid)
+
+print('res', res)
