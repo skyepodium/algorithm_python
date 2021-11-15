81a42281dd434ec7c2b945d718a66a795544eccd
skyepodium <skyepodium@gmail.com>
2021-11-15 23:16:32 +0900

    leet code binary-search loop

--- algorithm.py
+++ algorithm.py
@@ -1,54 +1,21 @@
 from typing import List
-from collections import deque
+
 
 class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        # 1. init
-        n, m = len(grid), len(grid[0])
-        check = [[False for _ in range(m)] for _ in range(n)]
-        d = (0, -1), (0, 1), (1, 0), (-1, 0)
-        result = 0
+    def search(self, nums: List[int], target: int) -> int:
 
-        # 2. bfs
-        def bfs(x, y):
-            check[x][y] = True
-            q = deque()
-            cnt = 0
-            link_cnt = 0
-            q.append((x, y))
+        def binary_search(s, e, target):
 
-            while q:
-                x, y = q.popleft()
-                cnt += 1
-                for dx, dy in d:
-                    nx = x + dx
-                    ny = y + dy
-                    if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                        continue
+            while s <= e:
+                mid = s + (e - s) // 2
+                cur = nums[mid]
+                if cur < target:
+                    s = mid + 1
+                elif cur == target:
+                    return mid
+                else:
+                    e = mid - 1
 
-                    if grid[nx][ny] == 1:
-                        link_cnt += 1
-                        if not check[nx][ny]:
-                            check[nx][ny] = True
-                            q.append((nx, ny))
-            return cnt * 4 - link_cnt
+            return -1
 
-        # 3. loop
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1 and not check[i][j]:
-                    result += bfs(i, j)
-
-
-        return result
-
-sl = Solution()
-
-grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
-# grid = [[1]]
-# grid = [[1, 0]]
-# grid = [[0, 1]]
-# grid = [[1,1],[1,1]]
-res = sl.islandPerimeter(grid)
-
-print('res', res)
+        return binary_search(0, len(nums) - 1, target)
