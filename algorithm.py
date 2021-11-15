23e87615c11911a1f6f92131107793e51baef016
skyepodium <skyepodium@gmail.com>
2021-11-16 00:21:59 +0900

    binary-gap with one loop

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,17 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        # 1. num_to_binary
-        def num_to_binary(n):
-            s = ""
-            while n > 0:
-                s += str(n % 2)
-                n //= 2
-
-            return s
-
-        s = num_to_binary(n)
-
-        # 2. loop
+        # 1. init
         max_dist = 0
         cnt = 0
-        for i in range(0, len(s)):
-            if s[i] == "1":
+
+        # 2. loop
+        while n > 0:
+            if n % 2 == 1:
                 max_dist = max(max_dist, cnt)
                 cnt = 1
             else:
-                if cnt > 0:
-                    cnt += 1
+                if cnt > 0: cnt += 1
+
+            n //= 2
 
         return max_dist
