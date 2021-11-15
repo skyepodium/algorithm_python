52e7ff5ff48d1a02cfcc62712f85c03156288d79
skyepodium <skyepodium@gmail.com>
2021-11-15 23:24:23 +0900

    leet code binary-search recursive

--- algorithm.py
+++ algorithm.py
@@ -5,17 +5,15 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
 
         def binary_search(s, e, target):
+            if s > e:
+                return -1
 
-            while s <= e:
-                mid = s + (e - s) // 2
-                cur = nums[mid]
-                if cur < target:
-                    s = mid + 1
-                elif cur == target:
-                    return mid
-                else:
-                    e = mid - 1
-
-            return -1
+            mid = s + (e - s) // 2
+            if nums[mid] < target:
+                return binary_search(mid + 1, e, target)
+            elif nums[mid] > target:
+                return binary_search(s, mid - 1, target)
+            else:
+                return mid
 
         return binary_search(0, len(nums) - 1, target)
