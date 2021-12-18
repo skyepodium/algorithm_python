9fab9c0585251c301d92a500e1f3763526a60a55
skyepodium <skyepodium@gmail.com>
2021-12-18 16:54:38 +0900

    self-dividing-numbers

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,24 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        # 1. init
+        result = []
+
+        # 2. check
+        def check(num):
+            str_num = str(num)
+
+            if '0' in str_num:
+                return False
+
+            for a in str_num:
+                if num % int(a) != 0:
+                    return False
+
+            return True
+
+        # 3. loop
+        for i in range(left, right + 1):
+            if check(i):
+                result.append(i)
+
+        return result
