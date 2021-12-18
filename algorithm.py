4183e94644b5e3b88e86a39aaac5fce921c6f2a1
skyepodium <skyepodium@gmail.com>
2021-12-18 18:00:28 +0900

    jewels-and-stones

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,3 @@
 class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        def is_even(num):
-            return -1 if num % 2 == 0 else 1
-
-        return sorted(nums, key=lambda x: is_even(x))
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return sum([s in jewels for s in stones])
