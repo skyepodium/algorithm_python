e9528d4325f6a8491ce62c6af86d1b8679a5c8a0
skyepodium <skyepodium@gmail.com>
2021-12-19 18:56:19 +0900

    perfect-squares

--- algorithm.py
+++ algorithm.py
@@ -1,29 +1,20 @@
 class Solution:
-    def numIslands(self, grid: list[list[str]]) -> int:
+    def numSquares(self, n: int) -> int:
+        # 1. init
+        sq_nums = []
+        d = [10000 for _ in range(n + 1)]
 
-        n = len(grid)
-        m = len(grid[0])
-        check = [[False for _ in range(m)] for _ in range(n)]
-        d = (0, -1), (0, 1), (1, 0), (-1, 0)
+        # 2. make sq_num
+        for i in range(1, 101):
+            sq_num = i * i
+            sq_nums.append(sq_num)
+            if sq_num <= n:
+                d[sq_num] = 1
 
-        cnt = 0
+        # 3. bottom up
+        for i in range(1, n + 1):
+            for sq in sq_nums:
+                if i + sq <= n:
+                    d[i + sq] = min(d[i] + 1, d[i + sq])
 
-        def dfs(x, y):
-            check[x][y] = True
-
-            for dx, dy in d:
-                nx, ny = x + dx, y + dy
-
-                if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                    continue
-
-                if grid[nx][ny] == "1" and not check[nx][ny]:
-                    dfs(nx, ny)
-
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == "1" and not check[i][j]:
-                    dfs(i, j)
-                    cnt += 1
-
-        return cnt
+        return d[n]
