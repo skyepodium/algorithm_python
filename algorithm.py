9f48bc83e9cfbd3e73e6c31552fad6897497ead0
skyepodium <skyepodium@gmail.com>
2021-12-19 22:15:46 +0900

    sort-colors with bubble sort

--- algorithm.py
+++ algorithm.py
@@ -1,49 +1,15 @@
-from collections import deque
-
-
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+    def sortColors(self, nums: List[int]) -> None:
         # 1. init
-        n = len(board)
-        m = len(board[0])
-        check = [[False for _ in range(m)] for _ in range(n)]
-        d = (0, -1), (0, 1), (1, 0), (-1, 0)
+        n = len(nums)
 
-        # 2. bfs
-        def bfs(x, y):
-            check[x][y] = True
-            is_adjacent_to_border = False
-            q = deque()
-            q.append((x, y))
-            s = []
+        # 2. bubble sort
+        for i in range(n - 1, 0, -1):
+            for j in range(i):
+                if nums[j] > nums[i]:
+                    temp = nums[i]
+                    nums[i] = nums[j]
+                    nums[j] = temp
 
-            while q:
-                x, y = q.popleft()
-                s.append((x, y))
-                if x == 0 or x == n - 1 or y == 0 or y == m - 1:
-                    is_adjacent_to_border = True
 
-                for dx, dy in d:
-                    nx, ny = x + dx, y + dy
-                    if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                        continue
 
-                    if not check[nx][ny] and board[nx][ny] == 'O':
-                        check[nx][ny] = True
-                        q.append((nx, ny))
-
-            return (is_adjacent_to_border, s)
-
-        # 3. flip
-        def flip(s):
-            for x, y in s:
-                board[x][y] = 'X'
-
-        # 4. loop
-        for i in range(n):
-            for j in range(m):
-                if not check[i][j] and board[i][j] == 'O':
-                    is_adjacent_to_border, s = bfs(i, j)
-
-                    if not is_adjacent_to_border:
-                        flip(s)        
