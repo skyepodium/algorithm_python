f11b85aa26e0d258f2c72ac20aa8f8b8a56e98d0
skyepodium <skyepodium@gmail.com>
2021-12-21 00:06:27 +0900

    check-if-two-string-arrays-are-equivalent

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,3 @@
-from functools import reduce
-
-
 class Solution:
-    def xorOperation(self, n: int, start: int) -> int:
-        return reduce(lambda x, y: x ^ y, [x for x in range(start, start + n * 2, 2)])
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return "".join(word1) == "".join(word2)
