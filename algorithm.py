f8f649e2c047196dd0138e4640e18542ddc9c1f7
skyepodium <skyepodium@gmail.com>
2022-01-22 23:28:31 +0900

    two-sum with O(n)

--- algorithm.py
+++ algorithm.py
@@ -1,30 +1,13 @@
-from typing import List
-
-
 class Solution:
-    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         # 1. init
-        letter = []
-        digit = []
+        d = {}
 
         # 2. loop
-        for log in logs:
-            is_numberic = True
-            for i in log.split(" ")[1:]:
-                if not i.isnumeric():
-                    is_numberic = False
-            if is_numberic:
-                digit.append(log)
+        for idx, num in enumerate(nums):
+            remain = target - num
+            if remain in d:
+                return [d[remain], idx]
             else:
-                letter.append(log)
+                d[num] = idx
 
-        # 3. sort
-        letter.sort(key=lambda x: (x.split(" ")[1:], x.split(" ")[0]))
-
-        return letter + digit
-
-sl = Solution()
-logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
-res = sl.reorderLogFiles(logs)
-
-print('res', res)
