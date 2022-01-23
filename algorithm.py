923687b842835478f32f3885330efa6f2dceac15
skyepodium <skyepodium@gmail.com>
2022-01-23 19:48:03 +0900

    leet code contains-duplicate-ii with dictionary

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,13 @@
-from typing import List
-
-
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         # 1. init
-        numList = []
+        d = {}
+
+        # 2. loop
         for idx, num in enumerate(nums):
-            numList.append((idx, num))
-
-        # 2. sort
-        numList.sort(key=lambda x: (x[1], x[0]))
-
-        # 3. loop
-        for i in range(len(numList) - 1):
-            f_idx, f_num = numList[i]
-            s_idx, s_num = numList[i+1]
-            if f_num == s_num and abs(f_idx - s_idx) <= k:
+            if num in d and idx - d[num] <= k:
                 return True
 
+            d[num] = idx
+
         return False
-
-sl = Solution()
-nums = [1,2,3,1,2,3]
-k = 2
-res = sl.containsNearbyDuplicate(nums, k)
-
-print('res', res)
