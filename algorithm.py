cfb06237c8c38df46e4cae998fd447e4969a3519
skyepodium <skyepodium@gmail.com>
2022-01-23 19:45:14 +0900

    leet code contains-duplicate-ii

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,28 @@
+from typing import List
+
+
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         # 1. init
-        d = {}
-
-        # 2. loop
+        numList = []
         for idx, num in enumerate(nums):
-            remain = target - num
-            if remain in d:
-                return [d[remain], idx]
-            else:
-                d[num] = idx
+            numList.append((idx, num))
 
+        # 2. sort
+        numList.sort(key=lambda x: (x[1], x[0]))
+
+        # 3. loop
+        for i in range(len(numList) - 1):
+            f_idx, f_num = numList[i]
+            s_idx, s_num = numList[i+1]
+            if f_num == s_num and abs(f_idx - s_idx) <= k:
+                return True
+
+        return False
+
+sl = Solution()
+nums = [1,2,3,1,2,3]
+k = 2
+res = sl.containsNearbyDuplicate(nums, k)
+
+print('res', res)
