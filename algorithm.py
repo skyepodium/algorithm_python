c1b84b467ea8c119650766b0e327e3d1d392cf59
skyepodium <skyepodium@gmail.com>
2022-01-25 00:37:30 +0900

    sort-integers-by-the-number-of-1-bits

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,25 @@
-class Solution:
-    def addDigits(self, num: int) -> int:
-        # 1. loop
-        while num >= 10:
-            n = num
-            res = 0
-            while n > 0:
-                res += n % 10
-                n //= 10
-            num = res
+from typing import List
 
-        return num
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        # 1. get_binary
+        def get_one_cnt(num):
+            one_cnt = 0
+            while num > 0:
+                if num % 2 == 1: one_cnt += 1
+                num //= 2
+            return one_cnt
+
+        # 2. init
+        arr_list = [(x, get_one_cnt(x)) for x in arr]
+
+        # 3. sort
+        arr_list.sort(key=lambda x: (x[1], x[0]))
+
+        return [x[0] for x in arr_list]
 
 sl = Solution()
-
-res = sl.addDigits(0)
-
+arr = [0,1,2,3,4,5,6,7,8]
+res = sl.sortByBits(arr)
 print('res', res)
