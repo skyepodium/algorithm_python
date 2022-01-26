386cae6575366b07b5086b02bdbe1d583312f480
skyepodium <skyepodium@gmail.com>
2022-01-26 23:07:56 +0900

    count-good-triplets

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,15 @@
 class Solution:
-    def sortByBits(self, arr: List[int]) -> List[int]:
-        return sorted(arr, key=lambda x: (bin(x).count('1'), x))
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        # 1. init
+        n = len(arr)
+        res = 0
+
+        # 2. loop
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
+                    q, w, e = arr[i], arr[j], arr[k]
+                    if abs(q - w) <= a and abs(w - e) <= b and abs(q - e) <= c:
+                        res += 1
+
+        return res
