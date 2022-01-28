71d0773dc9d489c2e2429e015676ce0255dcecec
skyepodium <skyepodium@gmail.com>
2022-01-28 23:14:33 +0900

    leet code find-n-unique-integers-sum-up-to-zero

--- algorithm.py
+++ algorithm.py
@@ -1,15 +1,3 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        # 1. inti
-        res = []
-        cnt = n // 2
-
-        # 2. loop
-        for i in range(1, cnt + 1):
-            res.append(i)
-            res.append(-i)
-
-        if n % 2 != 0:
-            res.append(0)
-
-        return res
+        return range(1-n, n, 2)
