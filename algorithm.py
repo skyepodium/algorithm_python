93a6bda27a5d8f6afcaae54fd6b83aeb3d5fac3a
skyepodium <skyepodium@gmail.com>
2022-01-28 23:09:36 +0900

    leet code find-n-unique-integers-sum-up-to-zero

--- algorithm.py
+++ algorithm.py
@@ -1,15 +1,15 @@
 class Solution:
-    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        # 1. init
-        n = len(arr)
-        res = 0
+    def sumZero(self, n: int) -> List[int]:
+        # 1. inti
+        res = []
+        cnt = n // 2
 
         # 2. loop
-        for i in range(n - 2):
-            for j in range(i + 1, n - 1):
-                for k in range(j + 1, n):
-                    q, w, e = arr[i], arr[j], arr[k]
-                    if abs(q - w) <= a and abs(w - e) <= b and abs(q - e) <= c:
-                        res += 1
-    
+        for i in range(1, cnt + 1):
+            res.append(i)
+            res.append(-i)
+
+        if n % 2 != 0:
+            res.append(0)
+
         return res
