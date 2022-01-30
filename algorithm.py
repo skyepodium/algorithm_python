342d14eb7f99dfa147b964e31ce4544014d99154
skyepodium <skyepodium@gmail.com>
2022-01-31 00:22:24 +0900

    element-appearing-more-than-25-in-sorted-array

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,35 @@
 class Solution:
-    def detectCapitalUse(self, word: str) -> bool:
-        return word == word.upper() or word == word.lower() or word[0:1].upper() + word[1:].lower() == word
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        # 1. init
+        numList = list(set(arr))
+
+        def lower_bound(arr, target):
+            l, r = 0, len(arr)
+
+            while l < r:
+                mid = (l + r) // 2
+                if arr[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid
+
+            return r + 1
+
+        def upper_bound(arr, target):
+            l, r = 0, len(arr)
+
+            while l < r:
+                mid = (l + r) // 2
+                if arr[mid] <= target:
+                    l = mid + 1
+                else:
+                    r = mid
+
+            return r + 1
+
+        for num in numList:
+            diff = upper_bound(arr, num) - lower_bound(arr, num)
+            if diff > (len(arr) / 4):
+                return num
+
+        return -1
