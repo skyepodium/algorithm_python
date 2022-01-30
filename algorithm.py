ce49481ad12351a1fae80a72b30885e93797e5be
skyepodium <skyepodium@gmail.com>
2022-01-31 00:59:05 +0900

    find-common-characters

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,27 @@
+from typing import List
+
+
 class Solution:
-    def findSpecialInteger(self, arr: List[int]) -> int:
+    def commonChars(self, words: List[str]) -> List[str]:
         # 1. init
-        diff = len(arr) // 4
+        n = len(words)
+        origin = words[0]
+        res = []
 
         # 2. loop
-        for i in range(len(arr) - diff):
-            if arr[i] == arr[i + diff]:
-                return arr[i]
+        for w in origin:
+            cnt = 0
+            for word in words:
+                if w in word: cnt += 1
+            if n == cnt:
+                res.append(w)
+                for i in range(n):
+                    words[i] = words[i].replace(w, '', 1)
 
-        return arr[-1]
+        return res
+
+sl = Solution()
+
+res = sl.commonChars(["cool","lock","cook"])
+
+print('res', res)
