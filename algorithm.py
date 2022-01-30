994562c432431ea5e3d33c48a18e243acaf8b689
skyepodium <skyepodium@gmail.com>
2022-01-30 23:15:55 +0900

    relative-sort-array

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,19 @@
 class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        return address.replace(".", "[.]")
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        # 1. init
+        d = {}
+        a = []
+        b = []
 
+        # 2. loop
+        for idx, num in enumerate(arr2):
+            d[num] = idx
+
+        # 3. search
+        for num in arr1:
+            if num in d:
+                a.append(num)
+            else:
+                b.append(num)
+
+        return sorted(a, key=lambda x: d[x]) + sorted(b)
