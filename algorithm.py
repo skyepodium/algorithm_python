5b3614e5c515c7db56f54c0067b411a68edc94b5
skyepodium <skyepodium@gmail.com>
2022-01-30 23:51:13 +0900

    leet cod detect-capital

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,3 @@
 class Solution:
-    def toLowerCase(self, s: str) -> str:
-        return s.lower()
+    def detectCapitalUse(self, word: str) -> bool:
+        return word == word.upper() or word == word.lower() or word[0:1].upper() + word[1:].lower() == word
