0df3ecdb9a4c7a9e3736e2ad6614a0b5011296fb
skyepodium <skyepodium@gmail.com>
2022-01-31 01:15:20 +0900

    find-common-characters with counter

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,16 @@
 from typing import List
-
+from collections import Counter
 
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
         # 1. init
-        n = len(words)
-        origin = words[0]
-        res = []
+        res = Counter(words[0])
 
         # 2. loop
-        for w in origin:
-            cnt = 0
-            for word in words:
-                if w in word: cnt += 1
-            if n == cnt:
-                res.append(w)
-                for i in range(n):
-                    words[i] = words[i].replace(w, '', 1)
+        for w in words:
+            res = res & Counter(w)
 
-        return res
+        return list(res.elements())
 
 sl = Solution()
 
