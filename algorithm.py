19eacb6aa54b048bd81c44223d0f2cd802c4f7ef
skyepodium <skyepodium@gmail.com>
2022-01-31 00:31:49 +0900

    element-appearing-more-than-25-in-sorted-array - sliding window

--- algorithm.py
+++ algorithm.py
@@ -1,35 +1,11 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
         # 1. init
-        numList = list(set(arr))
+        diff = len(arr) // 4
 
-        def lower_bound(arr, target):
-            l, r = 0, len(arr)
+        # 2. loop
+        for i in range(len(arr) - diff):
+            if arr[i] == arr[i + diff]:
+                return arr[i]
 
-            while l < r:
-                mid = (l + r) // 2
-                if arr[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid
-
-            return r + 1
-
-        def upper_bound(arr, target):
-            l, r = 0, len(arr)
-
-            while l < r:
-                mid = (l + r) // 2
-                if arr[mid] <= target:
-                    l = mid + 1
-                else:
-                    r = mid
-
-            return r + 1
-
-        for num in numList:
-            diff = upper_bound(arr, num) - lower_bound(arr, num)
-            if diff > (len(arr) / 4):
-                return num
-
-        return -1
+        return arr[-1]
