9be57509ad38d4fe109bc351faddcf67a8b12d50
skyepodium <skyepodium@gmail.com>
2022-01-30 23:45:30 +0900

    leet code capitalize-the-title

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,3 @@
 class Solution:
-    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        # 1. init
-        res = 0
-
-        # 2. loop
-        for w in text.split(" "):
-            isOk = True
-            for a in w:
-                if a in brokenLetters:
-                    isOk = False
-                    break
-            if isOk:
-                res += 1
-
-        return res
+    def capitalizeTitle(self, title: str) -> str:
+        return " ".join([x.capitalize() if len(x) > 2 else x.lower() for x in title.split(" ")])
