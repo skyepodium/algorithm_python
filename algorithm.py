ecd2bbe5e53921a061077f0c2ce78170f5928057
skyepodium <skyepodium@gmail.com>
2022-02-01 00:54:11 +0900

    top-k-frequent-elements - sorted

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,21 @@
-import re
 from typing import List
-
+from collections import Counter
 
 class Solution:
-    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. init
+        res = []
+        c = sorted(Counter(nums).items(), key=lambda x: -x[1])
+        cnt = 0
+        for x in c:
+            cnt += 1
+            res.append(x[0])
+            if cnt >= k:
+                break
 
-        return sum(['' == re.sub(f"[{allowed}]", '', w) for w in words])
+        return res
+
+sl = Solution()
+res = sl.topKFrequent([1, 1, 1, 2, 2, 3], 2)
+
+print('res', res)
