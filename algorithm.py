282eada72bc1d43913d9a7b44ff072ded622e0b2
skyepodium <skyepodium@gmail.com>
2022-02-01 00:16:48 +0900

    richest-customer-wealth

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,3 @@
-from typing import List
-from collections import Counter
-
 class Solution:
-    def commonChars(self, words: List[str]) -> List[str]:
-        # 1. init
-        res = Counter(words[0])
-
-        # 2. loop
-        for w in words:
-            res = res & Counter(w)
-
-        return list(res.elements())
-
-sl = Solution()
-
-res = sl.commonChars(["cool","lock","cook"])
-
-print('res', res)
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max([sum(x) for x in accounts])
