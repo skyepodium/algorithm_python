af4ce90b464e2dd5c99021c9f7c4891ccab7081d
skyepodium <skyepodium@gmail.com>
2022-02-02 00:30:07 +0900

    find-the-town-judge

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,18 @@
 class Solution:
-    def bitwiseComplement(self, n: int) -> int:
-        # 0. exception
-        if n == 0: return 1
-
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
         # 1. init
         res = 0
-        base = 0
+        in_list = [0 for _ in range(n + 1)]
+        out_list = [0 for _ in range(n + 1)]
 
         # 2. loop
-        while n > 0:
-            res += 2 ** base if n % 2 == 0 else 0
-            n //= 2
-            base += 1
+        for a, b in trust:
+            in_list[b] += 1
+            out_list[a] += 1
 
-        return res
+        # 3. check
+        for i in range(1, n + 1):
+            if in_list[i] == n - 1 and out_list[i] == 0:
+                return i
+
+        return -1
