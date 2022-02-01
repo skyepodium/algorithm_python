89d52053541f9d266483586ba0e7c51a5d646706
skyepodium <skyepodium@gmail.com>
2022-02-02 00:40:19 +0900

    maximum-difference-between-increasing-elements

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,13 @@
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    def maximumDifference(self, nums: List[int]) -> int:
         # 1. init
-        res = 0
-        in_list = [0 for _ in range(n + 1)]
-        out_list = [0 for _ in range(n + 1)]
+        n = len(nums)
+        res = -1
 
         # 2. loop
-        for a, b in trust:
-            in_list[b] += 1
-            out_list[a] += 1
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    res = max(res, nums[i] - nums[j])
 
-        # 3. check
-        for i in range(1, n + 1):
-            if in_list[i] == n - 1 and out_list[i] == 0:
-                return i
-
-        return -1
+        return res    
