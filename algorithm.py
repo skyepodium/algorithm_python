3050a100abd3bdba36d2ae63fd54488dfc5a01a5
skyepodium <skyepodium@gmail.com>
2022-02-02 00:52:01 +0900

    generate-a-string-with-characters-that-have-odd-counts

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,3 @@
 class Solution:
-    def countCharacters(self, words: List[str], chars: str) -> int:
-        # 1. init
-        c = Counter(chars)
-        res = 0
-
-        # 2. loop
-        for w in words:
-            temp = (c | Counter(w)) - c
-            if len(temp) == 0:
-                res += len(w)
-
-        return res
+    def generateTheString(self, n: int) -> str:
+        return 'a' * n if n % 2 == 1 else 'a' * (n - 1) + 'b'
