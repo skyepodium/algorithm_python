a02b684c7a6d512325af81dc32ea3a9ed809f5de
skyepodium <skyepodium@gmail.com>
2022-02-03 01:40:48 +0900

    minimum-absolute-difference

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,21 @@
 class Solution:
-    def generateTheString(self, n: int) -> str:
-        return 'a' * n if n % 2 == 1 else 'a' * (n - 1) + 'b'
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        # 1. init
+        diff = 2147483647
+        n = len(arr)
+        res = []
+
+        # 2. sort
+        arr.sort()
+
+        # 3. loop
+        for i in range(n - 1):
+            if arr[i] < arr[i + 1]:
+                diff = min(diff, arr[i + 1] - arr[i])
+
+        for i in range(n - 1):
+            if arr[i] < arr[i + 1]:
+                if diff == arr[i + 1] - arr[i]:
+                    res.append((arr[i], arr[i + 1]))
+
+        return res
