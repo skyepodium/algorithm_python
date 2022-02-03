8e1ee0095677c16978bedf08d8ea330ceeebef54
skyepodium <skyepodium@gmail.com>
2022-02-04 01:43:02 +0900

    4sum-ii/

--- algorithm.py
+++ algorithm.py
@@ -1,33 +1,19 @@
-import bisect
-from typing import List
-
+from collections import defaultdict
 
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         # 1. init
         res = 0
-        f = []
-        s = []
+        d = defaultdict(int)
 
         # 2. loop
         for i in nums1:
             for j in nums2:
-                f.append(i+j)
+                d[i+j] += 1
+
         for i in nums3:
             for j in nums4:
-                s.append(i+j)
-
-        # 3. sort
-        f.sort()
-        s.sort()
-
-        # 4. loop
-        for num in f:
-            target = -num
-
-            l_idx = bisect.bisect_left(s, target)
-            r_idx = bisect.bisect_right(s, target)
-            res += r_idx - l_idx
+                res += d[0 - (i+j)]
 
         return res
 
