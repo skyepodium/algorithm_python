37eee4fe5a1b8e0e4bf2082f7a4fc1ebb82b6bf0
skyepodium <skyepodium@gmail.com>
2022-02-05 14:23:35 +0900

    design-hashmap

--- algorithm.py
+++ algorithm.py
@@ -1,51 +1,98 @@
-from typing import List
-from collections import defaultdict
+class MyHashMap:
+    def __init__(self):
+        self.size = 1000
+        self.table = [None] * self.size
 
-class Solution:
-    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        # 1. init
-        d = defaultdict()
-        check = defaultdict()
-        res = []
+    def get_idx(self, key):
+        return key % self.size
 
-        # 2. make graph
-        for s, e in tickets:
-            if s in d: d[s].append(e)
-            else: d[s] = [e]
+    def put(self, key: int, value: int) -> None:
+        idx = self.get_idx(key)
 
-            key = f"{s}{e}"
-            if key in check: check[key] += 1
-            else: check[key] = 1
-        check["NneJFK"] = 1
+        node = self.table[idx]
+        # 1) not exist
+        if not node: self.table[idx] = ListNode(key, value)
 
-        # 3. sort
-        for k in d.keys():
-            d[k].sort()
+        # 2) exist
+        else:
+            prev = None
 
-        # 4. dfs
-        def dfs(s, e):
-            key = f"{s}{e}"
-            check[key] -= 1
-            if e in d:
-                for n_node in d[e]:
-                    next_key = f"{e}{n_node}"
-                    if check[next_key] > 0:
-                        dfs(e, n_node)
+            while node:
+                # duplicated key
+                if node.key == key:
+                    node.value = value
+                    break
+                # search
+                prev, node = node, node.next
+            # not found
+            if not node: prev.next = ListNode(key, value)
 
-            res.append(e)
+    def get(self, key: int) -> int:
+        idx = self.get_idx(key)
 
-        dfs("Nne", "JFK")
+        node = self.table[idx]
+        res = -1
 
-        return res[::-1]
+        while node:
+            if node.key == key:
+                res = node.value
+                break
 
-tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
-tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
-tickets = [["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]
-tickets = [["EZE","AXA"],["TIA","ANU"],["ANU","JFK"],["JFK","ANU"],["ANU","EZE"],["TIA","ANU"],["AXA","TIA"],["TIA","JFK"],["ANU","TIA"],["JFK","TIA"]]
-sl = Solution()
+            node = node.next
 
-res = sl.findItinerary(tickets)
+        return res
 
-print('res', res)
+    def remove(self, key: int) -> None:
+        idx = self.get_idx(key)
+
+        prev, node = None, self.table[idx]
+
+        while node:
+            if node.key == key:
+                if not prev:
+                    self.table[idx] = node.next
+                    return
+                elif not node.next:
+                    prev.next = None
+                    return
+                else:
+                    prev.next = node.next
+                    return
+
+            prev, node = node, node.next
+
+        return None
 
 
+class ListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return f"key: {self.key} value: {self.value} next: {self.next}"
+
+m = MyHashMap()
+
+m.put(1, 1)
+print(m.get(1))
+m.remove(1)
+print(m.get(1))
+
+m.put(1, 1)
+m.put(1001, 2)
+m.put(1000001, 3)
+print(m.get(1))
+print(m.get(1001))
+print(m.get(1000001))
+print()
+m.remove(1001)
+print(m.get(1))
+print(m.get(1001))
+print(m.get(1000001))
+print()
+m.remove(1)
+print(m.get(1))
+print(m.get(1001))
+print(m.get(1000001))
