abf28a610a2caaa2f4d4b4374d28ba5009194813
skyepodium <skyepodium@gmail.com>
2022-02-05 17:12:17 +0900

    Merge k Sorted Lists

--- algorithm.py
+++ algorithm.py
@@ -1,29 +1,36 @@
+from typing import List, Optional
+
+from Cython.Compiler.ExprNodes import ListNode
+from heapq import heappush, heappop
+
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         # 1. init
-        d = {}
-        l = 0
-        res = 0
+        pq = []
+        head = node = None
+        l = []
 
-        # 2. loop
-        for r, cur in enumerate(s):
-            # 1) dup check
-            if cur in d:
-                l = max(l, d[cur] + 1)
+        # 2. make pq
+        for idx, node in enumerate(lists):
+            cur = []
+            while node:
+                cur.append(node.val)
+                node = node.next
+            l.append(cur)
+            if len(cur) > 0:
+                heappush(pq, (cur[0], 0, idx))
 
-            # 2) insert
-            d[cur] = r
+        # 3. search
+        while pq:
+            val, inner_idx, list_idx = heappop(pq)
+            if inner_idx < len(l[list_idx]) - 1:
+                heappush(pq, (l[list_idx][inner_idx + 1], inner_idx + 1, list_idx))
 
-            # 3) update
-            res = max(res, r-l+1)
+            if node:
+                node.next = ListNode(val, None)
+                node = node.next
+            else:
+                node = ListNode(val, None)
+                head = node
 
-        return res
-
-sl = Solution()
-s = "abcabcbb"
-s = "bbbbb"
-s = "pwwkew"
-s = "abba"
-res = sl.lengthOfLongestSubstring(s)
-print('res', res)
-
+        return head
