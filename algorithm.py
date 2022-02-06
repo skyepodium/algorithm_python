477325da0e1607040e2da90eae9b65a83b64eaad
skyepodium <skyepodium@gmail.com>
2022-02-07 00:32:31 +0900

    leet code design-hashmap - open address

--- algorithm.py
+++ algorithm.py
@@ -1,99 +1,73 @@
 class MyHashMap:
     def __init__(self):
-        self.size = 1000
+        self.count = 0
+        self.size = 10
+        self.deleted_size = 0
         self.table = [None] * self.size
         self.load_factor = 0.6
-        self.count = 0
+        self.min_factor = 3
 
-    def resize(self):
-        # 기존 요소 모두 탐색
-        prev_element = self.all_element()
-        # 테이블 크기 * 2
-        self.size *= 2
-        self.table = [None] * self.size
-        self.count = 0
-        for key, value in prev_element:
-            self.put(key, value)
-
-    def all_element(self):
-        res = []
-        for node in self.table:
-            while node:
-                res.append((node.key, node.value))
-                node = node.next
-        return res
-
-    def hash_code(self, key):
+    def _hash_code(self, key):
         return key % self.size
 
+    def __get_entry(self, key):
+        hash_key = self._hash_code(key)
+        root_idx = hash_key
+
+        for offset in range(self.size):
+            new_idx = (root_idx + offset) % self.size
+            node = self.table[new_idx]
+            if not node or (node.key is not None and node.hash_key == hash_key and node.key == key):
+                return new_idx, node
+
+    def resize(self):
+        old_table = self.table
+        self.size *= self.min_factor
+        self.table = [None] * self.size
+        self.count = 0
+        for node in old_table:
+            if node and node.key is not None: self.put(node.key, node.value)
+
     def put(self, key: int, value: int) -> None:
-        h = self.hash_code(key)
+        new_idx, node = self.__get_entry(key)
 
-        node = self.table[h]
-        # 1) 노드가 비어있는 경우
-        if not node:
-            self.count += 1
-            self.table[h] = ListNode(key, value)
-        # 2) 노드에 값이 들어있는 경우
-        else:
-            prev = None
-            while node:
-                # 노드를 찾은 경우 - value 업데이트
-                if node.key == key:
-                    node.value = value
-                    break
-                # 다음 노드로 진행
-                prev, node = node, node.next
-            # 노드를 찾지 못한 경우 - 맨 뒤에 새로우 노드 추가
-            if not node:
-                self.count += 1
-                prev.next = ListNode(key, value)
+        if not node: self.count += 1
+        self.table[new_idx] = Node(key, self._hash_code(key), value)
 
-        if self.count / self.size >= self.load_factor:
+        if (self.deleted_size + self.count) / self.size >= self.load_factor:
             self.resize()
 
     def get(self, key: int) -> int:
-        h = self.hash_code(key)
-
-        node = self.table[h]
-        res = -1
-
-        while node:
-            # 노드를 찾은 경우 - value 반환
-            if node.key == key:
-                res = node.value
-                break
-            # 다음 노드 탐색
-            node = node.next
-        return res
+        new_idx, node = self.__get_entry(key)
+        if not node: return -1
+        else: return node.value
 
     def remove(self, key: int) -> None:
-        h = self.hash_code(key)
+        new_idx, node = self.__get_entry(key)
+        if node and node.key is not None:
+            self.table[new_idx] = Node(None, None, None)
+            self.count -= 1
+            self.deleted_size += 1
 
-        prev, node = None, self.table[h]
-
-        while node:
-            if node.key == key:
-                self.count -= 1
-                # 1) 제일 첫번재 노드 삭제
-                if not prev:
-                    self.table[h] = node.next
-                    break
-                # 2) 제일 마지막 노드 삭제
-                elif not node.next:
-                    prev.next = None
-                    break
-                # 3) 가운데 노드 삭제
-                else:
-                    prev.next = node.next
-                    break
-            # 다음 노드 탐색
-            prev, node = node, node.next
-        return
-
-
-class ListNode:
-    def __init__(self, key, value):
+class Node:
+    def __init__(self, key, hash_key, value):
         self.key = key
+        self.hash_key = hash_key
         self.value = value
-        self.next = None
+
+    def __str__(self):
+        return f"key {self.key} hash_key {self.hash_key} value {self.value}"
+
+m = MyHashMap()
+m.put(0, 0)
+m.put(1, 1)
+print("key 0", m.get(0))
+print(m.get(1))
+m.put(10, 3)
+print(m.get(10))
+m.remove(1)
+print(m.get(10))
+
+a = Node(None, None, None)
+b = Node(None, None, None)
+print(a == b)
