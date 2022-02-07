a67eeb72e2eb4eeec76f57cbf8912a4e83fd8d34
skyepodium <skyepodium@gmail.com>
2022-02-08 01:42:30 +0900

    프로그래머스 - 베스트 앨범

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,30 @@
 from collections import defaultdict
 
-def solution(clothes):
+def solution(genres, plays):
     # 1. init
-    d = defaultdict(int)
-    res = 1
+    res = []
+    c = defaultdict(int)
+    i = defaultdict(list)
 
-    # 2. make dict
-    for c, t in clothes:
-        d[t] += 1
+    # 2. genre total
+    for idx, (g, p) in enumerate(zip(genres, plays)):
+        c[g] += p
+        i[g].append((idx, p))
 
-    # 3. all combinations
-    for x in d.values():
-        res *= (x+1)
+    # 3. count
+    for g, _ in sorted(c.items(), key=lambda x: -x[1]):
+        song_list = i[g]
+        song_list.sort(key=lambda x: (-x[1], x[0]))
+        res.append(song_list[0][0])
+        if len(song_list) >= 2: res.append(song_list[1][0])
 
-    return res - 1
+    return res
+
+
+
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]
+
+res = solution(genres, plays)
+
+print('res', res)
