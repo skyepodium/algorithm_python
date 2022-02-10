4905b2ff7335e484bdcb3786a0807a12b7bef06c
skyepodium <skyepodium@gmail.com>
2022-02-10 23:05:31 +0900

    check-if-n-and-its-double-exist

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,8 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        d = {}
-        arr.sort(key=lambda x: -x)
-        for i, a in enumerate(arr):
-            if a not in d:
-                d[a] = i
-
-            b = a * 2
-            if b in d and d[b] != i:
+        s = set()
+        for a in arr:
+            if a * 2 in s or a / 2 in s:
                 return True
-
-            c = a / 2
-            if c in d and d[c] != i:
-                return True
+            s.add(a)
         return False
