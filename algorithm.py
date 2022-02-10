b0201487907dc8aa67b65712b0dae3c66c49091c
skyepodium <skyepodium@gmail.com>
2022-02-10 22:48:33 +0900

    check-if-n-and-its-double-exist

--- algorithm.py
+++ algorithm.py
@@ -1,30 +1,9 @@
-from collections import defaultdict
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i, a in enumerate(arr):
+            for j, b in enumerate(arr):
+                if i == j: continue
 
-def solution(genres, plays):
-    # 1. init
-    res = []
-    c = defaultdict(int)
-    i = defaultdict(list)
-
-    # 2. genre total
-    for idx, (g, p) in enumerate(zip(genres, plays)):
-        c[g] += p
-        i[g].append((idx, p))
-
-    # 3. count
-    for g, _ in sorted(c.items(), key=lambda x: -x[1]):
-        song_list = i[g]
-        song_list.sort(key=lambda x: (-x[1], x[0]))
-        res.append(song_list[0][0])
-        if len(song_list) >= 2: res.append(song_list[1][0])
-
-    return res
-
-
-
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]
-
-res = solution(genres, plays)
-
-print('res', res)
+                if a * 2 == b:
+                    return True
+        return False
