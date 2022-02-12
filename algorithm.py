391f175f3d7cdc36cb004d3b4a3c93b23bea846e
skyepodium <skyepodium@gmail.com>
2022-02-12 13:32:34 +0900

    delete-the-middle-node-of-a-linked-list

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,18 @@
 class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # 1. init
-        slow = fast = head
+        fast = slow = head
+        s = []
 
         # 2. runner
         while fast and fast.next:
+            s.append(slow.val)
             slow = slow.next
             fast = fast.next.next
 
-        return slow
+        # 3. stack
+        res = slow.next
+        while s:
+            res = ListNode(s.pop(), res)
+
+        return res
