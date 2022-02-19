e8a0beb69b3a3ee947ececb6c7eadb6c87b337ec
skyepodium <skyepodium@gmail.com>
2022-02-19 11:02:21 +0900

    convert-integer-to-the-sum-of-two-no-zero-integers

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,12 @@
+from typing import List
+
+
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
-        for i in range(1, n // 2 + 1):
-            if "0" in str(i) or "0" in str(n - i): continue
+        return [[x, n-x] for x in range(1, n//2 + 1) if not "0" in str(x) and not "0" in str(n-x)][0]
 
-            if i + n - i == n:
-                return [i, n - i]
+sl = Solution()
+
+res = sl.getNoZeroIntegers(11)
+
+print('res', res)
