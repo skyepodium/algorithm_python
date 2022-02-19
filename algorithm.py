bc403c17d9bbfc433d75f55148b7283a33869742
skyepodium <skyepodium@gmail.com>
2022-02-19 10:50:52 +0900

    number-of-equivalent-domino-pairs

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,7 @@
 class Solution:
-    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        return sum([x * (x - 1) // 2 for x in Counter([tuple(sorted(do)) for do in dominoes]).values()])
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for i in range(1, n // 2 + 1):
+            if "0" in str(i) or "0" in str(n - i): continue
+
+            if i + n - i == n:
+                return [i, n - i]
