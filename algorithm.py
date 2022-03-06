f8c25f8e5e56136bf7a6e61d317d795570ea7e17
skyepodium <skyepodium@gmail.com>
2022-03-06 23:26:50 +0900

    mean-of-array-after-removing-some-elements

--- algorithm.py
+++ algorithm.py
@@ -2,23 +2,22 @@ from typing import List
 
 
 class Solution:
-    def replaceElements(self, arr: List[int]) -> List[int]:
-        # 1. init
+    def trimMean(self, arr: List[int]) -> float:
+        # 1. sort
+        arr.sort()
+
+        # 2. sub
         n = len(arr)
-        res = [-1 for _ in range(n)]
-        cur_max = arr[n-1]
+        cnt = int(n * 0.05)
+        a = arr[cnt: n-cnt]
 
-        # 2. loop
-        for i in reversed(range(n)):
-            if i == n - 1: continue
-            if arr[i+1] > cur_max: cur_max = arr[i+1]
-            res[i] = cur_max
+        return sum(a) / len(a)
 
-        return res
-
+arr = [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3]
+arr = [6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0]
+arr = [6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]
 
 sl = Solution()
-arr = [17,18,5,4,6,1]
-res = sl.replaceElements(arr)
+res = sl.trimMean(arr)
 
 print('res', res)
