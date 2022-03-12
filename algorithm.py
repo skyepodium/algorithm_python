bf5a6b40e61e6c2e0af4c06c218818ee3615d464
skyepodium <skyepodium@gmail.com>
2022-03-12 19:30:05 +0900

    duplicate-zeros

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,17 @@
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        return list((Counter(t) - Counter(s)).keys())[0]
+    def duplicateZeros(self, arr: List[int]) -> None:
+        # 1. init
+        origin = arr[::]
+        n = len(arr)
+        arr_idx = 0
+
+        # 2. loop
+        for cur in origin:
+            if arr_idx >= n: break
+
+            arr[arr_idx] = cur
+            arr_idx += 1
+
+            if cur == 0 and arr_idx < n:
+                arr[arr_idx] = cur
+                arr_idx += 1
