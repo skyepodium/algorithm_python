6e3a3e31209d576f51db8b9e7c43fdaf7ed11a76
Kim Jung yoon <skyepodium@gmail.com>
2022-03-15 17:13:18 +0900

    leetcode - min-cost-climbing-stairs
    leetcode - min-cost-climbing-stairs

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,16 @@
 class Solution:
-    def minRemoveToMakeValid(self, s: str) -> str:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
         # 1. init
-        res = ""
-        cnt = 0
+        n = len(cost)
+        cost += [0]
+        d = [int(1e7)] * (n+1)
 
-        # 2. loop
-        for c in s:
-            if c == '(':
-                cnt += 1
-                res += c
-            elif c == ')':
-                if cnt > 0:
-                    cnt -= 1
-                    res += c
-            else:
-                res += c
-
-        # 3. exception
-        origin = res
-        res = ""
-        for c in reversed(origin):
-            if c == '(' and cnt > 0:
-                cnt -= 1
-            else:
-                res += c
-
-        return res[::-1]
+        # 2. bottom up
+        d[0], d[1] = cost[0], cost[1]
+        for i in range(0, n):
+            if i + 1 <= n:
+                d[i+1] = min(d[i+1], d[i] + cost[i+1])
+            if i + 2 <= n:
+                d[i+2] = min(d[i+2], d[i] + cost[i+2])
+                
+        return d[n]
