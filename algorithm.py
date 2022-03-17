f0ef44b64d7ae834de67250b7314476fefca4f89
Kim Jung yoon <skyepodium@gmail.com>
2022-03-17 09:36:43 +0900

    leetcode - truncate-sentence
    leetcode - truncate-sentence

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,3 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # 1. init
-        n = len(cost)
-        cost += [0]
-        d = [int(1e7)] * (n+1)
-
-        # 2. bottom up
-        d[0], d[1] = cost[0], cost[1]
-        for i in range(0, n):
-            if i + 1 <= n:
-                d[i+1] = min(d[i+1], d[i] + cost[i+1])
-            if i + 2 <= n:
-                d[i+2] = min(d[i+2], d[i] + cost[i+2])
-                
-        return d[n]
+    def truncateSentence(self, s: str, k: int) -> str:
+        return " ".join(s.split(" ")[:k])
