538704bbe54b001bfc3a66dc07713c9d96c7cc8a
skyepodium <skyepodium@gmail.com>
2022-03-19 10:31:29 +0900

    leetcode - check-if-the-sentence-is-pangram/

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,13 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
+        return len(set(sentence)) == 26
+
+
+
+sl = Solution()
+sentence = "thequickbrownfoxjumpsoverthelazydog"
+sentence = "leetcode"
+
+res = sl.checkIfPangram(sentence)
+
+print('res', res)
