9cb9fed8fff312d1b7f838eefb9659425fbd3860
skyepodium <skyepodium@gmail.com>
2022-03-19 18:32:35 +0900

    leetcode - group-anagrams

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,19 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
-    def checkIfPangram(self, sentence: str) -> bool:
-        return len(set(sentence)) == 26
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # 1. init
+        d = defaultdict(list)
 
+        # 2. loop
+        for idx, s in enumerate(strs):
+            d["".join(sorted(s))].append(s)
 
+        return d.values()
 
 sl = Solution()
-sentence = "thequickbrownfoxjumpsoverthelazydog"
-sentence = "leetcode"
-
-res = sl.checkIfPangram(sentence)
+strs = ["eat","tea","tan","ate","nat","bat"]
+res = sl.groupAnagrams(strs)
 
 print('res', res)
