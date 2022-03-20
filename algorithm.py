e46c1de0352806a8fca5f9d29453e39f1923739b
skyepodium <skyepodium@gmail.com>
2022-03-20 23:47:18 +0900

    leetcode - kth-distinct-string-in-an-array

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,11 @@
-a = "ASdf"
+from collections import Counter
 
-a.lower()
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        cnt = 0
+        c = Counter(arr)
+        for key, val in c.items():
+            if val == 1: cnt += 1
+            if cnt >= k:
+                return key
+        return ""
