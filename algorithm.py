2ea053782833e6f9d130600f9d9c85a10bcf6dbc
skyepodium <skyepodium@gmail.com>
2022-03-20 11:46:11 +0900

    프로그래머스 - 신규 아이디 추천

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,3 @@
-from heapq import heappush, heappop, heapify
+a = "ASdf"
 
-
-def solution(scoville, K):
-    # 1. init
-    heapify(scoville)
-    res = 0
-
-    # 2. loop
-    while len(scoville) >= 2 and scoville[0] < K:
-        res += 1
-        f = heappop(scoville)
-        s = heappop(scoville)
-        heappush(scoville, f + s)
-
-    return res if scoville[0] >= K else -1
+a.lower()
