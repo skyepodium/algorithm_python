4025333822ad32d542a59240d6c46237a3a9e3ef
skyepodium <skyepodium@gmail.com>
2022-03-20 23:59:56 +0900

    find-target-indices-after-sorting-array

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,31 @@
 class Solution:
-    def countWords(self, words1: List[str], words2: List[str]) -> int:
-        # 1. init
-        a = Counter(words1)
-        b = Counter(words2)
-        s = set()
-        res = 0
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        # 1. sort
+        nums.sort()
+        n = len(nums)
 
-        # 2. loop
-        for key, val in a.items():
-            if val == 1:
-                s.add(key)
+        # 2. lower_bound
+        def lower_bound(t):
+            s, e = 0, n
+            while s < e:
+                mid = s + (e - s) // 2
+                if nums[mid] < t:
+                    s = mid + 1
+                else:
+                    e = mid
+            return e
 
-        for key, val in b.items():
-            if val == 1 and key in s:
-                res += 1
+        # 3. upper_bound
+        def upper_bound(t):
+            s, e = 0, n
+            while s < e:
+                mid = s + (e - s) // 2
+                if nums[mid] <= t:
+                    s = mid + 1
+                else:
+                    e = mid
+            return e
 
-        return res
+        l, r = lower_bound(target), upper_bound(target) - 1
+
+        return [l, r] if l != r else [l]
