89970c10f07cb11be71f2a4c34d8d975034c66b3
skyepodium <skyepodium@gmail.com>
2022-03-22 01:12:08 +0900

    leetcode - count-items-matching-a-rule

--- algorithm.py
+++ algorithm.py
@@ -1,39 +1,16 @@
 class Solution:
-    def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        # 1. sort
-        nums.sort()
-        n = len(nums)
-        res = []
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        # 1. init
+        d = {
+            "type": 0,
+            "color": 1,
+            "name": 2
+        }
+        res = 0
 
-        # 2. lower_bound
-        def lower_bound(t):
-            s, e = 0, n
-            while s < e:
-                mid = s + (e - s) // 2
-                if nums[mid] < t:
-                    s = mid + 1
-                else:
-                    e = mid
-            return e
-
-        # 3. upper_bound
-        def upper_bound(t):
-            s, e = 0, n
-            while s < e:
-                mid = s + (e - s) // 2
-                if nums[mid] <= t:
-                    s = mid + 1
-                else:
-                    e = mid
-            return e
-
-        # 4. check
-        l, r = lower_bound(target), upper_bound(target) - 1
-        if l == r:
-            res = [l]
-        elif l < r:
-            res = [x for x in range(l, r + 1)]
-        else:
-            res = []
+        # 2. loop
+        for i in items:
+            if i[d[ruleKey]] == ruleValue:
+                res += 1
 
         return res
