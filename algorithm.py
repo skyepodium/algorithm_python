c35050f89e8a9c3df6d58372f99fb6565f4c35ab
Kim Jung yoon <skyepodium@gmail.com>
2022-03-25 19:24:03 +0900

    leetcode - reverse-prefix-of-word

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,6 @@
 class Solution:
-    def countKDifference(self, nums: List[int], k: int) -> int:
+    def reversePrefix(self, word: str, ch: str) -> str:
         # 1. init
-        n = len(nums)
-        res = 0
+        idx = word.find(ch)
 
-        # 2. loop
-        for i in range(n):
-            for j in range(i + 1, n):
-                start = nums[i]
-                end = nums[j]
-                if abs(start - end) == k: res += 1
-
-        return res
+        return word[:idx+1][::-1] + word[idx+1:] if idx > -1 else word
