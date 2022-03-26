a62197b6ddc1c1da5fb94c17c192a8a2326c2be9
skyepodium <skyepodium@gmail.com>
2022-03-27 07:45:20 +0900

    boats-to-save-people

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,30 @@
+from typing import List
+from collections import deque
+
 class Solution:
-    def maxAscendingSum(self, nums: List[int]) -> int:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
         # 1. init
+        dq = deque(sorted(people, reverse=True))
         res = 0
-        cur = 0
-        prev = -1
 
         # 2. loop
-        for num in nums:
-            if num > prev:cur += num
-            else:
-                cur = num
-            prev = num
-
-            res = max(res, cur)
+        while dq:
+            front = dq.popleft()
+            if dq and front + dq[-1] <= limit:
+                dq.pop()
+            res += 1
 
         return res
+
+sl = Solution()
+people = [1, 2]
+limit = 3
+people = [3,2,2,1]
+limit = 3
+# people = [3,5,3,4]
+# limit = 5
+# people = [5, 1, 4, 2]
+# limit = 6
+res = sl.numRescueBoats(people, limit)
+
+print('res', res)
