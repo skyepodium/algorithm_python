f821934718b9d4d59ee776debec6f5017efe0295
skyepodium <skyepodium@gmail.com>
2022-03-28 02:29:05 +0900

    leetcode - string-to-integer-atoi

--- algorithm.py
+++ algorithm.py
@@ -1,30 +1,46 @@
-from typing import List
-from collections import deque
+import re
 
 class Solution:
-    def numRescueBoats(self, people: List[int], limit: int) -> int:
+    def myAtoi(self, s: str) -> int:
         # 1. init
-        dq = deque(sorted(people, reverse=True))
-        res = 0
+        MIN_INT = -2147483648
+        MAX_INT = 2147483647
 
-        # 2. loop
-        while dq:
-            front = dq.popleft()
-            if dq and front + dq[-1] <= limit:
-                dq.pop()
-            res += 1
+        # 2. def
+        def check(val):
+            res = int(val.strip())
+            if res < MIN_INT: res = MIN_INT
+            elif res > MAX_INT: res = MAX_INT
+            return res
 
-        return res
+        # step 1
+        a = re.findall("^ +[0-9]+", s)
+        if a:
+            return check(a[0])
+
+        # step 2
+        b = re.findall("^ *[-+][0-9]+", s)
+        if b:
+            return check(b[0])
+
+        # step 3
+        c = re.findall("^[0-9]+ *", s)
+        if c:
+            return check(c[0])
+
+        return 0
+
 
 sl = Solution()
-people = [1, 2]
-limit = 3
-people = [3,2,2,1]
-limit = 3
-# people = [3,5,3,4]
-# limit = 5
-# people = [5, 1, 4, 2]
-# limit = 6
-res = sl.numRescueBoats(people, limit)
+s = "42"
+s = "-42"
+s = "+42"
+s = "+-12"
+# s = "4193 with words"
+# s = "words and 987"
+# s = "   123 with  3"
+s = "-91283472332"
+# s = "   -42"
+res = sl.myAtoi(s)
 
 print('res', res)
