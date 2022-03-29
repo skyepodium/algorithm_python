4b918ab054e2449c4ceaee360b6ff59768d7c294
Kim Jung yoon <skyepodium@gmail.com>
2022-03-29 12:28:54 +0900

    python - dequeue
    python - dequeue

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,80 @@
-class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # 1. init
-        n = len(triangle)
-        MAX_VAL = float(2e6 + 1)
-        d = [[MAX_VAL for i in range(_+1)] for _ in range(n)]
-        d[0][0] = triangle[0][0]
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
 
-        # 2. loop
-        for i, l in enumerate(triangle):
-            if i == 0: continue
 
-            for j, t in enumerate(l):
-                if j == 0:
-                    d[i][j] = d[i-1][j] + t
-                elif j == i:
-                    d[i][j] = d[i-1][j-1] + t
-                else:
-                    d[i][j] = min(d[i-1][j-1], d[i-1][j]) + t
+class Dequeue:
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__cnt = 0
 
-        return int(min(d[n-1]))
+    def is_empty(self):
+        return self.__cnt == 0
+
+    def size(self):
+        return self.__cnt
+
+    def __increase(self):
+        self.__cnt += 1
+
+    def __decrease(self):
+        if self.__cnt > 0:
+            self.__cnt -= 1
+
+    def add_first(self, val):
+        node = Node(val)
+
+        self.__head = node
+        self.__tail = node
+
+        self.__increase()
+
+    def add_last(self, val):
+        node = Node(val)
+
+        self.__tail.next = node
+        node.prev = self.__tail
+        self.__tail = self.__tail.next
+
+        self.__increase()
+
+    def push(self, val):
+        if self.is_empty():
+            self.add_first(val)
+        else:
+            self.add_last(val)
+
+    def pop_left(self):
+        if not self.is_empty():
+            head = self.__head
+
+            if self.__head.next:
+                self.__head = self.__head.next
+
+            self.__decrease()
+
+            return head.val
+        else:
+            return None
+
+    def pop(self):
+        if not self.is_empty():
+            tail = self.__tail
+
+            if tail.prev:
+                self.__tail = self.__tail.prev
+
+            self.__decrease()
+
+            return tail.val
+        else:
+            return None
+
+    def front(self):
+        return self.__head.val
+
+    def last(self):
+        return self.__tail.val
