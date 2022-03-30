07463999dfdd48a6eb58f7b35d4653e765909d78
skyepodium <skyepodium@gmail.com>
2022-03-30 23:53:47 +0900

    leetcode - set-mismatch

--- algorithm.py
+++ algorithm.py
@@ -2,12 +2,15 @@ from typing import List
 from collections import Counter
 
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        return (Counter([x for x in range(len(nums) + 1)]) - Counter(nums)).most_common()[0][0]
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        base = Counter([x for x in range(1, len(nums) + 1)])
+        cur = Counter(nums)
 
+        return [(cur - base).most_common()[0][0], (base - cur).most_common()[0][0]]
 
 sl = Solution()
-nums = [3, 0, 1]
-res = sl.missingNumber(nums)
+nums = [1,2,2,4]
+nums = [1, 1]
+res = sl.findErrorNums(nums)
 
 print('res', res)
