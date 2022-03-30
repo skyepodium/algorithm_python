8aaab4abaeba14eaf0713c4b97343fe99b30a092
skyepodium <skyepodium@gmail.com>
2022-03-30 22:06:41 +0900

    leetcode -find-the-duplicate-number

--- algorithm.py
+++ algorithm.py
@@ -1,46 +1,15 @@
-import re
+from collections import Counter
+from typing import List
+
 
 class Solution:
-    def myAtoi(self, s: str) -> int:
-        # 1. init
-        MIN_INT = -2147483648
-        MAX_INT = 2147483647
+    def findDuplicate(self, nums: List[int]) -> int:
+        return Counter(nums).most_common()[0][0]
 
-        # 2. def
-        def check(val):
-            res = int(val.strip())
-            if res < MIN_INT: res = MIN_INT
-            elif res > MAX_INT: res = MAX_INT
-            return res
-
-        # step 1
-        a = re.findall("^ +[0-9]+", s)
-        if a:
-            return check(a[0])
-
-        # step 2
-        b = re.findall("^ *[-+][0-9]+", s)
-        if b:
-            return check(b[0])
-
-        # step 3
-        c = re.findall("^[0-9]+ *", s)
-        if c:
-            return check(c[0])
-
-        return 0
-
+nums = [ 1, 3, 4, 2, 2]
 
 sl = Solution()
-s = "42"
-s = "-42"
-s = "+42"
-s = "+-12"
-# s = "4193 with words"
-# s = "words and 987"
-# s = "   123 with  3"
-s = "-91283472332"
-# s = "   -42"
-res = sl.myAtoi(s)
+
+res = sl.findDuplicate(nums)
 
 print('res', res)
