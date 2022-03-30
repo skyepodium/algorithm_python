79bfa49323af8479f872f108b0020e7c41b0ed28
skyepodium <skyepodium@gmail.com>
2022-03-30 22:24:34 +0900

    leetcode - missing-number

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,13 @@
+from typing import List
+from collections import Counter
+
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        return Counter(nums).most_common()[0][0]
+    def missingNumber(self, nums: List[int]) -> int:
+        return (Counter([x for x in range(len(nums) + 1)]) - Counter(nums)).most_common()[0][0]
+
+
+sl = Solution()
+nums = [3, 0, 1]
+res = sl.missingNumber(nums)
+
+print('res', res)
