a4cffb548a5a0d4679722049b5986d92eceec173
Kim Jung yoon <skyepodium@gmail.com>
2022-03-31 10:41:34 +0900

    leetcode - number-of-different-integers-in-a-string
    leetcode - number-of-different-integers-in-a-string

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,3 @@
-from typing import List
-from collections import Counter
-
 class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
-        base = Counter([x for x in range(1, len(nums) + 1)])
-        cur = Counter(nums)
-
-        return [(cur - base).most_common()[0][0], (base - cur).most_common()[0][0]]
-
-sl = Solution()
-nums = [1,2,2,4]
-nums = [1, 1]
-res = sl.findErrorNums(nums)
-
-print('res', res)
+    def numDifferentIntegers(self, word: str) -> int:
+        return len(set([int(x) for x in re.split("[a-z]", word) if x != '']))
