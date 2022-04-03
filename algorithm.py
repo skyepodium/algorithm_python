a87fc18a0dcc5aaaa2186e1324b0a70ae0cb1e1f
skyepodium <skyepodium@gmail.com>
2022-04-03 18:04:31 +0900

    leetcode - number-of-segments-in-a-string

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,25 @@
-import re
-
 class Solution:
-    def countSegments(self, s: str) -> int:
-        if s.strip() == "": return 0
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        s = s.replace("-", "")[::-1].upper()
+        n = len(s)
 
-        return len(re.split(" +", s.strip()))
+        t = ""
+        for i in range(0, n, k):
+            t += s[i:i+k] + "-"
+
+        t = t[0:len(t) - 1]
+
+        return t[::-1]
+
+
 
 
 sl = Solution()
-s = "Of all the gin joints in all the towns in all the world,   "
-res = sl.countSegments(s)
+s = "5F3Z-2e-9-w"
+k = 4
+
+s = "2-5g-3-J"
+k = 2
+res = sl.licenseKeyFormatting(s, k)
 
 print('res', res)
