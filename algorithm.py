5547cd5572b705bbff2026f04600764f48d632e3
Kim Jung yoon <skyepodium@gmail.com>
2022-04-07 17:31:52 +0900

    leetcode - average-salary-excluding-the-minimum-and-maximum-salary
    leetcode - average-salary-excluding-the-minimum-and-maximum-salary

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,3 @@
-from typing import List
-
-
 class Solution:
-    def divideString(self, s: str, k: int, fill: str) -> List[str]:
-
-        return [s[i:i+k] if i+k <= len(s) else s[i:i+k] + fill * (i+k - len(s)) for i in range(0, len(s), k)]
-
-sl = Solution()
-s = "abcdefghi"
-k = 3
-fill = "x"
-
-s = "abcdefghij"
-k = 3
-fill = "x"
-res = sl.divideString(s, k, fill)
-
-print('res', res)
+    def average(self, salary: List[int]) -> float:
+        return (sum(salary) - max(salary) - min(salary)) / (len(salary) - 2)
