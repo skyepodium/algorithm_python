4f43a99ebe631d7ad3ebeea34a8d8ef0f5dc6d7f
skyepodium <skyepodium@gmail.com>
2022-04-09 23:30:24 +0900

    프로그래머스 - 소수 찾기

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,20 @@
-def solution(arr):
-    s = []
+def solution(n):
+    # 1. init
+    d = [1 for _ in range(n+1)]
 
-    for a in arr:
-        if len(s) == 0 or s[-1] != a:
-            s.append(a)
+    def eratos():
+        for i in range(2, int(n ** 0.5) + 1):
+            for j in range(i*2, n+1, i):
+                d[j] = 0
 
-    return s
+    d[0], d[1] = 0, 0
+    eratos()
+
+    return sum(d)
+
+
+n = 10
+n = 5
+res = solution(n)
+
+print('res', res)
