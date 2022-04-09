a639cc06ce3f6c678a1ccbbce56fbfa61b1448ff
skyepodium <skyepodium@gmail.com>
2022-04-09 17:44:49 +0900

    leetcode - longest-increasing-subsequence

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,25 @@
 from typing import List
-from collections import Counter
 
 
 class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        return [a for a, b in sorted(Counter(words).items(), key=lambda x: -x[1])][:k]
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # 1. init
+        n = len(nums)
+        d = [1 for _ in range(n)]
+
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i] and d[j] + 1 > d[i]:
+                    d[i] = d[j] + 1
+
+        return max(d)
+
+
 
 sl = Solution()
-nums = [1,1,1,2,2,3]
-k = 2
-nums = [1]
-k = 1
-res = sl.topKFrequent(nums, k)
+nums = [10,9,2,5,3,7,101,18]
+nums = [0,1,0,3,2,3]
+nums = [7,7,7,7,7,7,7]
+res = sl.lengthOfLIS(nums)
 
 print('res', res)
