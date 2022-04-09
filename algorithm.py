9bbeec523fe911290a87b8638a4bbb1c10031d47
skyepodium <skyepodium@gmail.com>
2022-04-09 23:47:01 +0900

    정수 제곱근 판별

--- algorithm.py
+++ algorithm.py
@@ -1,20 +1,5 @@
-def solution(n):
-    # 1. init
-    d = [1 for _ in range(n+1)]
+a = [1, 2, 3, 4, 5]
 
-    def eratos():
-        for i in range(2, int(n ** 0.5) + 1):
-            for j in range(i*2, n+1, i):
-                d[j] = 0
+res = sum(a)
 
-    d[0], d[1] = 0, 0
-    eratos()
-
-    return sum(d)
-
-
-n = 10
-n = 5
-res = solution(n)
-
-print('res', res)
+print(res) # 15
