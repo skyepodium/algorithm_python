5249314825944f7fb60a701b92f56f08ec247c89
skyepodium <skyepodium@gmail.com>
2022-04-10 13:18:38 +0900

    leetcode - Baseball Game

--- algorithm.py
+++ algorithm.py
@@ -1,70 +1,35 @@
-from collections import deque
 from typing import List
 
-
 class Solution:
-    def largestIsland(self, grid: List[List[int]]) -> int:
+    def calPoints(self, ops: List[str]) -> int:
         # 1. init
-        n = len(grid)
-        check = [[-1 for _ in range(n)] for _ in range(n)]
-        d = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        s = []
+        sum_val = 0
 
-        # 2. bfs
-        def bfs(start_x, start_y, idx):
-            check[start_x][start_y] = idx
-            q = deque()
-            q.append((start_x, start_y))
-            cnt = 0
-
-            while q:
-                x, y = q.popleft()
-                cnt += 1
-                for dx, dy in d:
-                    nx, ny = x + dx, y + dy
-                    if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                        continue
-                    if check[nx][ny] == -1 and grid[nx][ny] == 1:
-                        check[nx][ny] = idx
-                        q.append((nx, ny))
-            return cnt
-
-        # 3. loop
-        idx = 0
-        cnt_dict = {}
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1 and check[i][j] == -1:
-                    cnt = bfs(i, j, idx)
-                    cnt_dict[idx] = cnt
-                    idx += 1
-        print(cnt_dict)
-        # 4. loop
-        res = 0
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    cur_cnt = 0
-                    seen = set()
-                    for dx, dy in d:
-                        nx, ny = i + dx, j + dy
-                        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                            continue
-                        print(nx, ny)
-
-                        if check[nx][ny] > -1 and check[nx][ny] not in seen:
-                            n_idx = check[nx][ny]
-                            n_cnt = cnt_dict[n_idx]
-                            cur_cnt += n_cnt
-                            seen.add(n_idx)
-                    res = max(res, cur_cnt + 1)
-
-        return res if res > 0 else n * n
-
+        # 2. loop
+        for o in ops:
+            if o == 'C':
+                if len(s) > 0:
+                    sum_val -= s[-1]
+                    s.pop()
+            elif o == 'D':
+                val = s[-1] * 2
+                sum_val += val
+                s.append(val)
+            elif o == '+':
+                val = s[-1] + s[-2]
+                sum_val += val
+                s.append(val)
+            else:
+                val = int(o)
+                sum_val += val
+                s.append(val)
+        return sum_val
 
 sl = Solution()
 
-grid = [[1,0],[0,1]]
+ops = ["5","-2","4","C","D","9","+","+"]
 
-res = sl.largestIsland(grid)
+res = sl.calPoints(ops)
 
 print('res', res)
