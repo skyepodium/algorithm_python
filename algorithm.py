17af85eed8c747582d84d8b8e1d1b6be3ac7c87c
Kim Jung yoon <skyepodium@gmail.com>
2022-04-12 18:00:56 +0900

    leetcode - game-of-life
    leetcode - game-of-life

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,30 @@
 class Solution:
-    def checkTree(self, root: Optional[TreeNode]) -> bool:
-        return root.val == root.left.val + root.right.val        
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        # 1. init
+        n = len(board)
+        m = len(board[0])
+        d = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+        a = [[0 for _ in range(m)] for _ in range(n)]
+        
+        # 2. loop
+        for i in range(n):
+            for j in range(m):
+                cur = board[i][j]
+                cnt = 0
+                
+                for dx, dy in d:
+                    nx, ny = i + dx, j + dy
+                    if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
+                        
+                    ne = board[nx][ny]
+                    if ne == 1: cnt += 1
+                
+                if cur == 1:
+                    if 2 <= cnt <= 3: a[i][j] = 1
+                else:
+                    if cnt == 3: a[i][j] = 1
+        
+        # 3. copy
+        for i in range(n):
+            for j in range(m):
+                board[i][j] = a[i][j]
