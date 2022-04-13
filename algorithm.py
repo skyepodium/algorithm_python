8af3ee69d1ef0b7c774639b082fc69373209d4ea
Kim Jung yoon <skyepodium@gmail.com>
2022-04-13 10:31:15 +0900

    leetcode - regular-expression-matching
    leetcode - regular-expression-matching

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,4 @@
 class Solution:
-    def countPairs(self, nums: List[int], k: int) -> int:
-        # 1. init
-        n = len(nums)
-        res = 0
+    def isMatch(self, s: str, p: str) -> bool:
 
-        # 2. loop
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] == nums[j] and (i * j) % k == 0: res += 1
-
-        return res
+        return re.fullmatch(re.sub("\\.", "[a-z]", p), s) is not None
