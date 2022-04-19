baa0ea94a2409c1558d5924f577be3ae5694ce3b
skyepodium <skyepodium@gmail.com>
2022-04-20 02:21:08 +0900

    leetcode - find-greatest-common-divisor-of-array

--- algorithm.py
+++ algorithm.py
@@ -1,29 +1,10 @@
-from typing import List
-from collections import defaultdict
-
 class Solution:
-    def destCity(self, paths: List[List[str]]) -> str:
-        # 1. init
-        d = defaultdict(str)
+    def findGCD(self, nums: List[int]) -> int:
 
-        # 2. make graph
-        for s, e in paths:
-            d[s] = e
+        def gcd(a, b):
+            if b == 0:
+                return a
+            else:
+                return gcd(b, a % b)
 
-        # 3. search
-        node = paths[0][0]
-        while d[node]:
-            n_node = d[node]
-            node = n_node
-
-        return node
-
-
-sl = Solution()
-
-paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
-paths = [["B","C"],["D","B"],["C","A"]]
-paths = [["A","Z"]]
-res = sl.destCity(paths)
-
-print('res', res)
+        return gcd(min(nums), max(nums))
