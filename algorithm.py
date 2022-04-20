7a938af3554e86ab56dbf5737a460de7fad19d78
skyepodium <skyepodium@gmail.com>
2022-04-20 14:31:05 +0900

    leetcode - number-of-valid-words-in-a-sentence

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,26 @@
+import re
+
 class Solution:
-    def mostWordsFound(self, sentences: List[str]) -> int:
+    def countValidWords(self, sentence: str) -> int:
+        # 1. init
         res = 0
 
-        for s in sentences:
-            res = max(res, len(s.split(" ")))
+        # 2. loop
+        for r in re.split(" +", sentence):
+            if re.fullmatch("[a-z]+", r) and r.lower() == r:
+                res += 1
+            elif re.fullmatch("[a-z]+-[a-z]+[\\.!, ]*", r):
+                res += 1
+            elif re.fullmatch("[a-z]*[\\.!, ]", r):
+                res += 1
 
         return res
+
+sl = Solution()
+sentence = "cat and  dog"
+# sentence = "!this  1-s b8d!"
+# sentence = "alice and  bob are playing stone-game10"
+# sentence = "he bought 2 pencils, 3 erasers, and 1  pencil-sharpener."
+res = sl.countValidWords(sentence)
+
+print('res', res)
