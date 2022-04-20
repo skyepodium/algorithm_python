1c78c09d8aa86d781b8398034aa5a904927a1f0c
Kim Jung yoon <skyepodium@gmail.com>
2022-04-20 18:07:43 +0900

    base64
    base64

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,55 @@
-import re
+import base64
 
-class Solution:
-    def countValidWords(self, sentence: str) -> int:
-        # 1. init
-        res = 0
+class Base64:
 
-        # 2. loop
-        for r in re.split(" +", sentence):
-            if re.fullmatch("[a-z]+", r) and r.lower() == r:
-                res += 1
-            elif re.fullmatch("[a-z]+-[a-z]+[\\.!, ]*", r):
-                res += 1
-            elif re.fullmatch("[a-z]*[\\.!, ]", r):
-                res += 1
+    def __init__(self):
+        self.table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
-        return res
+    def encode(self, s:str):
 
-sl = Solution()
-sentence = "cat and  dog"
-# sentence = "!this  1-s b8d!"
-# sentence = "alice and  bob are playing stone-game10"
-# sentence = "he bought 2 pencils, 3 erasers, and 1  pencil-sharpener."
-res = sl.countValidWords(sentence)
+        binary_list = self.__str_to_binary_list(s)
 
-print('res', res)
+        binary_str = "".join(binary_list)
+
+        hex_list = self.__binary_str_to_hext_list(binary_str)
+
+        return "".join(self.__hex_list_to_ascii_list__(hex_list))
+
+    def decode(self):
+        return
+
+    def __binary_str_to_hext_list(self, binary_str):
+        hex_list = []
+        for i in range(0, len(binary_str), 6):
+            # hex_list.append(binary_str[i:i+6])
+            hex_list.append(format(binary_str[i:i+6]).zfill(8))
+        # print(hex_list)
+        return hex_list
+
+    def __hex_list_to_ascii_list__(self, hex_list):
+        return [self.table[self.__binary_to_int(hex)] for hex in hex_list]
+
+    def __str_to_binary_list(self, s:str):
+        return [self.__int_to_binary__(ord(c)) for c in s]
+
+    def __int_to_binary__(self, n:int):
+        return "{0:b}".format(n).zfill(8)
+
+    def __binary_to_int(self, b):
+        return int(b, 2)
+
+
+b = Base64()
+
+s = "webisfree"
+s = "E1L"
+s = "a"
+
+sitename_bytes = s.encode('ascii')
+sitename_base64 = base64.b64encode(sitename_bytes)
+
+print("res", sitename_base64)
+print("myres", b.encode(s))
+
+# sitename_base64_str = sitename_base64.decode('ascii')
+# print('sitename_base64_str', sitename_base64_str)
