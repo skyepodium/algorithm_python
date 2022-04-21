063fbce5c2524c811839f76326860044e4a92c45
skyepodium <skyepodium@gmail.com>
2022-04-22 00:14:17 +0900

    leetcode - final-value-of-variable-after-performing-operations

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,13 @@
-import re
-
 class Solution:
-    def areNumbersAscending(self, s: str) -> bool:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        # 1. init
+        res = 0
 
-        s = re.sub("[^0-9 ]", "", s).strip()
-        s_list = [int(x) for x in re.split(" +", s)]
+        # 2. loop
+        for o in operations:
+            if o == '++X' or o == 'X++':
+                res += 1
+            else:
+                res -= 1
 
-        prev = -1
-        for c in s_list:
-            if not prev < c: return False
-            prev = c
-
-        return True
-
-
-sl = Solution()
-s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles"
-s = "hello world 5 x 5"
-res = sl.areNumbersAscending(s)
-
-print('res', res)
-
+        return res
