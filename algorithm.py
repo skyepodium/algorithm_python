320d572c404ae0ba747a4ab4927d9dcaacb278bf
skyepodium <skyepodium@gmail.com>
2022-04-22 16:02:43 +0900

    leetcode - check-if-every-row-and-column-contains-all-numbers

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,33 @@
+from typing import List
+
+
 class Solution:
-    def finalValueAfterOperations(self, operations: List[str]) -> int:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
         # 1. init
-        res = 0
+        n = len(matrix)
 
         # 2. loop
-        for o in operations:
-            if o == '++X' or o == 'X++':
-                res += 1
-            else:
-                res -= 1
+        for a in matrix:
+            s = set()
+            for b in a:
+                if b < 1 or b > n: return False
+                if b in s: return False
+                s.add(b)
 
-        return res
+        for j in range(n):
+            s = set()
+            for i in range(n):
+                b = matrix[i][j]
+                if b < 1 or b > n: return False
+                if b in s: return False
+                s.add(b)
+
+        return True
+
+
+sl = Solution()
+matrix = [[1,2,3],[3,1,2],[2,3,1]]
+matrix = [[1,1,1],[1,2,3],[1,2,3]]
+res = sl.checkValid(matrix)
+
+print('res', res)
