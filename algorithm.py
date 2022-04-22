29ad488b0176d8d97f36a7d72416997fe4237791
skyepodium <skyepodium@gmail.com>
2022-04-22 16:46:46 +0900

    leetcode - keep-multiplying-found-values-by-two

--- algorithm.py
+++ algorithm.py
@@ -2,32 +2,18 @@ from typing import List
 
 
 class Solution:
-    def checkValid(self, matrix: List[List[int]]) -> bool:
-        # 1. init
-        n = len(matrix)
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        s = set(nums)
 
-        # 2. loop
-        for a in matrix:
-            s = set()
-            for b in a:
-                if b < 1 or b > n: return False
-                if b in s: return False
-                s.add(b)
+        while original in s:
+            original *= 2
 
-        for j in range(n):
-            s = set()
-            for i in range(n):
-                b = matrix[i][j]
-                if b < 1 or b > n: return False
-                if b in s: return False
-                s.add(b)
-
-        return True
+        return original
 
 
 sl = Solution()
-matrix = [[1,2,3],[3,1,2],[2,3,1]]
-matrix = [[1,1,1],[1,2,3],[1,2,3]]
-res = sl.checkValid(matrix)
+nums = [1,16,13,19,12,10]
+original = 2
+res = sl.findFinalValue(nums, original)
 
 print('res', res)
