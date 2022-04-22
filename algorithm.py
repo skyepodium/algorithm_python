c275521375c7b0900bf7dacb34cb224a687133c6
skyepodium <skyepodium@gmail.com>
2022-04-23 01:57:17 +0900

    leetcode - thousand-separator

--- algorithm.py
+++ algorithm.py
@@ -1,40 +1,13 @@
-def solution(s):
-    # 1. init
-    n = len(s)
-    res = n
+import re
 
-    # 2. loop
-    for i in range(1, n//2 + 1):
-        t = ""
-        prev = ""
-        cnt = 1
-        is_append = False
-        for j in range(0, n, i):
-            cur = s[j:j+i]
-            if cur == prev:
-                cnt += 1
-                is_append = False
-            else:
-                t += f"{cnt}{prev}" if cnt >= 2 else prev
-                prev = cur
-                cnt = 1
-                is_append = True
-        if not is_append:
-            t += f"{cnt}{prev}" if cnt >= 2 else prev
-        else:
-            t += cur
-
-        res = min(res, len(t))
-
-    return res
+class Solution:
+    def thousandSeparator(self, n: int) -> str:
+        return format(n, ",").replace(",", ".")
 
 
-s = "aabbaccc"
-s = "ababcdcdababcdcd"
-# s = "abcabcdede"
-# s = "abcabcabcabcdededededede"
-# s = "xababcdcdababcdcd"
+sl = Solution()
 
-res = solution(s)
+n = 10000
+res = sl.thousandSeparator(n)
 
 print('res', res)
