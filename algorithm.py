9e7e9d914a97db41776eb97e34f7486f5061a694
skyepodium <skyepodium@gmail.com>
2022-04-23 12:05:56 +0900

    leetcode - https://leetcode.com/problems/goat-latin/

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,26 @@
-import re
-
 class Solution:
-    def thousandSeparator(self, n: int) -> str:
-        return format(n, ",").replace(",", ".")
+    def toGoatLatin(self, sentence: str) -> str:
+        # 1. init
+        vowel = ['a', 'e', 'i', 'o', 'u']
+        v = set(vowel)
+        s = sentence.split(" ")
+        n = len(s)
+
+        # 2. loop
+        for i in range(n):
+            cur = s[i]
+            if cur[0].lower() in v:
+                s[i] += "ma"
+            else:
+                s[i] = s[i][1:] + s[i][0] + "ma"
+            s[i] += "a" * (i+1)
+
+        return " ".join(s)
 
 
 sl = Solution()
-
-n = 10000
-res = sl.thousandSeparator(n)
+sentence = "I speak Goat Latin"
+# sentence = "The quick brown fox jumped over the lazy dog"
+res = sl.toGoatLatin(sentence)
 
 print('res', res)
