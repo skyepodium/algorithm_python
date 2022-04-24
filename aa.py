455f71d48a41cd639eb4d39d730bf794c987b3a4
skyepodium <skyepodium@gmail.com>
2022-04-24 16:35:25 +0900

    [1차] 프렌즈4블록

--- aa.py
+++ aa.py
@@ -1,44 +1,8 @@
-import hashlib
-from itertools import chain
+# uncompyle6 version 3.5.0
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 2.7.5 (default, Nov 16 2020, 22:23:17) 
+# [GCC 4.8.5 20150623 (Red Hat 4.8.5-44)]
+# Embedded file name: out.py
+# Compiled at: 2022-04-09 05:54:01
 
-probably_public_bits = [.    
-    'root',  # username
-    'flask.app',  # modname 고정
-    'Flask',  # getattr(app, '__name__', getattr(app.__class__, '__name__')) 고정
-    '/usr/local/lib/python3.6/dist-packages/flask/app.py'  # getattr(mod, '__file__', None),
-    # python 버전 마다 위치 다름
-]
 
-private_bits = [
-    '2485378547714',  # MAC주소를 int형으로 변환한 값,
-    'b875f1295ae64ab190c0ae07a6134578'  # get_machine_id()
-]
-
-h = hashlib.md5()
-for bit in chain(probably_public_bits, private_bits):
-    if not bit:
-        continue
-    if isinstance(bit, str):
-        bit = bit.encode('utf-8')
-    h.update(bit)
-h.update(b'cookiesalt')
-# h.update(b'shittysalt')
-
-cookie_name = '__wzd' + h.hexdigest()[:20]
-
-num = None
-if num is None:
-    h.update(b'pinsalt')
-    num = ('%09d' % int(h.hexdigest(), 16))[:9]
-
-rv = None
-if rv is None:
-    for group_size in 5, 4, 3:
-        if len(num) % group_size == 0:
-            rv = '-'.join(num[x:x + group_size].rjust(group_size, '0')
-                          for x in range(0, len(num), group_size))
-            break
-    else:
-        rv = num
-
-print(rv)
--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,83 @@
-class Solution:
-    def toGoatLatin(self, sentence: str) -> str:
-        # 1. init
-        vowel = ['a', 'e', 'i', 'o', 'u']
-        v = set(vowel)
-        s = sentence.split(" ")
-        n = len(s)
+def solution(m, n, board):
+    # 1. init
+    n, m = m, n
+    res = 0
+    board = [list(x) for x in board]
+    check = [[False for _ in range(m)] for _ in range(n)]
+    d = [(0, 0), (0, 1), (1, 0), (1, 1)]
 
-        # 2. loop
+    # 2. search
+    def search():
+        cnt = 0
+        for i in range(n-1):
+            for j in range(m-1):
+                prev = board[i][j]
+                if prev == "": continue
+
+                is_same = True
+                for dx, dy in d:
+                    nx, ny = i + dx, j + dy
+                    cur = board[nx][ny]
+                    if prev != cur:
+                        is_same = False
+                        break
+
+                if not is_same: continue
+
+                for dx, dy in d:
+                    nx, ny = i + dx, j + dy
+                    if check[nx][ny]: continue
+                    check[nx][ny] = True
+                    cnt += 1
+
+        return cnt
+
+    # 3. move
+    def move():
+        for j in range(m):
+            for idx in range(n-1, -1, -1):
+                i = idx
+                if board[i][j] == "" or check[i][j]: continue
+
+                while i < n-1 and board[i][j] != "" and board[i+1][j] == "":
+                    board[i+1][j] = board[i][j]
+                    board[i][j] = ""
+                    i += 1
+
+    # 4. clear
+    def clear():
         for i in range(n):
-            cur = s[i]
-            if cur[0].lower() in v:
-                s[i] += "ma"
-            else:
-                s[i] = s[i][1:] + s[i][0] + "ma"
-            s[i] += "a" * (i+1)
+            for j in range(m):
+                if check[i][j]:
+                    board[i][j] = ""
 
-        return " ".join(s)
+    # 5. loop
+    while True:
+        # 1) init
+        check = [[False for _ in range(m)] for _ in range(n)]
+
+        # 2) search
+        search_cnt = search()
+        if search_cnt == 0: break
+        res += search_cnt
+
+        # 3) clear
+        clear()
+
+        # 4) move
+        move()
+
+    return res
 
 
-sl = Solution()
-sentence = "I speak Goat Latin"
-# sentence = "The quick brown fox jumped over the lazy dog"
-res = sl.toGoatLatin(sentence)
+m = 4
+n = 5
+board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
+
+# m = 6
+# n = 6
+# board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
+
+res = solution(m, n, board)
 
 print('res', res)
