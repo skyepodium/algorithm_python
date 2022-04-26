1bb939a837a46e65b2c0fb7b8c2820366f6b3eca
skyepodium <skyepodium@gmail.com>
2022-04-26 13:17:43 +0900

    프로그래머스 - 야근지수

--- algorithm.py
+++ algorithm.py
@@ -1,64 +1,24 @@
-from collections import defaultdict
+from heapq import heappush, heappop
 
 
-def solution(fees, records):
-    # 1. init
-    base_minute, base_fee, per_minute, per_fee = fees
-    p = defaultdict(list)
-    remain = defaultdict(int)
-    res = defaultdict(int)
+def solution(n, works):
+    # 1. pq
+    pq = []
+    for w in works:
+        heappush(pq, -w)
 
-    # 2. cal_diff
-    def cal_diff(before, cur):
-        before_hour, before_minute = before
-        cur_hour, cur_minute = cur
-        hour_diff = cur_hour - before_hour
-        minute_diff = cur_minute - before_minute
+    # 2. loop
+    while n > 0 and pq:
+        top = heappop(pq)
+        if -top >= 2:
+            heappush(pq, -(-top-1))
+        n -= 1
 
-        if minute_diff < 0:
-            hour_diff -= 1
-            minute_diff += 60
+    # 3. sum
+    return sum([x*x for x in pq])
 
-        return hour_diff * 60 + minute_diff
+works = [4, 3, 3]
+n = 4
 
-    # 3. cal_fee
-    def cal_fee(diff):
-        res = base_fee
-
-        if diff > base_minute:
-            m_diff = diff - base_minute
-            a = m_diff // per_minute
-            if m_diff % per_minute != 0: a += 1
-            res += a * per_fee
-
-        return res
-
-    # 4. loop
-    for r in records:
-        t, num, cmd = r.split(" ")
-        cur_time = [int(x) for x in t.split(":")]
-        if cmd == "IN":
-            p[num] = cur_time
-        else:
-            remain[num] += cal_diff(p[num], cur_time)
-            p.pop(num, None)
-
-    # 5. sum time
-    for r in p.items():
-        num, t = r
-        remain[num] += cal_diff(t, [23, 59])
-
-    # 6. total fee
-    for num, val in remain.items():
-        res[num] = cal_fee(val)
-
-    return [fee for num, fee in sorted(res.items(), key=lambda x: (x[0], [1]))]
-
-
-fees = [180, 5000, 10, 600]
-records = ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
-# records = ["05:34 5961 IN", "07:59 5961 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
-
-res = solution(fees, records)
-
+res = solution(n, works)
 print('res', res)
