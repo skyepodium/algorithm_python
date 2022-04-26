0fd0676be6b70a534dc5b1fad651326d553e6315
skyepodium <skyepodium@gmail.com>
2022-04-26 14:40:33 +0900

    프로그래머스 - 보석 쇼핑

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,42 @@
-from heapq import heappush, heappop
+from collections import Counter
 
 
-def solution(n, works):
-    # 1. pq
-    pq = []
-    for w in works:
-        heappush(pq, -w)
+def solution(gems):
+    # 1. init
+    n = len(gems)
+    total_cnt = len(set(gems))
+    c = Counter()
+    res = [0, n-1]
 
-    # 2. loop
-    while n > 0 and pq:
-        top = heappop(pq)
-        if -top >= 2:
-            heappush(pq, -(-top-1))
-        n -= 1
+    # 2. two pointer
+    s = 0
+    for e, cur in enumerate(gems):
+        # 1) cnt update
+        c[cur] += 1
 
-    # 3. sum
-    return sum([x*x for x in pq])
+        # 2) minify
+        while len(c) >= total_cnt:
+            if e - s < res[1] - res[0]:
+                res[0], res[1] = s, e
 
-works = [4, 3, 3]
-n = 4
+            s_val = gems[s]
+            c[s_val] -= 1
+            if c[s_val] == 0:
+                c.pop(s_val)
+            s += 1
 
-res = solution(n, works)
+    return [x+1 for x in res]
+
+gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+
+gems = ["AA", "AB", "AC", "AA", "AC"]
+
+gems = ["XYZ", "XYZ", "XYZ"]
+
+gems = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
+
+
+
+res = solution(gems)
+
 print('res', res)
