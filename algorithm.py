7fd2e94ee0647f552c0b565a5c7c6c5823cacf79
Kim Jung yoon <skyepodium@gmail.com>
2022-04-27 12:23:58 +0900

    프로그래머스 - 경주로 건설
    프로그래머스 - 경주로 건설

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,39 @@
-def solution(n, times):
+from heapq import heappush, heappop
+
+def solution(board):
     # 1. init
-    max_val = max(times)
-    res = max_val * n
-    s, e = 0, max_val * n
+    n = len(board)
+    m = len(board[0])
+    MAX_VAL = n * m * 500
+    d = [[[MAX_VAL for _ in range(4)] for _ in range(m)] for _ in range(n)]
+    dir_list = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+    # 0 - left, 1 - right, 2 - down, 3 - up
 
-    # 2. cal cnt
-    def cal_cnt(a):
-        r = 0
-        for t in times:
-            r += a // t
-        return r
+    # 2. dijkstra
+    def dijkstra(start_x, start_y):
+        pq = []
 
-    # 3. binary search
-    while s < e:
-        mid = s + (e - s) // 2
+        for c_dir in range(4):
+            d[start_x][start_y][c_dir] = 0
+            heappush(pq, (0, start_x, start_y, c_dir))
 
-        cnt = cal_cnt(mid)
+        while pq:
+            cost, x, y, dir = heappop(pq)
 
-        if cnt < n:
-            s = mid + 1
-        else:
-            res = min(res, mid)
-            e = mid
+            for n_dir_idx, n_dir in enumerate(dir_list):
+                dx, dy = n_dir
+                nx = x + dx
+                ny = y + dy
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if board[nx][ny] == 1: continue
 
-    return res
+                n_cost = 100 if dir == n_dir_idx else 600
+
+                if d[nx][ny][n_dir_idx] > d[x][y][dir] + n_cost:
+                    d[nx][ny][n_dir_idx] = d[x][y][dir] + n_cost
+                    heappush(pq, (d[nx][ny][n_dir_idx], nx, ny, n_dir_idx))
+
+    dijkstra(0, 0)
+
+    return min(d[n-1][m-1])
