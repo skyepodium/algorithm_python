d3a75446bd9f3a30d058706016026f378d5dba37
skyepodium <skyepodium@gmail.com>
2022-05-01 00:25:40 +0900

    프로그래머스 - 도둑질

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,25 @@
-from itertools import permutations
-from typing import List
+from collections import defaultdict
+
+def solution(money):
+    # 1. init
+    n = len(money)
+    a = [0 for _ in range(n)]
+    b = [0 for _ in range(n)]
+    a[0] = money[0]
+
+    for i in range(1, n-1):
+        a[i] = max(a[i-2] + money[i], a[i-1])
+
+    for i in range(1, n):
+        b[i] = max(b[i-2] + money[i], b[i-1])
+
+    return max(a[n-2], b[n-1])
 
 
-class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        s = set()
-        res = []
+money = [1, 2, 3, 1]
+money = [10000, 3, 7, 4, 5]
+money = [1, 100, 1, 100, 1]
 
-        for x in permutations(nums):
-            key = "".join([str(a) for a in x])
-            if key not in s:
-                s.add(key)
-                res.append(x)
-
-        return res
-
-
-
-sl = Solution()
-
-nums = [1, 1, 2]
-res = sl.permuteUnique(nums)
+res = solution(money)
 
 print('res', res)
