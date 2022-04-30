44bd6d72604321ba90daa8513628941af8141bc7
skyepodium <skyepodium@gmail.com>
2022-04-30 19:08:01 +0900

    permutations-ii

--- algorithm.py
+++ algorithm.py
@@ -1,39 +1,25 @@
+from itertools import permutations
 from typing import List
 
 
 class Solution:
-    def specialArray(self, nums: List[int]) -> int:
-        # 1. init
-        max_val = max(nums)
-        res = -1
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        s = set()
+        res = []
 
-        # 2. cal cnt
-        def cal_cnt(mid):
-            return sum([1 for x in nums if x >= mid])
-
-        # 3. sort
-        nums.sort()
-
-        # 4. binary search
-        s, e = 0, max_val
-        while s <= e:
-            mid = s + (e - s) // 2
-            cnt = cal_cnt(mid)
-            if cnt > mid:
-                s = mid + 1
-            elif cnt == mid:
-                return mid
-            else:
-                e = mid - 1
+        for x in permutations(nums):
+            key = "".join([str(a) for a in x])
+            if key not in s:
+                s.add(key)
+                res.append(x)
 
         return res
 
+
+
 sl = Solution()
 
-nums = [3,5]
-# nums = [0,0]
-# nums = [0,4,3,0,4]
-
-res = sl.specialArray(nums)
+nums = [1, 1, 2]
+res = sl.permuteUnique(nums)
 
 print('res', res)
