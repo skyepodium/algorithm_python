eb49a49ca2512a1aae7ee84c185bf830c398ac9f
skyepodium <skyepodium@gmail.com>
2022-05-02 18:15:48 +0900

    leetcode - sort-array-by-parity

--- algorithm.py
+++ algorithm.py
@@ -1,32 +1,9 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        # 0. exception
-        if len(s) < 2 or s == s[::-1]: return s
+a = "a"
 
-        # 1. init
-        res = ""
+print(a.isalpha())
+# True
 
-        # 2. get_max_len
-        def get_max_len(l, r):
-            while l >= 0 and r <= len(s) and s[l] == s[r-1]:
-                l -= 1
-                r += 1
+b = "0"
 
-            return s[l+1:r-1]
-
-        # 3. loop
-        for i in range(len(s)):
-            res = max(res,
-                      get_max_len(i, i+1),
-                      get_max_len(i, i+2),
-                      key=len)
-
-        return res
-
-s = "babad"
-
-sl = Solution()
-
-res = sl.longestPalindrome(s)
-
-print('res', res)
+print(b.isdigit())
+# True
