057da93883303f29a991c6a1e8334aa4f4bbd3b7
skyepodium <skyepodium@gmail.com>
2022-05-04 16:19:22 +0900

    leetcode - check-if-it-is-a-straight-line

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,36 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        s = str(x)
+from typing import List
 
-        return s == s[::-1]
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        # 1. init
+        n = len(coordinates)
+
+        # 2. ccw
+        def ccw(r, p, q):
+            first = (p[0] - r[0]) * (q[1] - r[1])
+            second = (p[1] - r[1]) * (q[0] - r[0])
+            result = first - second
+
+            if result > 0:
+                return 1
+            elif result == 0:
+                return 0
+            else:
+                return -1
+
+        # 3. loop
+        for i in range(n-2):
+            if ccw(coordinates[i], coordinates[i+1], coordinates[i+2]) != 0:
+                return False
+
+        return True
+
+
+sl = Solution()
+
+coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+# coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+res = sl.checkStraightLine(coordinates)
+
+print('res', res)
