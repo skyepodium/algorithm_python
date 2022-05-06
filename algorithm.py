8d1cbd441f73db43eec8ba427ccd8e35af73ab8e
skyepodium <skyepodium@gmail.com>
2022-05-07 00:45:17 +0900

    leetcode - max-number-of-k-sum-pairs

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,38 @@
+from typing import List
+
+
 class Solution:
-    def removeDuplicates(self, s: str) -> str:
+    def maxOperations(self, nums: List[int], k: int) -> int:
         # 1. init
-        st = []
+        n = len(nums)
+        res = 0
 
-        # 2. loop
-        for c in s:
-            if st and st[-1] == c:
-                while st and st[-1] == c:
-                    st.pop()
+        # 2. sort
+        nums.sort()
+
+        # 3. two pointer
+        l, r = 0, n - 1
+        while l < r:
+            cur = nums[l] + nums[r]
+            if cur == k:
+                res += 1
+                l += 1
+                r -= 1
+            elif cur < k:
+                l += 1
             else:
-                st.append(c)
+                r -= 1
 
-        return "".join(st)
+        return res
+
+sl = Solution()
+
+nums = [1,2,3,4]
+k = 5
+nums = [3,1,3,4,3]
+k = 6
 
 
+res = sl.maxOperations(nums, k)
+
+print('res', res)
