e1f486eca81934a139e4c834bd2fbbb4d79c8ca6
skyepodium <skyepodium@gmail.com>
2022-05-06 22:08:01 +0900

    leetcode - remove-all-adjacent-duplicates-in-string-ii

--- algorithm.py
+++ algorithm.py
@@ -1,36 +1,37 @@
-from typing import List
-
-
 class Solution:
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+    def removeDuplicates(self, s: str, k: int) -> str:
         # 1. init
-        n = len(coordinates)
+        st = []
 
-        # 2. ccw
-        def ccw(r, p, q):
-            first = (p[0] - r[0]) * (q[1] - r[1])
-            second = (p[1] - r[1]) * (q[0] - r[0])
-            result = first - second
-
-            if result > 0:
-                return 1
-            elif result == 0:
-                return 0
+        # 2. loop
+        for c in s:
+            if st:
+                t, cnt = st[-1]
+                if t == c:
+                    if cnt == k - 1:
+                        while st and st[-1][0] == c:
+                            st.pop()
+                    else:
+                        st.append((c, cnt + 1))
+                else:
+                    st.append((c, 1))
             else:
-                return -1
+                st.append((c, 1))
 
-        # 3. loop
-        for i in range(n-2):
-            if ccw(coordinates[i], coordinates[i+1], coordinates[i+2]) != 0:
-                return False
+        return "".join([t for t, _ in st])
 
-        return True
+s = "abcd"
+k = 2
 
+s = "deeedbbcccbdaa"
+k = 3
+
+s = "pbbcggttciiippooaais"
+k = 2
 
 sl = Solution()
 
-coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
-# coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
-res = sl.checkStraightLine(coordinates)
+res = sl.removeDuplicates(s, k)
 
 print('res', res)
+
