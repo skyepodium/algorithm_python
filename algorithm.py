188ab938702433a2dd86b818e6b5084b612d413d
skyepodium <skyepodium@gmail.com>
2022-05-07 13:51:39 +0900

    leetcode - find-the-highest-altitude

--- algorithm.py
+++ algorithm.py
@@ -1,35 +1,23 @@
 from typing import List
-from collections import Counter
+
 
 class Solution:
-    def maxOperations(self, nums: List[int], k: int) -> int:
+    def largestAltitude(self, gain: List[int]) -> int:
         # 1. init
-        c = Counter(nums)
+        prev = 0
         res = 0
 
         # 2. loop
-        for num in nums:
-            remain = k - num
-            if num == remain and c[remain] <= 1: continue
-            if c[remain] < 1 or c[num] < 1: continue
-
-            res += 1
-            c[remain] -= 1
-            c[num] -= 1
+        for g in gain:
+            prev = prev + g
+            res = max(res, prev)
 
         return res
 
 sl = Solution()
 
-nums = [1,2,3,4]
-k = 5
-nums = [3,1,3,4,3]
-k = 6
+gain = [-5,1,5,0,-7]
 
-nums = [2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2]
-k = 3
-
-
-res = sl.maxOperations(nums, k)
+res = sl.largestAltitude(gain)
 
 print('res', res)
