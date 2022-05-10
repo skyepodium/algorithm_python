1eaac1003de107ec50d1c1909e0532b8928ab196
skyepodium <skyepodium@gmail.com>
2022-05-10 23:17:37 +0900

    leetcode - how-many-numbers-are-smaller-than-the-current-number - O(N))

--- algorithm.py
+++ algorithm.py
@@ -1,35 +1,28 @@
 from typing import List
-from collections import Counter
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         # 1. init
-        c = sorted(Counter(nums).items(), key=lambda x: x[0])
-        n = len(c)
-        d = [0 for _ in range(n)]
-        for i in range(1, n, 1):
-            d[i] = d[i-1] + c[i-1][1]
+        max_int = 101
+        c = [0 for _ in range(max_int)]
+        d = [0 for _ in range(max_int)]
         res = []
 
-        # 2. binary_search
-        def binary_search(target):
-            l, r = 0, n - 1
-            while l <= r:
-                mid = l + (r - l) // 2
-                cur = c[mid][0]
-                if cur < target:
-                    l = mid + 1
-                elif cur == target:
-                    return mid
-                else:
-                    r = mid - 1
-
-        # 3. loop
+        # 2. loop
         for num in nums:
-            idx = binary_search(num)
-            res.append(d[idx])
+            c[num] += 1
+
+        prev = 0
+        for i in range(max_int):
+            if c[i] != 0:
+                d[i], prev = prev, c[i] + prev
+
+        for num in nums:
+            res.append(d[num])
 
         return res
+
+
 
 sl = Solution()
 nums = [8,1,2,2,3]
