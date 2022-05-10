71125f208ed0578fb2ec58dd3d33c2c215c23585
skyepodium <skyepodium@gmail.com>
2022-05-10 22:47:54 +0900

    leetcode - how-many-numbers-are-smaller-than-the-current-number - N^2

--- algorithm.py
+++ algorithm.py
@@ -1,43 +1,15 @@
-from typing import List
-
-
 class Solution:
-    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        if len(original) != m * n: return []
-
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         # 1. init
         res = []
+        n = len(nums)
 
         # 2. loop
-        cnt = 0
-        t = []
-        for o in original:
-            t.append(o)
-            cnt += 1
-            if cnt >= n:
-                cnt = 0
-                res.append(t[::])
-                t = []
+        for i in range(n):
+            cnt = 0
+            for j in range(n):
+                if i == j: continue
+                if nums[i] > nums[j]: cnt += 1
+            res.append(cnt)
 
         return res
-
-
-
-original = [1,2,3,4]
-
-m = 2
-n = 2
-
-original = [1,2,3]
-m = 1
-n = 3
-
-original = [1,2]
-m = 1
-n = 1
-
-sl = Solution()
-
-res = sl.construct2DArray(original, m, n)
-
-print('res', res)
