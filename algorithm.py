c310fddb2e127d5bb32e4a8967b89f1d597328bd
skyepodium <skyepodium@gmail.com>
2022-05-12 21:24:54 +0900

    codeforces - lucky

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,11 @@
 t = int(input())
 
 for _ in range(t):
-    n, m = map(int, input().split())
+    q = input()
 
-    a = []
-    for i in range(n):
-        a.append(list(map(int, input().split())))
+    a = int(q[0]) + int(q[1]) + int(q[2])
+    b = int(q[3]) + int(q[4]) + int(q[5])
 
-    def sum_val(s, e, nx, ny):
-        res = 0
-        while 0 <= s < n and 0 <= e < m:
-            res += a[s][e]
-            s, e = s + nx, e + ny
-        return res
+    res = "YES" if a == b else "NO"
 
-    res = 0
-    for i in range(n):
-        for j in range(m):
-            val = -3 * a[i][j]
-            d = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
-            for dx, dy in d:
-                val += sum_val(i, j, dx, dy)
-            res = max(res, val)
     print(res)
-
-
-
