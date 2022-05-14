3cd70c7d3557c8b7e9230807dbb98ceed9bd48e1
skyepodium <skyepodium@gmail.com>
2022-05-14 22:31:31 +0900

    leetcode - maximum-number-of-balloons

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,30 @@
-t = int(input())
+from collections import Counter
+import re
 
-for _ in range(t):
-    cnt = int(input())
-    a = list(map(int, input().split()))
-    sum_val = sum(a)
-    min_val = min(a)
-    res = sum_val - min_val * cnt
-    print(res)
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        # 1. init
+        t = re.sub("[^balon]", "", text)
+        c = Counter(t)
+        res = int(1e4)
+
+        # 2. exception
+        if len(c) != 5: return 0
+
+        if c['o'] < 2 or c['l'] < 2: return 0
+
+        # 3. loop
+        for key, val in c.items():
+            n = val // 2 if key == 'o' or key == 'l' else val
+            res = min(res, n)
+
+        return res
+
+
+sl = Solution()
+text = "nlaebolko"
+text = "loonbalxballpoon"
+text = "leetcode"
+res = sl.maxNumberOfBalloons(text)
+
+print('res', res)
