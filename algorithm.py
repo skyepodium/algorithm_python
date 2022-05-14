76706edd55337c7fa7a107eac4b6b472765389cb
skyepodium <skyepodium@gmail.com>
2022-05-14 22:35:39 +0900

    leetcode - maximum-number-of-balloons

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,10 @@
 from collections import Counter
-import re
 
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        # 1. init
-        t = re.sub("[^balon]", "", text)
-        c = Counter(t)
-        res = int(1e4)
+        c = Counter(text)
 
-        # 2. exception
-        if len(c) != 5: return 0
-
-        if c['o'] < 2 or c['l'] < 2: return 0
-
-        # 3. loop
-        for key, val in c.items():
-            n = val // 2 if key == 'o' or key == 'l' else val
-            res = min(res, n)
-
-        return res
+        return min(c['b'], c['a'], c['l']//2, c['o']//2, c['n'])
 
 
 sl = Solution()
