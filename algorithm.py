1bb1230a256a5b4a55b20fa2c989737d28fa95c1
skyepodium <skyepodium@gmail.com>
2022-05-18 20:44:15 +0900

    leetcode - build-array-from-permutation

--- algorithm.py
+++ algorithm.py
@@ -1,42 +1,14 @@
-from collections import deque
 from typing import List
 
 
 class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # 1. init
-        n = len(grid)
-        m = len(grid[0])
-        check = [[-1 for _ in range(m)] for _ in range(n)]
-        d = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
+    def buildArray(self, nums: List[int]) -> List[int]:
 
-        # 2. bfs
-        def bfs(x, y):
-            if grid[x][y] != 0: return
-
-            check[x][y] = 1
-            q = deque([(x, y)])
-
-            while q:
-                x, y = q.popleft()
-
-                for dx, dy in d:
-                    nx, ny = x + dx, y + dy
-                    if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
-                    if grid[nx][ny] != 0 or check[nx][ny] != -1: continue
-
-                    check[nx][ny] = check[x][y] + 1
-                    q.append((nx, ny))
-
-        bfs(0, 0)
-
-        return check[n-1][m-1]
-
+        return [nums[nums[idx]] for idx in range(len(nums))]
 
 sl = Solution()
-grid = [[0,1],[1,0]]
-grid = [[0,0,0],[1,1,0],[1,1,0]]
-grid = [[1,0,0],[1,1,0],[1,1,0]]
-res = sl.shortestPathBinaryMatrix(grid)
+nums = [0,2,1,5,3,4]
+nums = [5,0,1,2,3,4]
+res = sl.buildArray(nums)
 
 print('res', res)
