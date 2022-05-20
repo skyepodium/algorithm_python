b58f519b4562b040eb85206df036132eb6006107
skyepodium <skyepodium@gmail.com>
2022-05-20 17:00:48 +0900

    leetcode - unique-paths

--- algorithm.py
+++ algorithm.py
@@ -2,24 +2,20 @@ from typing import List
 
 
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    def uniquePaths(self, m: int, n: int) -> int:
         # 1. init
-        n = len(obstacleGrid)
-        m = len(obstacleGrid[0])
+        m, n = n, m
         d = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        if obstacleGrid[0][0] == 1:
-            return 0
         d[1][1] = 1
 
         # 2. loop
         for i in range(1, n+1):
             for j in range(1, m+1):
                 if i == 1 and j == 1: continue
-                if obstacleGrid[i-1][j-1] == 1: continue
-
                 d[i][j] = d[i-1][j] + d[i][j-1]
 
         return d[n][m]
+
 
 
 sl = Solution()
