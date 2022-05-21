7fd549385a92e341aa645b94b37c889389618dcc
skyepodium <skyepodium@gmail.com>
2022-05-21 11:52:46 +0900

    leetcode - coin-change

--- algorithm.py
+++ algorithm.py
@@ -2,26 +2,34 @@ from typing import List
 
 
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
+    def coinChange(self, coins: List[int], amount: int) -> int:
         # 1. init
-        m, n = n, m
-        d = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        d[1][1] = 1
+        max_int = amount + 1
+        d = [max_int for _ in range(max_int)]
+        d[0] = 0
 
-        # 2. loop
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if i == 1 and j == 1: continue
-                d[i][j] = d[i-1][j] + d[i][j-1]
+        # 2. bottom up
+        for i in range(0, max_int):
+            for c in coins:
+                if i-c >= 0:
+                    d[i] = min(d[i], d[i-c] + 1)
 
-        return d[n][m]
-
-
+        return d[amount] if d[amount] <= amount else -1
 
 sl = Solution()
-obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
-obstacleGrid = [[0,1],[0,0]]
-obstacleGrid = [[1]]
-res = sl.uniquePathsWithObstacles(obstacleGrid)
+coins = [1,2,5]
+amount = 11
+
+coins = [2]
+amount = 3
+
+coins = [1]
+amount = 0
+
+# coins = [186,419,83,408]
+# amount = 6249
+res = sl.coinChange(coins, amount)
 
 print('res', res)
+
+
