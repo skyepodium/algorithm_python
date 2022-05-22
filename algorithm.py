bb3bc9ebd00aebe1b0ba75ac62fb0d02f78a53f2
skyepodium <skyepodium@gmail.com>
2022-05-23 02:48:05 +0900

    leetcode - three-divisors

--- algorithm.py
+++ algorithm.py
@@ -1,27 +1,10 @@
 class Solution:
-    def countSubstrings(self, s: str) -> int:
+    def isThree(self, n: int) -> bool:
         # 1. init
-        n = len(s)
-        self.res = 0
+        res = 0
 
-        # 2. is_palindrome
-        def is_palindrome(l, r):
-            while 0 <= l <= r < n and s[l] == s[r]:
-                self.res += 1
-                l -= 1
-                r += 1
+        # 2. loop
+        for i in range(1, n+1):
+            if n % i == 0: res += 1
 
-        # 3. loop
-        for i in range(n):
-            is_palindrome(i, i)
-            is_palindrome(i, i+1)
-
-        return self.res
-
-sl = Solution()
-s = "abc"
-s = "aaa"
-s = "qwer"
-res = sl.countSubstrings(s)
-
-print('res', res)
+        return res == 3
