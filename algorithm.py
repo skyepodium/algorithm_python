0a40b946e74dceab94819ef22ad7d11569b82fec
skyepodium <skyepodium@gmail.com>
2022-05-24 22:42:53 +0900

    leetcode - find-if-path-exists-in-graph

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,25 @@
+from typing import List
+
+
 class Solution:
-    def isThree(self, n: int) -> bool:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         # 1. init
-        res = 0
+        check = [False for _ in range(n)]
+        v = [[] for _ in range(n)]
 
-        # 2. loop
-        for i in range(1, n+1):
-            if n % i == 0: res += 1
+        # 2. make graph
+        for s, e in edges:
+            v[s].append(e)
+            v[e].append(s)
 
-        return res == 3
+        # 3. DFS
+        def dfs(node):
+            check[node] = True
+            for n_node in v[node]:
+                if check[n_node]: continue
+                dfs(n_node)
+
+        dfs(source)
+
+        return check[destination]
+
