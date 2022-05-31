4faf2b5de677467eaeb3f9be1bc897ecea7d4d95
skyepodium <skyepodium@gmail.com>
2022-05-31 22:16:28 +0900

    leetcode - check-if-a-string-contains-all-binary-codes-of-size-k

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,31 @@
-from typing import List
-from itertools import zip_longest
-
 class Solution:
-    def sortEvenOdd(self, nums: List[int]) -> List[int]:
-        res = []
-        for e, o in zip_longest(sorted(nums[0::2]), sorted(nums[1::2], reverse=True)):
-            if e: res.append(e)
-            if o: res.append(o)
-        return res
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        # 1. init
+        seen = set()
+        n = len(s)
+        cnt = 2 ** k
+
+        # 2. loop
+        for i in range(n - k + 1):
+            cur = s[i:i+k]
+            if cur not in seen:
+                cnt -= 1
+                seen.add(cur)
+            if cnt == 0: return True
+
+        return False
 
 sl = Solution()
-nums = [4,1,2,3]
-nums = [2, 1]
-nums = [5,39,33,5,12,27,20,45,14,25,32,33,30,30,9,14,44,15,21]
-res = sl.sortEvenOdd(nums)
+
+s = "00110110"
+k = 2
+
+s = "0110"
+k = 1
+
+s = "0110"
+k = 2
+
+res = sl.hasAllCodes(s, k)
 
 print('res', res)
