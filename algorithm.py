76504da55396f9882adcf4972d7f0a230424157d
skyepodium <skyepodium@gmail.com>
2022-06-05 22:56:57 +0900

    leetcode - v

--- algorithm.py
+++ algorithm.py
@@ -1,89 +1,34 @@
-plain_text = "flag{CiphersAreAwesome}"
-
-cyper_text = "gwox{RgqssihYspOntqpxs}"
-
-key = "blorpy"
-
-plain_text = "divert troops to east ridge"
-
-cyper_text = "vstwbr lbmgzq ly cscr jsbyo"
-
-key = "sky"
-
-
-
-class vigenere:
-    def __init__(self):
-        self.__make_table()
-
-    def __make_table(self):
-        self.t = []
-        for i in range(26):
-            self.t.append([c if c < 26 else c - 26 for c in range(i, i+26)])
-
-    def __get_base_by_is_upper(self, c):
-        return ord('A') if c.isupper() else ord('a')
-
-    def encode(self, s:str, key:str):
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
         # 1. init
+        n = len(s)
         res = ""
-        key_idx = 0
 
         # 2. loop
-        for c in s:
-            if not c.isalpha():
-                res += c
-            else:
-                k = key[key_idx]
-                key_idx += 1
-                if key_idx >= len(key): key_idx = 0
+        for i in range(n):
+            for j in range(i+1, n+1):
+                # 1) set
+                cur = s[i:j]
+                se = set(cur)
+                is_nice = True
 
-                c_base = self.__get_base_by_is_upper(c)
-                k_base = self.__get_base_by_is_upper(k)
-                col_idx = ord(c) - c_base
-                row_idx = ord(k) - k_base
-
-                res += chr(self.t[row_idx][col_idx] + c_base)
-
-        return res
-
-    def decode(self, s:str, key:str):
-        # 1. init
-        res = ""
-        key_idx = 0
-
-        for c in s:
-            if not c.isalpha():
-                res += c
-            else:
-                k = key[key_idx]
-                key_idx += 1
-                if key_idx >= len(key): key_idx = 0
-
-                c_base = self.__get_base_by_is_upper(c)
-                k_base = self.__get_base_by_is_upper(k)
-                row_idx = ord(k) - k_base
-                col_idx = 0
-                for c_idx, val in enumerate(self.t[row_idx]):
-                    if chr(val + c_base) == c:
-                        col_idx = c_idx
+                # 2) check
+                for c in list(se):
+                    if (c == c.lower() and c.upper() not in se) or (c == c.upper() and c.lower() not in se):
+                        is_nice = False
                         break
 
-                res += chr(col_idx + c_base)
+                if is_nice:
+                    if len(res) < len(cur):
+                        res = cur
 
         return res
 
-v = vigenere()
+s = "YazaAay"
+# s = "Bb"
+# s = "c"
 
-encrypt = v.encode(plain_text, key)
+sl = Solution()
+res = sl.longestNiceSubstring(s)
 
-print("encrypt", encrypt)
-
-print(encrypt == cyper_text)
-
-decrypt = v.decode(cyper_text, key)
-
-print("decrypt", decrypt)
-
-print(decrypt == plain_text)
-
+print('res', res)
