a7fe8c40bb291c7d6574720554f1617fa7855b85
skyepodium <skyepodium@gmail.com>
2022-06-06 22:29:30 +0900

    외판원 순회 - 경로추적 추가

--- algorithm.py
+++ algorithm.py
@@ -7,25 +7,31 @@ MAX_INT = 1 << n
 v = []
 d = [[MAX_VAl for _ in range(MAX_INT)] for _ in range(n)]
 size = 1 << n
-res = [MAX_VAl]
+result = {
+    "dist": MAX_VAl,
+    "st": ""
+}
 
 # 2. make graph
 for _ in range(n):
     v.append(list(map(int, input().split())))
 
-# 3. BFS
+# 3. dijkstra
 def dijkstra(node):
     d[node][1] = 0
     pq = []
-    heappush(pq, (0, node, 1))
+    heappush(pq, (0, node, 1, "0"))
 
     while pq:
-        cost, node, status = heappop(pq)
+        cost, node, status, st = heappop(pq)
 
         if cost > d[node][status]: continue
 
         if status == size - 1 and v[node][0] != 0:
-            res[0] = min(res[0], cost + v[node][0])
+            cur_cost = cost + v[node][0]
+            if result["dist"] > cur_cost:
+                result["dist"] = cur_cost
+                result["st"] = st
 
         for n_node in range(n):
             if v[node][n_node] == 0 or status & 1 << n_node: continue
@@ -34,8 +40,8 @@ def dijkstra(node):
             n_cost = d[node][status] + v[node][n_node]
             if d[n_node][n_status] > n_cost:
                 d[n_node][n_status] = n_cost
-                heappush(pq, (n_cost, n_node, n_status))
+                heappush(pq, (n_cost, n_node, n_status, f"{st} {str(n_node)}"))
 dijkstra(0)
 
-print(res[0])
+print(result["dist"])
 
