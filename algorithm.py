8c99e02b3c775849948c9aba3df28c046aff1ada
skyepodium <skyepodium@gmail.com>
2022-06-06 13:11:56 +0900

    백준 2098 - 외판원 순회

--- algorithm.py
+++ algorithm.py
@@ -1,33 +1,34 @@
-class Solution:
-    def longestNiceSubstring(self, s: str) -> str:
-        # 1. init
-        n = len(s)
-        res = ""
+# 1. init
+n = int(input())
+MAX_VAl = 1000001 * 16
+MAX_INT = 1 << n
+v = []
+d = [[MAX_VAl for _ in range(MAX_INT)] for _ in range(n)]
+size = 1 << n
 
-        # 2. loop
-        for i in range(n):
-            for j in range(i+1, n+1):
-                # 1) set
-                cur = s[i:j]
-                se = set(cur)
-                is_nice = True
+# 2. make graph
+for _ in range(n):
+    v.append(list(map(int, input().split())))
 
-                # 2) check
-                for c in list(se):
-                    if (c == c.lower() and c.upper() not in se) or (c == c.upper() and c.lower() not in se):
-                        is_nice = False
-                        break
+# 3. travel
+# cur 도시를 방문하고, 나머지 모든 도시를 방문했을때 최소 비용
+def go(cur, status):
+    # 1) 이미 방문한 경우 - memoization
+    if d[cur][status] != MAX_VAl:
+        return d[cur][status]
 
-                if is_nice and len(res) < len(cur):
-                    res = cur
+    if status == size - 1 and v[cur][0] != 0: return v[cur][0]
 
-        return res
+    # 2) 탐색
+    for next in range(n):
+        # 길이 없거나, 이미 방문한 도시이면 건너뜀
+        if v[cur][next] == 0 or status & 1 << next: continue
 
-s = "YazaAay"
-# s = "Bb"
-# s = "c"
+        d[cur][status] = min(d[cur][status], go(next, status | (1 << next)) + v[cur][next])
 
-sl = Solution()
-res = sl.longestNiceSubstring(s)
+    return d[cur][status]
 
-print('res', res)
+res = go(0, 1)
+
+print(res)
+
