ba27b346943a03eb67f202f1f7a4cf3b51ef3a30
skyepodium <skyepodium@gmail.com>
2022-06-08 22:44:03 +0900

    pico ctf

--- algorithm.py
+++ algorithm.py
@@ -1,47 +1,5 @@
-from heapq import heappush, heappop
-
-# 1. init
-n = int(input())
-MAX_VAl = 1000001 * 16
-MAX_INT = 1 << n
-v = []
-d = [[MAX_VAl for _ in range(MAX_INT)] for _ in range(n)]
-size = 1 << n
-result = {
-    "dist": MAX_VAl,
-    "st": ""
-}
-
-# 2. make graph
-for _ in range(n):
-    v.append(list(map(int, input().split())))
-
-# 3. dijkstra
-def dijkstra(node):
-    d[node][1] = 0
-    pq = []
-    heappush(pq, (0, node, 1, "0"))
-
-    while pq:
-        cost, node, status, st = heappop(pq)
-
-        if cost > d[node][status]: continue
-
-        if status == size - 1 and v[node][0] != 0:
-            cur_cost = cost + v[node][0]
-            if result["dist"] > cur_cost:
-                result["dist"] = cur_cost
-                result["st"] = st
-
-        for n_node in range(n):
-            if v[node][n_node] == 0 or status & 1 << n_node: continue
-
-            n_status = status | (1 << n_node)
-            n_cost = d[node][status] + v[node][n_node]
-            if d[n_node][n_status] > n_cost:
-                d[n_node][n_status] = n_cost
-                heappush(pq, (n_cost, n_node, n_status, f"{st} {str(n_node)}"))
-dijkstra(0)
-
-print(result["dist"])
-
+res = ""
+while True:
+    a = int(input())
+    res += chr(a)
+    print('res', res)
