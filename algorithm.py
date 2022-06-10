93dd84b408bbefdea8cec389581355e6cd574fe2
skyepodium <skyepodium@gmail.com>
2022-06-10 22:48:19 +0900

    longest-substring-without-repeating-characters

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,25 @@
-res = ""
-while True:
-    a = int(input())
-    res += chr(a)
-    print('res', res)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # 1. inti
+        res = 0
+        st = set()
+
+        # 2. two pointer
+        l = 0
+        for r, c in enumerate(s):
+            while c in st:
+                st.remove(s[l])
+                l += 1
+            st.add(c)
+            res = max(res, r - l + 1)
+
+        return res
+
+
+sl = Solution()
+s = "abcabcbb"
+s = "bbbbb"
+s = "pwwkew"
+res = sl.lengthOfLongestSubstring(s)
+
+print('res', res)
