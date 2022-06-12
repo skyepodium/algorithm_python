5fc0699b5da9053d11c2e9bb8b3081e52b951e54
skyepodium <skyepodium@gmail.com>
2022-06-12 23:59:20 +0900

    Stegasaurus - Treasure hunt - ascii

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,3 @@
-for k in range(250):
-    res = "".join(chr(ord(c)^k) for c in "Fpvgpa8'M,m&!s,!")
-    print('res', res)
+for k in range(127):
+    res = "".join(chr(ord(c) ^ k) for c in "Fpvgpa8'M,m&!s,!")
+    print('k', k, 'res', res)
