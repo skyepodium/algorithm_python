f24706595ab87404a4a82741d10d21231aedbc72
skyepodium <skyepodium@gmail.com>
2022-10-22 18:29:17 +0900

    leetcode - flipAndInvertImage

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,18 @@
-from collections import Counter
+from typing import List
 
-def solution(want, number, discount):
-    # 1. init
-    c = Counter()
-    for w, n in zip(want, number):
-        c[w] = n
 
-    # 2. discount
-    n = len(discount)
-    res = 0
-    for i in range(n):
-        if discount[i] not in c:
-            continue
-        d = Counter(discount[i:i+10])
-        if len(c - d) == 0:
-            res += 1
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for row in image:
+            row.reverse()
+            for i in range(len(row)):
+                row[i] = 1 - row[i]
+        return image
 
-    return res
 
-want = ["b", "a", "r", "p1", "p2"]
-number = [3, 2, 2, 2, 1]
-discount = ["c", "a", "a", "b", "r", "a", "p1", "b", "p1", "r", "p2", "b", "a", "b"]
+image = [[1,1,0],[1,0,1],[0,0,0]]
 
-res = solution(want, number, discount)
-
+res = Solution().flipAndInvertImage(image)
 print('res', res)
+
 
