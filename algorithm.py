95ab42664339553a5f284f0477b8ef294c0204b4
skyepodium <skyepodium@gmail.com>
2022-10-29 11:24:55 +0900

    leetcode find-the-town-judge

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,37 @@
+from collections import Counter
+from typing import List
+
+
 class Solution:
-    def reverseWords(self, s: str) -> str:
-        return ' '.join([c[::-1] for c in s.split()])
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        # 0. exception
+        if n == 1:
+            return 1
 
+        # 1. init
+        trust_count = Counter()
 
-s = "Let's take LeetCode contest"
-s = "God Ding"
+        # 2. loop
+        for start, end in trust:
+            trust_count[end] += 1
+            trust_count[start] -= 1
+
+        for person, cnt in trust_count.items():
+            if cnt == n - 1:
+                return person
+
+        return -1
+
+n = 2
+trust = [[1,2]]
+
+n = 3
+trust = [[1,3],[2,3]]
+
+n = 3
+trust = [[1,3],[2,3],[3,1]]
+
 sl = Solution()
-res = sl.reverseWords(s)
+res = sl.findJudge(n, trust)
 
 print('res', res)
