766b0b0c08dcde4452cad441b0d139318a4a11db
skyepodium <skyepodium@gmail.com>
2022-11-02 23:32:10 +0900

    baekjoon 4158

--- algorithm.py
+++ algorithm.py
@@ -1,32 +1,18 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 
-a, b = map(int, input().split())
-n = int(input())
+while True:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0: break
 
-MAX_INT = 1001
-check = [-1 for _ in range(MAX_INT)]
-q = deque()
-check[a] = 0
-q.append(a)
+    res = 0
+    s = set()
 
-for _ in range(n):
-    c = int(input())
-    check[c] = 1 if c != a else 0
-    q.append(c)
+    for _ in range(n + m):
+        c = int(input())
+        if c in s:
+            res += 1
+        else:
+            s.add(c)
 
-def bfs():
-    while q:
-        node = q.popleft()
-
-        l, r = node - 1, node + 1
-
-        if l >= 1 and check[l] == -1:
-            check[l] = check[node] + 1
-            q.append(l)
-        if r < MAX_INT and check[r] == -1:
-            check[r] = check[node] + 1
-            q.append(r)
-
-bfs()
-
-print(check[b])
+    print(res)
