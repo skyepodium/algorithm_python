f9de12dd58cd9fe293408b357457285c83e6f175
skyepodium <skyepodium@gmail.com>
2022-11-03 23:11:19 +0900

    baekjoon 8545

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,1 @@
-n, m = map(int, input().split())
-
-a = []
-
-for i in range(1, m+1):
-    if len(a) >= m: break
-    for _ in range(i):
-        if len(a) >= m: break
-        a.append(i)
-
-print(sum(a[n-1:]))
+print(input()[::-1])
