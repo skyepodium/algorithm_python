ace8ef1713fc88e487ea9ffd3eb3c9a01a45cb58
skyepodium <skyepodium@gmail.com>
2022-11-06 02:58:18 +0900

    leetcode - most-frequent-even-element

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,13 @@
-n, m = map(int, input().split())
-a = int(input())
-b = int(input())
-print(a*b)
+s = input()
+a = [0, 0]
+
+if len(s) == 2:
+    a[0], a[1] = int(s[0]), int(s[1])
+elif len(s) == 4:
+    a[0], a[1] = 10, 10
+elif s[1] == '0':
+    a[0], a[1] = 10, int(s[2])
+else:
+    a[0], a[1] = int(s[0]), 10
+
+print(sum(a))
