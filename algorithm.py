e5e27a5a0dec50cee755e8752c4417b71a64d06a
skyepodium <skyepodium@gmail.com>
2022-11-06 03:20:29 +0900

    boj 11091

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,13 @@
-a, b, c = map(int, input().split())
-res = 0
+n = int(input())
 
-res = max(max(res, a * b / c), a / b * c)
-print(int(res))
+alpha_array = [chr(ord('a') + i) for i in range(0, 26)]
+alpah_set = set(alpha_array)
+
+for _ in range(n):
+    s = [c for c in input().lower() if c in alpah_set]
+    input_set = set(s)
+
+    if len(input_set) == 26:
+        print("pangram")
+    else:
+        print("missing", "".join(sorted(list(alpah_set - input_set))))
