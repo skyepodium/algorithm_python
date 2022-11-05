25c85466c24e70092084b82a324fb4a54adc0334
skyepodium <skyepodium@gmail.com>
2022-11-06 03:03:28 +0900

    boj 15726

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,5 @@
-s = input()
-a = [0, 0]
+a, b, c = map(int, input().split())
+res = 0
 
-if len(s) == 2:
-    a[0], a[1] = int(s[0]), int(s[1])
-elif len(s) == 4:
-    a[0], a[1] = 10, 10
-elif s[1] == '0':
-    a[0], a[1] = 10, int(s[2])
-else:
-    a[0], a[1] = int(s[0]), 10
-
-print(sum(a))
+res = max(max(res, a * b / c), a / b * c)
+print(int(res))
