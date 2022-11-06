52abc7b6242104dc1c093b2eda98e6823d8c68b3
skyepodium <skyepodium@gmail.com>
2022-11-06 10:04:47 +0900

    boj 1009

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,28 @@
-from collections import defaultdict
+t = int(input())
 
-d = defaultdict(int)
+for _ in range(t):
+    # 0. input
+    a, b = map(int, input().split())
+    base = a % 10
+    last_num = a % 10
+    res = 0
 
-a, b, c = map(int, input().split())
-for i in range(1, a+1):
-    for j in range(1, b+1):
-        for k in range(1, c+1):
-            r = i + j + k
-            d[r] += 1
+    # 1. exception
+    if last_num == 1:
+        print(1)
+        continue
 
-r = sorted(d.items(), key=lambda x: (-x[1], x[0]))
+    # 2. init
+    s = set()
+    cycle = []
+    while last_num not in s:
+        s.add(last_num)
+        cycle.append(last_num)
+        last_num = (last_num * base) % 10
 
-print(r[0][0])
+    # 3. find
+    cycle_len = len(cycle)
+    last_idx = b % cycle_len
+    res = cycle[last_idx - 1]
+
+    print(res if res != 0 else 10)
