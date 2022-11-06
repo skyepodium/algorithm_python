6bfd3fe1d20aa6f965c47ec1eb3104ab8832b501
skyepodium <skyepodium@gmail.com>
2022-11-06 20:49:05 +0900

    boj 10870

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,9 @@
-from collections import Counter
+a = [0 for _ in range(21)]
+
+a[0], a[1] = 0, 1
+
+for i in range(2, 21):
+    a[i] = a[i-1] + a[i-2]
 
 n = int(input())
-
-for _ in range(n):
-    s = input()
-    c = Counter(s.lower())
-
-    res = "GOOD" if c['g'] > c['b'] else "A BADDY"
-    if c['g'] == c['b']:
-        res = "NEUTRAL"
-
-    print(f"{s} is {res}")
+print(a[n])
