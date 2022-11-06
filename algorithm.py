40d2e01969fb7c9b58d5356249961d41190ee963
skyepodium <skyepodium@gmail.com>
2022-11-06 21:16:48 +0900

    boj 11170

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,9 @@
-a = [0 for _ in range(21)]
-
-a[0], a[1] = 0, 1
-
-for i in range(2, 21):
-    a[i] = a[i-1] + a[i-2]
-
 n = int(input())
-print(a[n])
+
+for _ in range(n):
+    s, e = map(int, input().split())
+    res = 0
+    for i in range(s, e + 1):
+        res += str(i).count("0")
+
+    print(res)
