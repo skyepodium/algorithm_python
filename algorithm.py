cefd0e33661b16c93db4fdcaa6cf3e88442f2cb3
skyepodium <skyepodium@gmail.com>
2022-11-13 02:18:11 +0900

    percentage-of-letter-in-string

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,5 @@
 from collections import Counter
 
 class Solution:
-    def frequencySort(self, s: str) -> str:
-        return "".join([str(key)*count for key, count in sorted([(key, count) for key, count in Counter(s).items()], key=lambda x: -x[1])])
-
-sl = Solution()
-print(sl.frequencySort("tree"))
+    def percentageLetter(self, s: str, letter: str) -> int:
+        return Counter(s)[letter] * 100 // len(s)
