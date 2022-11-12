11651dd4b43cea78bf525122b240443f2e27943f
skyepodium <skyepodium@gmail.com>
2022-11-13 02:04:05 +0900

    first-unique-character-in-a-string

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,23 @@
-from collections import defaultdict
+from collections import Counter
 
 class Solution:
-    def repeatedCharacter(self, s: str) -> str:
-        # 1. init
-        d = defaultdict(int)
+    def firstUniqChar(self, s: str) -> int:
+        # 1. one appearance
+        a = [key for key, count in Counter(s).items() if count == 1]
 
         # 2. loop
-        for c in s:
-            d[c] += 1
-            if d[c] >= 2:
-                return c
+        min_idx = len(s) - 1
+        res = -1
 
-        return ''
+        for c in a:
+            idx = s.index(c)
+            if idx <= min_idx:
+                min_idx = idx
+                res = idx
+
+        return res
+
+
+sl = Solution()
+s = "leetcode"
+print(sl.firstUniqChar(s))
