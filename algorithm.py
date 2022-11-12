8d87c580aeb71e1b4f1eb3fcf477b576b4a80410
skyepodium <skyepodium@gmail.com>
2022-11-13 04:56:11 +0900

    boj 14567

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,28 @@
-from typing import List
+from collections import deque
 
+n, m = map(int, input().split())
+v = [[] for _ in range(n+1)]
+ind = [0] * (n + 1)
+res = [1] * (n + 1)
 
-class Solution:
-    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        res = 0
-        for s, e in zip(startTime, endTime):
-            if s <= queryTime <= e:
-                res += 1
+for _ in range(m):
+    s, e = map(int, input().split())
+    ind[e] += 1
+    v[s].append(e)
 
-        return res
+q = deque()
+for i in range(1, n+1):
+    if ind[i] == 0:
+        q.append(i)
+
+while q:
+    node = q.popleft()
+
+    for n_node in v[node]:
+        ind[n_node] -= 1
+        res[n_node] = res[node] + 1
+
+        if ind[n_node] == 0:
+            q.append(n_node)
+
+print(" ".join([str(i) for i in res[1:]]))
