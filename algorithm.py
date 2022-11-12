d39f88418b5e4ebf687c9bcffa22a614417288de
skyepodium <skyepodium@gmail.com>
2022-11-13 03:15:18 +0900

    programmers - 두 큐의 합 같게 만들기

--- algorithm.py
+++ algorithm.py
@@ -1,33 +1,49 @@
-def solution(n,a,b):
+from collections import deque
+
+def solution(queue1, queue2):
     # 1. init
+    queue1, queue2 = deque(queue1), deque(queue2)
+    s1, s2 = sum(queue1), sum(queue2)
+    total = s1 + s2
     res = 0
-    a -= 1
-    b -= 1
+    s = set()
 
-    # 2. swap
-    if a > b:
-        a, b = b, a
+    if total != (total // 2) * 2:
+        return -1
 
-    def next_round(num):
-        if num % 2 == 1:
-            num -= 1
-
-        return num // 2
-
-    # 3. loop
-    while a >= 0 and b >= 0:
+    # 2. loop
+    while queue1 and queue2 and s1 != s2:
         res += 1
-        if a % 2 == 0 and a + 1 == b:
-            break
+        key = f"s1{s1}{queue1[0]}s2{s2}{queue2[0]}"
+        if key in s:
+            return -1
+        s.add(key)
 
-        a = next_round(a)
-        b = next_round(b)
+        if s1 > s2:
+            front = queue1.popleft()
+            s1 -= front
+            queue2.append(front)
+            s2 += front
+        else:
+            front = queue2.popleft()
+            s2 -= front
+            queue1.append(front)
+            s1 += front
+
+    if not queue1 or not queue2:
+        return -1
 
     return res
 
-n = 8
-a = 4
-b = 7
 
-res = solution(n, a, b)
+queue1 = [3, 2, 7, 2]
+queue2 = [4, 6, 5, 1]
+
+queue1 = [1, 2, 1, 2]
+queue2 = [1, 10, 1, 2]
+
+queue1 = [1, 1]
+queue2 = [1, 5]
+
+res = solution(queue1, queue2)
 print('res', res)
