aff8c8e2acb36c0e1a025e78c2f101d53f116ad3
skyepodium <skyepodium@gmail.com>
2022-11-13 02:41:09 +0900

    programmers - 예상 대진표

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,33 @@
-from collections import Counter
+def solution(n,a,b):
+    # 1. init
+    res = 0
+    a -= 1
+    b -= 1
 
-class Solution:
-    def percentageLetter(self, s: str, letter: str) -> int:
-        return Counter(s)[letter] * 100 // len(s)
+    # 2. swap
+    if a > b:
+        a, b = b, a
+
+    def next_round(num):
+        if num % 2 == 1:
+            num -= 1
+
+        return num // 2
+
+    # 3. loop
+    while a >= 0 and b >= 0:
+        res += 1
+        if a % 2 == 0 and a + 1 == b:
+            break
+
+        a = next_round(a)
+        b = next_round(b)
+
+    return res
+
+n = 8
+a = 4
+b = 7
+
+res = solution(n, a, b)
+print('res', res)
