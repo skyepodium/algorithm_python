05a6ac916b0ac488a27c6e04c28a25e76881bd02
skyepodium <skyepodium@gmail.com>
2022-11-13 02:09:50 +0900

    leetcode - sort-characters-by-frequency

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,8 @@
 from collections import Counter
 
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        # 1. one appearance
-        a = [key for key, count in Counter(s).items() if count == 1]
-
-        # 2. loop
-        min_idx = len(s) - 1
-        res = -1
-
-        for c in a:
-            idx = s.index(c)
-            if idx <= min_idx:
-                min_idx = idx
-                res = idx
-
-        return res
-
+    def frequencySort(self, s: str) -> str:
+        return "".join([str(key)*count for key, count in sorted([(key, count) for key, count in Counter(s).items()], key=lambda x: -x[1])])
 
 sl = Solution()
-s = "leetcode"
-print(sl.firstUniqChar(s))
+print(sl.frequencySort("tree"))
