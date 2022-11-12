6dbc392d26bc25e138353a592f3d9252bcbaf036
skyepodium <skyepodium@gmail.com>
2022-11-12 14:26:16 +0900

    leetcode - find-median-from-data-stream

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,30 @@
-class Solution:
-    def makeGood(self, s: str) -> str:
-        res = []
+from heapq import heappush, heappop
 
-        for c in s:
-            top = res[-1] if len(res) > 0 else ""
+class MedianFinder:
 
-            if top.lower() == c.lower() and ord(top) != ord(c):
-                res.pop()
-            else:
-                res.append(c)
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
 
-        return "".join(res)
+    def addNum(self, num: int) -> None:
+        if len(self.max_heap) < 1:
+            heappush(self.max_heap, -num)
+            return
+
+        if num < -self.max_heap[0]:
+            heappush(self.max_heap, -num)
+        else:
+            heappush(self.min_heap, num)
+
+        if len(self.max_heap) >= len(self.min_heap) + 2:
+            heappush(self.min_heap, -heappop(self.max_heap))
+        elif len(self.max_heap) < len(self.min_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
+
+        return
+
+    def findMedian(self) -> float:
+        if len(self.max_heap) == len(self.min_heap):
+            return (-self.max_heap[0] + self.min_heap[0]) / 2.0
+
+        return -float(self.max_heap[0])
