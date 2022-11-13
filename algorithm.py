f098bd0b1ca59ccdfde649d720790ee55b05d59e
skyepodium <skyepodium@gmail.com>
2022-11-13 14:01:34 +0900

    leetcod - reverse-words-in-a-string

--- algorithm.py
+++ algorithm.py
@@ -1,22 +1,9 @@
-def solution(elements):
-    # 1. init
-    s = set()
-    n = len(elements)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(s.strip().split()[::-1])
 
-    # 2. loop
-    for num in range(1, n+1):
-        for l in range(n):
-            r = l + num
-            if r > n:
-                cur = sum(elements[l:r] + elements[:r - n])
-            else:
-                cur = sum(elements[l:r])
+s = "the sky is blue"
+s = "  hello world  "
+s = "a good   example"
 
-            if not cur in s:
-                s.add(cur)
-        print()
-
-    return len(s)
-
-elements = [7,9,1,1,4]
-print(solution(elements))
+print(Solution().reverseWords(s))
