be6b2bfde2090dca1408cad378abb6d28e612f22
skyepodium <skyepodium@gmail.com>
2022-11-23 04:31:08 +0900

    boj 5972

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,31 @@
-t = int(input())
+from heapq import heappush, heappop
 
-for _ in range(t):
-    n = int(input())
-    s = input()
-    s = sorted(list(s))
-    print(ord(s[-1]) - ord('a') + 1)
+n, m = map(int, input().split())
+v = [[] for _ in range(n+1)]
+MAX_INT = int(1e9)
+d = [MAX_INT] * (n+1)
+
+for _ in range(m):
+    s, e, c = map(int, input().split())
+    v[s].append((e, c))
+    v[e].append((s, c))
+
+def dijkstra(start_node):
+    pq = []
+    d[start_node] = 0
+    heappush(pq, (d[start_node], start_node))
+
+    while pq:
+        cost, node = heappop(pq)
+
+        if d[node] < cost:
+            continue
+
+        for next_node, n_cost in v[node]:
+            if d[next_node] > d[node] + n_cost:
+                d[next_node] = d[node] + n_cost
+                heappush(pq, (d[next_node], next_node))
+
+dijkstra(1)
+
+print(d[n])
