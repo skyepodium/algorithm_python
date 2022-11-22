60d9ad51067ecdee5aae8da8209edec7e6baddcf
skyepodium <skyepodium@gmail.com>
2022-11-23 02:34:11 +0900

    leetcode - find-first-and-last-position-of-element-in-sorted-array

--- algorithm.py
+++ algorithm.py
@@ -2,39 +2,53 @@ from typing import List
 
 
 class Solution:
-    def totalNQueens(self, n: int) -> List[List[str]]:
-        # 1. init
-        check = [False] * (n + 1)
-        st = []
-        res = [0]
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def lower_bound(l, r, nums, target):
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
 
-        # 2. valid
-        def valid(i, j):
-            for x, y in st:
-                if abs(i - x) == abs(j - y):
-                    return False
-            return True
+        def upper_bound(l, r, nums, target):
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] <= target:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
 
-        # 3. DFS
-        def dfs(i):
-            if i >= n:
-                res[0] += 1
-                return
+        def binary_search(l, r, nums, target):
+            while l <= r:
+                mid = l + (r - l) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return -1
 
-            for j in range(n):
-                if not check[j] and valid(i, j):
-                    st.append((i, j))
-                    check[j] = True
-                    dfs(i + 1)
-                    st.pop()
-                    check[j] = False
+        idx = binary_search(0, len(nums)-1, nums, target)
+        if idx == -1:
+            return [-1, -1]
 
-        dfs(0)
+        l = lower_bound(0, len(nums), nums, target)
+        r = upper_bound(0, len(nums), nums, target)
 
-        return res[0]
+        return [l, r-1]
 
 sl = Solution()
-n = 4
-# n = 1
-res = sl.solveNQueens(n)
+nums = [5,7,7,8,8,10]
+target = 8
+nums = [5,7,7,8,8,10]
+target = 6
+nums = []
+target = 0
+# nums = [2, 2]
+# target = 3
+res = sl.searchRange(nums, target)
 print('res', res)
