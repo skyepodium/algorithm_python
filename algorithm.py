e8e995ba171c2016e6b0b479d6e1c045b4aa38ac
skyepodium <skyepodium@gmail.com>
2022-11-23 05:01:04 +0900

    boj 11779

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,17 @@
 from heapq import heappop, heappush
 
-n, m = map(int, input().split())
-p = list(map(int, input().split()))
+n = int(input())
+m = int(input())
+v = [[] for _ in range(n+1)]
 MAX_INT = int(1e12)
-d = [MAX_INT] * (n + 1)
-v = [[] for _ in range(n)]
+d = [MAX_INT] * (n+1)
+path = [0] * (n+1)
 
 for _ in range(m):
     s, e, c = map(int, input().split())
     v[s].append((e, c))
-    v[e].append((s, c))
 
+start_node, end_node = map(int, input().split())
 
 def dijkstra(start_node):
     pq = []
@@ -23,11 +24,21 @@ def dijkstra(start_node):
             continue
 
         for next_node, next_cost in v[node]:
-            if next_node != n-1 and p[next_node] == 1: continue
             if d[next_node] > d[node] + next_cost:
                 d[next_node] = d[node] + next_cost
+                path[next_node] = node
                 heappush(pq, (d[next_node], next_node))
 
 
-dijkstra(0)
-print(d[n-1] if d[n-1] != MAX_INT else -1)
+path[start_node] = start_node
+dijkstra(start_node)
+
+print(d[end_node])
+
+st = []
+while path[end_node] != end_node:
+    st.append(end_node)
+    end_node = path[end_node]
+st.append(start_node)
+print(len(st))
+print(*st[::-1])
