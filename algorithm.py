8a7778a34685fb4b327e3f773b2e6d80668011e0
skyepodium <skyepodium@gmail.com>
2022-11-22 20:08:34 +0900

    leetcode- minimum-depth-of-binary-tree

--- algorithm.py
+++ algorithm.py
@@ -1,34 +1,33 @@
 from typing import Optional
-
-
+from collections import deque
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-
 class Solution:
-    def __init__(self):
-        self.res = int(-1e9)
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        # 0. exception
+        if not root:
+            return 0
 
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        # 1. dfs
-        def dfs(node):
-            # 1) exception
-            if not node:
-                return 0
+        # 1. init
+        q = deque()
+        q.append((root, 1))
 
-            # 2) recursion
-            l = max(0, dfs(node.left))
-            r = max(0, dfs(node.right))
+        # 2. BFS
+        while q:
+            node, step = q.popleft()
 
-            # 3) update
-            self.res = max(self.res, l + r + node.val)
+            l = node.left
+            r = node.right
+            if not l and not r:
+                return step
 
-            # 4) return
-            return max(l, r) + node.val
+            for next_node in [l, r]:
+                if not next_node: continue
+                q.append((next_node, step + 1))
 
-        dfs(root)
-        return self.res
-
+        return 0
