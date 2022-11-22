d5cdf1860dbc3eed06f93357a2a1650ae006ce06
skyepodium <skyepodium@gmail.com>
2022-11-22 10:15:18 +0900

    atcoder - abc278_c

--- algorithm.py
+++ algorithm.py
@@ -1,68 +1,17 @@
-from typing import List
+from collections import defaultdict
 
-class Solution:
-    def openLock(self, deadends: List[str], target: str) -> int:
-        # 1. init
-        visited = set(deadends)
-        check = set()
+n, q = map(int, input().split())
 
-        # 2. move_bit
-        def move_bit(node, idx, direction):
-            if direction == 1:
-                next_bit = (int(node[idx]) + 1) % 10
-            else:
-                next_bit = (int(node[idx]) - 1) % 10
-            if next_bit >= 10:
-                next_bit = 0
-            elif next_bit < 0:
-                next_bit = 9
-            return node[:idx] + str(next_bit) + node[idx+1:]
+d = defaultdict(set)
 
-
-        # 3. bfs
-        def bfs(start_node, target_node):
-            q1 = set([start_node])
-            q2 = set([target_node])
-            step = 0
-
-            while q1 and q2:
-                if len(q1) > len(q2):
-                    q1, q2 = q2, q1
-
-                temp = set()
-
-                for node in q1:
-                    if node in deadends:
-                        continue
-
-                    if node in q2:
-                        return step
-
-                    visited.add(node)
-
-                    for i in range(4):
-                        for j in [-1, 1]:
-                            next_node = move_bit(node, i, j)
-                            if next_node in visited:
-                                continue
-                            temp.add(next_node)
-
-                q1 = q2
-                q2 = temp
-                step += 1
-
-            return -1
-
-        return bfs("0000", target)
-
-deadends = ["0201","0101","0102","1212","2002"]
-target = "0202"
-
-deadends = ["8888"]
-target = "0009"
-
-deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"]
-target = "8888"
-
-sl = Solution()
-print(sl.openLock(deadends, target))
+for _ in range(q):
+    c, s, e = map(int, input().split())
+    if c == 1:
+        d[s].add(e)
+    elif c == 2:
+        if e in d[s]:
+            d[s].remove(e)
+    else:
+        res1 = True if e in d[s] else False
+        res2 = True if s in d[e] else False
+        print('Yes' if res1 and res2 else 'No')
