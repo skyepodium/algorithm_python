2085f8bb0886cb277c59c3bda395e9fe0cc1b8d4
skyepodium <skyepodium@gmail.com>
2022-11-23 00:37:29 +0900

    leetcode - perfect-squares

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,27 @@
-n, x = map(int, input().split())
+class Solution:
+    def numSquares(self, n: int) -> int:
+        # 1. init
+        squares = set()
+        d = [n] * (n + 1)
 
-nums = list(map(int, input().split()))
-res = 0
-l = 0
-sum_val = 0
-sum_val_count = 0
-for r, num in enumerate(nums):
-    sum_val += num
-    if r - l + 1 >= x:
-        if sum_val > res:
-            res = sum_val
-            sum_val_count = 1
-        elif sum_val != 0 and sum_val == res:
-            sum_val_count += 1
-        sum_val -= nums[l]
-        l += 1
+        # 2. loop
+        for i in range(1, n+1):
+            if int(i ** 0.5) ** 2 == i:
+                squares.add(i)
+                d[i] = 1
 
-if res == 0:
-    print("SAD")
-else:
-    print(res)
-    print(sum_val_count)
+        # 3. bottom-up
+        for i in range(1, n+1):
+            for square in squares:
+                diff = i - square
+                if diff < 0:
+                    continue
+                d[i] = min(d[i], d[diff] + 1)
+
+        return d[n]
+
+sl = Solution()
+n = 12
+n = 13
+res = sl.numSquares(n)
+print('res', res)
