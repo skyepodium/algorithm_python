3426a52cf8b735d998f164dca1c62c0b30a458a0
skyepodium <skyepodium@gmail.com>
2022-11-23 14:06:02 +0900

    leetcode - utf-8-validation

--- algorithm.py
+++ algorithm.py
@@ -1,20 +1,25 @@
 from typing import List
-from heapq import heappop, heappush
+
 
 class Solution:
-    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        # 1. init
-        pq = []
-        res = []
+    def validUtf8(self, data: List[int]) -> bool:
+        def check(start_idx, size):
+            for i in range(start_idx + 1, start_idx + size + 1):
+                if i >= len(data) or (data[i] >> 6) != 0b10:
+                    return False
+            return True
 
-        # 2. make max heap
-        for h, k in people:
-            heappush(pq, (-h, k))
-
-        # 3. insert
-        while pq:
-            h, k = heappop(pq)
-            res.insert(k, [-h, k])
-
-        return res
-
+        start = 0
+        while start < len(data):
+            first = data[start]
+            if first >> 3 == 0b11110 and check(start, 3):
+                start += 4
+            elif first >> 4 == 0b1110 and check(start, 2):
+                start += 3
+            elif first >> 5 == 0b110 and check(start, 1):
+                start += 2
+            elif first >> 7 == 0:
+                start += 1
+            else:
+                return False
+        return True
