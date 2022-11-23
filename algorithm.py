a8ea7e6ee0f8f4d56730934a30463d4ac03050e2
skyepodium <skyepodium@gmail.com>
2022-11-23 13:04:13 +0900

    leetcode - best-time-to-buy-and-sell-stock-ii

--- algorithm.py
+++ algorithm.py
@@ -1,51 +1,12 @@
-from collections import Counter
+from typing import List
+
 
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        # 0. exception
-        if len(s) < len(t):
-            return ""
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
 
-        # 1. init
-        base = Counter(t)
-        min_length = len(s) + 1
-        res = ""
-
-        # 2. loop
-        cur = Counter()
-        l = 0
-        for r, c in enumerate(s):
-            cur[c] += 1
-            while len(base - cur) == 0:
-                diff = r - l + 1
-                if min_length > diff:
-                    min_length = diff
-                    res = s[l:r+1]
-
-                if cur[s[l]] > 0:
-                    cur[s[l]] -= 1
-                l += 1
+        for i in range(len(prices) - 1):
+            if prices[i+1] > prices[i]:
+                res += prices[i+1] - prices[i]
 
         return res
-
-s = "ADOBECODEBANC"
-t = "ABC"
-
-s = "a"
-t = "a"
-
-s = "a"
-t = "aa"
-#
-s = "a"
-t = "b"
-#
-s = "ab"
-t = "A"
-
-s = "abc"
-t = "cba"
-
-sl = Solution()
-res = sl.minWindow(s, t)
-print('res', res)
