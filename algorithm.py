d89e2a7421b2edfd322c0de05b340abccec746e1
skyepodium <skyepodium@gmail.com>
2022-11-24 20:52:39 +0900

    2386

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,7 @@
-n = int(input())
+while True:
+    s = input()
+    if s[0] == "#":
+        break
 
-a = bin(n)[2:].zfill(32)
-b = a.replace('1', '2').replace('0', '1').replace('2', '0')
-
-m = int(b, 2)
-m += 1
-
-b = bin(m)[2:]
-b = b[:32]
-
-res = 0
-
-for q, w in zip(a, b):
-    if q != w:
-        res += 1
-
-print(res)
+    c, s = s[0], s[2:]
+    print(f"{c} {s.lower().count(c)}")
