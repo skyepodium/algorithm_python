6db7cab8f893a52b934292b0ce0aa33555b1c0c1
skyepodium <skyepodium@gmail.com>
2022-11-25 01:57:41 +0900

    boj 2307

--- algorithm.py
+++ algorithm.py
@@ -2,24 +2,29 @@ from heapq import heappop, heappush
 from collections import deque
 
 # 1. input
-n, m, p = map(int, input().split())
+n, m = map(int, input().split())
 MAX_INT = int(1e10)
-d = [MAX_INT] * (n+1)
+d = [MAX_INT for _ in range(n+1)]
+res = []
+path = [[] for _ in range(n+1)]
+used_edge = []
+check = [False] * (n+1)
+DISABLED_EDGE_IDX = 0
+MIN_DIST, MAX_DIST = MAX_INT, 0
 
 # 2. make graph
 v = [[] for _ in range(n+1)]
-path = [[] for _ in range(n+1)]
-for _ in range(m):
+for edge_idx in range(1, m+1):
     s, e, c = map(int, input().split())
-    v[s].append((e, c))
-    v[e].append((s, c))
+    v[s].append((e, c, edge_idx))
+    v[e].append((s, c, edge_idx))
 
 
-# 3. dijkstra
 def dijkstra(start_node):
     pq = []
     d[start_node] = 0
     heappush(pq, (d[start_node], start_node))
+
 
     while pq:
         cost, node = heappop(pq)
@@ -27,38 +32,47 @@ def dijkstra(start_node):
         if d[node] < cost:
             continue
 
-        for next_node, next_cost in v[node]:
+        for next_node, next_cost, next_edge_idx in v[node]:
+            if DISABLED_EDGE_IDX == next_edge_idx:
+                continue
+
             if d[next_node] > d[node] + next_cost:
-                path[next_node] = [node]
+                path[next_node] = [(node, next_edge_idx)]
                 d[next_node] = d[node] + next_cost
                 heappush(pq, (d[next_node], next_node))
             elif d[next_node] == d[node] + next_cost:
-                path[next_node].append(node)
+                path[next_node].append((node, next_edge_idx))
 
+dijkstra(1)
+MIN_DIST = d[n]
 
-start_node, end_node = 1, n
-dijkstra(start_node)
-
-check = [False] * (n+1)
-
-def bfs():
+def bfs(end_node):
     q = deque()
-    for node in path[end_node]:
-        q.append(node)
-        check[node] = True
+    check[end_node] = True
+    for next_node, next_edge_idx in path[end_node]:
+        q.append(next_node)
+        check[next_node] = True
+        used_edge.append(next_edge_idx)
 
     while q:
         node = q.popleft()
 
-        for next_node in path[node]:
+        for next_node, next_edge_idx in path[node]:
             if check[next_node]:
                 continue
             check[next_node] = True
+            used_edge.append(next_edge_idx)
             q.append(next_node)
 
-bfs()
+bfs(n)
 
-if check[p]:
-    print("SAVE HIM")
+for edge_idx in used_edge:
+    DISABLED_EDGE_IDX = edge_idx
+    d = [MAX_INT for _ in range(n + 1)]
+    dijkstra(1)
+    MAX_DIST = max(MAX_DIST, d[n])
+
+if MAX_DIST == MAX_INT:
+    print(-1)
 else:
-    print("GOOD BYE")
+    print(MAX_DIST - MIN_DIST)
