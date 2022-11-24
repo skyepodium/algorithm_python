59574d0fb8932c4607f7b1b269182a3e574b6d73
skyepodium <skyepodium@gmail.com>
2022-11-24 12:28:59 +0900

    boj 12919

--- algorithm.py
+++ algorithm.py
@@ -1,56 +1,31 @@
-from typing import List
+from collections import deque
 
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+start_node = input()
+end_node = input()
 
-        # 1. init
-        n, m = len(board), len(board[0])
-        check = [[False for _ in range(m)] for _ in range(n)]
-        start_nodes = []
-        d = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-        self.is_exist = False
+visited = set()
+n = len(end_node)
 
-        # 2. start_nodes
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == word[0]:
-                    start_nodes.append((i, j))
+def bfs(start):
+    q = deque()
+    q.append(start)
+    visited.add(start)
 
-        # 3. DFS
-        def dfs(x, y, cnt, s):
-            if cnt >= len(word):
-                if s == word:
-                    self.is_exist = True
-                return
+    while q:
+        node = q.popleft()
 
-            for dx, dy in d:
-                nx, ny = x + dx, y + dy
-                if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                    continue
-                if not check[nx][ny] and board[nx][ny] == word[cnt]:
-                    check[nx][ny] = True
-                    dfs(nx, ny, cnt + 1, s + board[nx][ny])
-                    check[nx][ny] = False
+        next_nodes = []
+        if node[-1] == "A":
+            next_nodes.append(node[:len(node)-1])
+        if node[0] == "B":
+            next_nodes.append(node[1:][::-1])
 
-        # 4. loop
-        for x, y in start_nodes:
-            check[x][y] = True
-            dfs(x, y, 1, f"{board[x][y]}")
-            if self.is_exist:
-                return True
-            check[x][y] = False
+        for next_node in next_nodes:
+            if len(next_node) <= 0: continue
+            if not next_node in visited:
+                visited.add(next_node)
+                q.append(next_node)
 
-        return False
+bfs(end_node)
 
-board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "ABCCED"
-
-board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "SEE"
-
-board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "ABCB"
-
-sl = Solution()
-res = sl.exist(board, word)
-print('res', res)
+print(1 if start_node in visited else 0)
