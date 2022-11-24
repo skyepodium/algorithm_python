9596a937aa5b122c10de8e835e01cb626a91e9bd
skyepodium <skyepodium@gmail.com>
2022-11-24 10:38:02 +0900

    leetcode - sliding-puzzle

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,58 @@
 from typing import List
-from collections import Counter
+from collections import defaultdict, deque
 
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
         # 1. init
-        cnt = Counter(nums)
-        base = len(nums) / 3
-        res = []
+        visited = defaultdict()
+        d = [(0, -1), (0, 1), (1, 0), (-1, 0)]
 
-        # 2. loop
-        for num, c in cnt.items():
-            if c > base:
-                res.append(num)
+        def bfs(start_node):
+            q = deque()
+            q.append(start_node)
+            key = str(start_node)
+            visited[key] = 0
 
-        return sorted(res)
+            while q:
+                node = q.popleft()
 
+                x, y = 0, 0
+                for i in range(2):
+                    for j in range(3):
+                        if node[i][j] == 0:
+                            x, y = i, j
+
+                for dx, dy in d:
+                    nx, ny = x + dx, y + dy
+                    if nx < 0 or nx >= 2 or ny < 0 or ny >= 3:
+                        continue
+
+                    temp = []
+                    for row in node:
+                        t = []
+                        for col in row:
+                            t.append(col)
+                        temp.append(t)
+
+                    temp[nx][ny] = node[x][y]
+                    temp[x][y] = node[nx][ny]
+                    key = str(temp)
+                    if key in visited:
+                        continue
+                    visited[key] = visited[str(node)] + 1
+                    q.append(temp)
+
+        bfs(board)
+
+        end_node = [[1, 2, 3], [4, 5, 0]]
+        key = str(end_node)
+        return visited[key] if key in visited else -1
+
+
+sl = Solution()
+board = [[1,2,3],[4,0,5]]
+board = [[1,2,3],[5,4,0]]
+board = [[4,1,2],[5,0,3]]
+res = sl.slidingPuzzle(board)
+
+print('res', res)
