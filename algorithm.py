bd6911af9b656526a50e98440b2abee8168f4354
skyepodium <skyepodium@gmail.com>
2022-11-24 20:49:17 +0900

    24389

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,18 @@
 n = int(input())
-a = input()
-b = input()
 
-if n % 2 == 1:
-    a = a.replace('1', '2').replace('0', '1').replace('2', '0')
+a = bin(n)[2:].zfill(32)
+b = a.replace('1', '2').replace('0', '1').replace('2', '0')
 
-if a == b:
-    print('Deletion succeeded')
-else:
-    print('Deletion failed')
+m = int(b, 2)
+m += 1
+
+b = bin(m)[2:]
+b = b[:32]
+
+res = 0
+
+for q, w in zip(a, b):
+    if q != w:
+        res += 1
+
+print(res)
