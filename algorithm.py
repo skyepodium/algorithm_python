078e346e18d5c863150f3a03753b8f1d786b5cf6
skyepodium <skyepodium@gmail.com>
2022-11-24 10:54:16 +0900

    leetcode two-sum-ii-input-array-is-sorted

--- algorithm.py
+++ algorithm.py
@@ -1,58 +1,19 @@
 from typing import List
-from collections import defaultdict, deque
+
 
 class Solution:
-    def slidingPuzzle(self, board: List[List[int]]) -> int:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         # 1. init
-        visited = defaultdict()
-        d = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        l, r = 0, len(numbers) - 1
 
-        def bfs(start_node):
-            q = deque()
-            q.append(start_node)
-            key = str(start_node)
-            visited[key] = 0
+        # 2. two pointer
+        while l <= r:
+            sum_val = numbers[l] + numbers[r]
+            if sum_val < target:
+                l += 1
+            elif sum_val > target:
+                r -= 1
+            else:
+                return [l + 1, r + 1]
 
-            while q:
-                node = q.popleft()
-
-                x, y = 0, 0
-                for i in range(2):
-                    for j in range(3):
-                        if node[i][j] == 0:
-                            x, y = i, j
-
-                for dx, dy in d:
-                    nx, ny = x + dx, y + dy
-                    if nx < 0 or nx >= 2 or ny < 0 or ny >= 3:
-                        continue
-
-                    temp = []
-                    for row in node:
-                        t = []
-                        for col in row:
-                            t.append(col)
-                        temp.append(t)
-
-                    temp[nx][ny] = node[x][y]
-                    temp[x][y] = node[nx][ny]
-                    key = str(temp)
-                    if key in visited:
-                        continue
-                    visited[key] = visited[str(node)] + 1
-                    q.append(temp)
-
-        bfs(board)
-
-        end_node = [[1, 2, 3], [4, 5, 0]]
-        key = str(end_node)
-        return visited[key] if key in visited else -1
-
-
-sl = Solution()
-board = [[1,2,3],[4,0,5]]
-board = [[1,2,3],[5,4,0]]
-board = [[4,1,2],[5,0,3]]
-res = sl.slidingPuzzle(board)
-
-print('res', res)
+        return [-1, -1]
