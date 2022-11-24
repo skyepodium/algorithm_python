53fedeab662a0d15379b59445fcd662a397e37e1
skyepodium <skyepodium@gmail.com>
2022-11-25 00:56:35 +0900

    boj 18223

--- algorithm.py
+++ algorithm.py
@@ -1,40 +1,64 @@
+from heapq import heappop, heappush
 from collections import deque
 
-n = int(input())
-check = [-1] * (n+1)
-path = [0] * (n+1)
+# 1. input
+n, m, p = map(int, input().split())
+MAX_INT = int(1e10)
+d = [MAX_INT] * (n+1)
 
-def bfs(start_node):
+# 2. make graph
+v = [[] for _ in range(n+1)]
+path = [[] for _ in range(n+1)]
+for _ in range(m):
+    s, e, c = map(int, input().split())
+    v[s].append((e, c))
+    v[e].append((s, c))
+
+
+# 3. dijkstra
+def dijkstra(start_node):
+    pq = []
+    d[start_node] = 0
+    heappush(pq, (d[start_node], start_node))
+
+    while pq:
+        cost, node = heappop(pq)
+
+        if d[node] < cost:
+            continue
+
+        for next_node, next_cost in v[node]:
+            if d[next_node] > d[node] + next_cost:
+                path[next_node] = [node]
+                d[next_node] = d[node] + next_cost
+                heappush(pq, (d[next_node], next_node))
+            elif d[next_node] == d[node] + next_cost:
+                path[next_node].append(node)
+
+
+start_node, end_node = 1, n
+dijkstra(start_node)
+
+check = [False] * (n+1)
+
+def bfs():
     q = deque()
-    q.append(start_node)
-    path[start_node] = start_node
-    check[start_node] = 0
+    for node in path[end_node]:
+        q.append(node)
+        check[node] = True
 
     while q:
         node = q.popleft()
 
-        next_nodes = []
-        if node % 3 == 0:
-            next_nodes.append(node // 3)
-        if node % 2 == 0:
-            next_nodes.append(node // 2)
-        if node > 2:
-            next_nodes.append(node - 1)
+        for next_node in path[node]:
+            if check[next_node]:
+                continue
+            check[next_node] = True
+            q.append(next_node)
 
-        for next_node in next_nodes:
-            if check[next_node] == -1:
-                check[next_node] = check[node] + 1
-                path[next_node] = node
-                q.append(next_node)
+bfs()
 
-bfs(n)
-
-res = []
-end_node = 1
-print(check[end_node])
-while path[end_node] != end_node:
-    res.append(end_node)
-    end_node = path[end_node]
-
-res.append(n)
-print(*res[::-1])
+if check[p]:
+    print("SAVE HIM")
+else:
+    print("GOOD BYE")
