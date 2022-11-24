70c4f5bcf820d1cd398360693ffbbc3837e29a7e
skyepodium <skyepodium@gmail.com>
2022-11-25 04:00:14 +0900

    leetcode - number-of-subarrays-with-lcm-equal-to-k

--- algorithm.py
+++ algorithm.py
@@ -1,40 +1,36 @@
-n, m = map(int, input().split())
+from typing import List
 
-parent = [x for x in range(n)]
-power = []
 
-for i in range(n):
-    power.append(int(input()))
+class Solution:
+    def subarrayLCM(self, nums: List[int], k: int) -> int:
+        # 1. init
+        n = len(nums)
+        res = 0
 
-def find(node):
-    if node == parent[node]:
-        return node
-    else:
-        parent[node] = find(parent[node])
-        return parent[node]
+        def gcd(a, b):
+            if b == 0:
+                return a
+            else:
+                return gcd(b, a%b)
 
-for _ in range(m):
-    o, p, q = map(int, input().split())
-    p -= 1
-    q -= 1
+        def lcm(a, b):
+            return (a * b) // gcd(a, b)
 
-    p, q = find(p), find(q)
-    if o == 1:
-        parent[p] = q
-        power[q] += power[p]
-        power[p] = 0
-    else:
-        if power[p] == power[q]:
-            power[p], power[q] = 0, 0
-        elif power[p] > power[q]:
-            parent[q] = p
-            power[p] -= power[q]
-            power[q] = 0
-        else:
-            parent[p] = q
-            power[q] -= power[p]
-            power[p] = 0
+        for i in range(n):
+            l = nums[i]
+            for j in range(i, n):
+                l = lcm(l, nums[j])
+                if l == k:
+                    res += 1
+                if l > k:
+                    break
 
-remain = sorted([x for x in power if x > 0])
-print(len(remain))
-print(*remain)
+        return res
+
+sl = Solution()
+nums = [3,6,2,7,1]
+k = 6
+nums = [3]
+k = 2
+res = sl.subarrayLCM(nums, k)
+print('res', res)
