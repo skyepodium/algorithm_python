c8974dcbb8ddd13cdbb81620d236d580c82c5286
skyepodium <skyepodium@gmail.com>
2022-11-26 19:41:49 +0900

    programmers - 138477

--- algorithm.py
+++ algorithm.py
@@ -1,27 +1,17 @@
-def solution(ability):
-    # 1. init
-    n = len(ability)
-    m = len(ability[0])
-    check = [False] * (n)
-    res = [0]
+from heapq import heappush, heappop
 
-    # 2. dfs
-    def dfs(idx, power):
-        if idx >= m:
-            res[0] = max(res[0], power)
-            return
 
-        for i in range(n):
-            if not check[i]:
-                check[i] = True
-                dfs(idx + 1, power + ability[i][idx])
-                check[i] = False
+def solution(k, score):
+    res = []
+    pq = []
 
-    dfs(0, 0)
+    for s in score:
+        if len(res) < k:
+            heappush(pq, s)
+        else:
+            if pq[0] < s:
+                heappop(pq)
+                heappush(pq, s)
+        res.append(pq[0])
 
-    # 3. return
-    return res[0]
-
-ability = [[40, 10, 10], [20, 5, 0], [30, 30, 30], [70, 0, 70], [100, 100, 100]]
-# ability = [[20, 30], [30, 20], [20, 30]]
-print(solution(ability))
+    return res
