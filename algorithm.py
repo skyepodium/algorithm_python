17d70fd294ca921ced625fd29292ffd0bf1d18fd
skyepodium <skyepodium@gmail.com>
2022-11-26 19:58:59 +0900

    programmers - 138476

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,27 @@
-from heapq import heappush, heappop
+from collections import Counter
 
+def solution(k, tangerine):
+    # 1. init
+    cnt_sum = 0
+    res = 0
 
-def solution(k, score):
-    res = []
-    pq = []
-
-    for s in score:
-        if len(res) < k:
-            heappush(pq, s)
-        else:
-            if pq[0] < s:
-                heappop(pq)
-                heappush(pq, s)
-        res.append(pq[0])
+    # 2. init
+    for num, cnt in sorted(Counter(tangerine).items(), key=lambda x: -x[1]):
+        cnt_sum += cnt
+        res += 1
+        if cnt_sum >= k:
+            return res
 
     return res
+
+
+k = 6
+t = [1, 3, 2, 5, 4, 5, 2, 3]
+
+k = 4
+t = [1, 3, 2, 5, 4, 5, 2, 3]
+
+k = 2
+[1, 1, 1, 1, 2, 2, 2, 3]
+
+print(solution(k, t))
