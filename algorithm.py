d43386f211a143e63866a4ce76b1a2700fbdd499
skyepodium <skyepodium@gmail.com>
2022-11-26 20:14:53 +0900

    boj 1436

--- algorithm.py
+++ algorithm.py
@@ -1,27 +1,12 @@
-from collections import Counter
+n = int(input())
 
-def solution(k, tangerine):
-    # 1. init
-    cnt_sum = 0
-    res = 0
+i = 666
+cnt = 0
+while True:
+    if "666" in str(i):
+        cnt += 1
+    if cnt >= n:
+        break
+    i += 1
 
-    # 2. init
-    for num, cnt in sorted(Counter(tangerine).items(), key=lambda x: -x[1]):
-        cnt_sum += cnt
-        res += 1
-        if cnt_sum >= k:
-            return res
-
-    return res
-
-
-k = 6
-t = [1, 3, 2, 5, 4, 5, 2, 3]
-
-k = 4
-t = [1, 3, 2, 5, 4, 5, 2, 3]
-
-k = 2
-[1, 1, 1, 1, 2, 2, 2, 3]
-
-print(solution(k, t))
+print(i)
