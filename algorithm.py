c5c2e4f9df6bcbdefc0ccf357da7c456c47e0803
skyepodium <skyepodium@gmail.com>
2022-11-27 23:56:41 +0900

    leetcode - rotate-strin

--- algorithm.py
+++ algorithm.py
@@ -1,36 +1,23 @@
-from typing import List
-from collections import defaultdict
+from collections import deque
 
 class Solution:
-    def countSubarrays(self, nums: List[int], k: int) -> int:
-        # 1. init
-        d = defaultdict(int)
-        d[0] = 1
-        res = 0
-        sum = 0
-        found = False
+    def rotateString(self, s: str, goal: str) -> bool:
+        n = len(s)
+        q = deque(list(s))
 
-        # 2. loop
-        for num in nums:
-            if num < k:
-                sum -= 1
-            elif num > k:
-                sum += 1
-            else:
-                found = True
-            if found:
-                res += d[sum] + d[sum - 1]
-            else:
-                d[sum] += 1
+        for _ in range(n):
+            if "".join(q) == goal:
+                return True
+            q.append(q.popleft())
 
-        return res
-
-nums = [3,2,1,4,5]
-k = 4
-
-nums = [2,3,1]
-k = 3
+        return False
 
 sl = Solution()
-res = sl.countSubarrays(nums, k)
+s = "abcde"
+goal = "cdeab"
+
+s = "abcde"
+goal = "abced"
+res = sl.rotateString(s, goal)
+
 print('res', res)
