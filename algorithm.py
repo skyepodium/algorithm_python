2dab3ade357e1e9e798382fc83d135820bbb9447
skyepodium <skyepodium@gmail.com>
2022-11-28 00:10:42 +0900

    leetcode - delete-columns-to-make-sorted

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,31 @@
-from collections import deque
+from typing import List
+
 
 class Solution:
-    def rotateString(self, s: str, goal: str) -> bool:
-        n = len(s)
-        q = deque(list(s))
+    def minDeletionSize(self, strs: List[str]) -> int:
+        # 1. init
+        n = len(strs)
+        m = len(strs[0])
+        res = 0
 
-        for _ in range(n):
-            if "".join(q) == goal:
-                return True
-            q.append(q.popleft())
+        # 2. loop
+        for j in range(m):
+            prev = "a"
+            is_sorted = True
+            for i in range(n):
+                if prev <= strs[i][j]:
+                    prev = strs[i][j]
+                else:
+                    is_sorted = False
+                    break
+            if not is_sorted:
+                res += 1
 
-        return False
+        return res
 
 sl = Solution()
-s = "abcde"
-goal = "cdeab"
-
-s = "abcde"
-goal = "abced"
-res = sl.rotateString(s, goal)
-
+strs = ["cba","daf","ghi"]
+strs = ["a","b"]
+strs = ["zyx","wvu","tsr"]
+res = sl.minDeletionSize(strs)
 print('res', res)
