2e935892c0fb4a8cf7c46a170ce2b474e1504187
skyepodium <skyepodium@gmail.com>
2022-12-03 23:13:27 +0900

    leetcode - decompress-run-length-encoded-list

--- algorithm.py
+++ algorithm.py
@@ -1,29 +1,12 @@
 class Solution:
-    def divisorSubstrings(self, num: int, k: int) -> int:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
         # 1. init
-        str_num = str(num)
-        n = len(str_num)
-        res = 0
+        res = []
 
         # 2. loop
-        for i in range(n-k+1):
-            s = str_num[i:i+k]
-            s_num = int(s)
-            if s_num == 0:
-                continue
-            if num % s_num == 0:
-                res += 1
+        for i in range(0, len(nums), 2):
+            freq, num = nums[i], nums[i + 1]
+            for _ in range(freq):
+                res.append(num)
 
         return res
-
-sl = Solution()
-
-num = 240
-k = 2
-
-num = 430043
-k = 2
-
-res = sl.divisorSubstrings(num, k)
-
-print('res', res)
