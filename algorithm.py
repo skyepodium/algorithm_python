8520c424e5229ea50903a8d429be97239e3ee540
skyepodium <skyepodium@gmail.com>
2022-12-08 00:53:58 +0900

    leetcode - range-sum-of-bst

--- algorithm.py
+++ algorithm.py
@@ -1,30 +1,29 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    res = 0
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         # 1. init
-        n, m = len(text1), len(text2)
-        d = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        self.res = 0
 
-        # 2. loop
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if text1[i-1] == text2[j-1]:
-                    d[i][j] = d[i-1][j-1] + 1
-                else:
-                    d[i][j] = max(d[i-1][j], d[i][j-1])
+        # 2. traverse
+        def dfs(node):
+            if not node:
+                return
 
-        return d[n][m]
+            if low <= node.val <= high:
+                self.res += node.val
 
+            dfs(node.left)
+            dfs(node.right)
 
-sl = Solution()
-text1 = "abcde"
-text2 = "ace"
+        dfs(root)
 
-text1 = "abc"
-text2 = "abc"
-
-text1 = "abc"
-text2 = "def"
-
-res = sl.longestCommonSubsequence(text1, text2)
-
-print('res', res)
+        return self.res
