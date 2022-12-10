2a703f8b373eedd23484f446afc47e194233044b
skyepodium <skyepodium@gmail.com>
2022-12-10 22:04:19 +0900

    leetcode - merge-similar-items

--- algorithm.py
+++ algorithm.py
@@ -1,37 +1,39 @@
-# Definition for a binary tree node.
-from typing import Optional
+from typing import List
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 class Solution:
-    a = []
-    b = []
-    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
         # 1. init
-        self.a = []
-        self.b = []
+        d = {}
 
-        # 2. dfs
-        def dfs(root, l):
-            if root:
-                if not root.left and not root.right:
-                    l.append(root.val)
-                dfs(root.left, l)
-                dfs(root.right, l)
+        # 2. sort
+        items1.sort()
+        items2.sort()
 
-        dfs(root1, self.a)
-        dfs(root2, self.b)
+        # 3. loop
+        for key, value in items1:
+            d[key] = value
 
-        return self.a == self.b
+        for key, value in items2:
+            if key in d:
+                d[key] += value
+            else:
+                d[key] = value
+
+        return [[key, value] for key, value in sorted(d.items())]
+
+
+items1 = [[1,1],[4,5],[3,8]]
+items2 = [[3,1],[1,5]]
+
+items1 = [[1,1],[3,2],[2,3]]
+items2 = [[2,1],[3,2],[1,3]]
+
+items1 = [[1,3],[2,2]]
+items2 = [[7,1],[2,2],[1,4]]
 
 sl = Solution()
 
-root1 = TreeNode(1)
-root2 = TreeNode(1)
+res = sl.mergeSimilarItems(items1, items2)
 
-res = sl.leafSimilar(root1, root2)
 print('res', res)
