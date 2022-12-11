2e1722f1983725df4db084233f7a403bee02af85
skyepodium <skyepodium@gmail.com>
2022-12-11 13:11:26 +0900

    leetcode - Delete Greatest Value in Each Row

--- algorithm.py
+++ algorithm.py
@@ -1,39 +1,20 @@
-from typing import List
-
-
 class Solution:
-    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
         # 1. init
-        d = {}
+        n = len(grid)
+        m = len(grid[0])
+        res = 0
 
         # 2. sort
-        items1.sort()
-        items2.sort()
+        for i in range(n):
+            grid[i].sort(reverse=True)
 
-        # 3. loop
-        for key, value in items1:
-            d[key] = value
+        # 2. find max
+        max_val = 0
+        for j in range(m):
+            max_val = 0
+            for i in range(n):
+                max_val = max(max_val, grid[i][j])
+            res += max_val
 
-        for key, value in items2:
-            if key in d:
-                d[key] += value
-            else:
-                d[key] = value
-
-        return [[key, value] for key, value in sorted(d.items())]
-
-
-items1 = [[1,1],[4,5],[3,8]]
-items2 = [[3,1],[1,5]]
-
-items1 = [[1,1],[3,2],[2,3]]
-items2 = [[2,1],[3,2],[1,3]]
-
-items1 = [[1,3],[2,2]]
-items2 = [[7,1],[2,2],[1,4]]
-
-sl = Solution()
-
-res = sl.mergeSimilarItems(items1, items2)
-
-print('res', res)
+        return res
