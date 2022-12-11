5940f626bdd2ae2358788b62c439c4fc5136e4a5
skyepodium <skyepodium@gmail.com>
2022-12-12 00:07:15 +0900

    leetcode - height-checker

--- algorithm.py
+++ algorithm.py
@@ -1,20 +1,11 @@
 class Solution:
-    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
-        # 1. init
-        n = len(grid)
-        m = len(grid[0])
-        res = 0
+  def isMatch(self, s: str, p: str):
+    if not p:
+      return not s
+    first_match = bool(s) and p[0] in {s[0], '.'}
 
-        # 2. sort
-        for i in range(n):
-            grid[i].sort(reverse=True)
-
-        # 2. find max
-        max_val = 0
-        for j in range(m):
-            max_val = 0
-            for i in range(n):
-                max_val = max(max_val, grid[i][j])
-            res += max_val
-
-        return res
+    if len(p) >= 2 and p[1] == '*':
+      return (self.isMatch(s, p[2:]) or
+              first_match and self.isMatch(s[1:], p))
+    else:
+      return first_match and self.isMatch(s[1:], p[1:])
