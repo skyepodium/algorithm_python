2b0d642b01228e4dbda7341eb4ea19fda1a16f2b
skyepodium <skyepodium@gmail.com>
2022-12-13 02:04:01 +0900

    leetcode - climbing-stairs - top-down

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # 0. exception
-        if n <= 2:
-            return n
-
         # 1. init
-        d = [0] * (n+1)
-        d[0] = 1
-        d[1] = 1
-        d[2] = 2
+        d = [0] * (n + 1)
 
-        # 2. loop
-        for i in range(2, n+1):
-            d[i] = d[i-1] + d[i-2]
+        # 2. top-down
+        def dp(i):
+            if i <= 2:
+                return i
 
-        return d[n]
+            if d[i] > 0:
+                return d[i]
+
+            d[i] = dp(i - 1) + dp(i - 2)
+            return d[i]
+
+        return dp(n)
