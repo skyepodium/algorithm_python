04c0a90336a46f88d6bbdaba04d16a08e5b68b34
skyepodium <skyepodium@gmail.com>
2022-12-13 22:32:12 +0900

    leetcode -  minimum-falling-path-sum

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,33 @@
+from typing import List
+
+
 class Solution:
-    def climbStairs(self, n: int) -> int:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         # 1. init
-        d = [0] * (n + 1)
+        n, m = len(matrix), len(matrix[0])
+        MAX_VAL = int(1e10)
+        d = [[MAX_VAL for _ in range(m)] for _ in range(n)]
 
-        # 2. top-down
-        def dp(i):
-            if i <= 2:
-                return i
+        # 2. bottom-up
+        for i in range(n):
+            for j in range(m):
+                # 1) exception
+                if i == 0:
+                    d[i][j] = matrix[i][j]
+                    continue
 
-            if d[i] > 0:
-                return d[i]
+                # 2) min update
+                c = d[i-1][j]
+                l = d[i-1][j-1] if j-1 >= 0 else MAX_VAL
+                r = d[i-1][j+1] if j+1 < m else MAX_VAL
+                d[i][j] = matrix[i][j] + min(l, c, r)
 
-            d[i] = dp(i - 1) + dp(i - 2)
-            return d[i]
+        return min(d[n-1])
 
-        return dp(n)
+
+sl = Solution()
+matrix = [[2,1,3],[6,5,4],[7,8,9]]
+matrix = [[-19,57],[-40,-5]]
+res = sl.minFallingPathSum(matrix)
+
+print('res', res)
