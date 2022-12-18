52c1e89645451ead9b2f0fde6337c54a5b756425
skyepodium <skyepodium@gmail.com>
2022-12-18 19:09:08 +0900

    leetcode - nim-game

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,3 @@
-from typing import List
-
-
 class Solution:
-    def isPossible(self, n: int, edges: List[List[int]]) -> bool:
-        # 1. init
-        d = [0] * (n+1)
-
-        # 2. count indegree
-        for s, e in edges:
-            d[s] += 1
-            d[e] += 1
-
-        # 3. sort
+    def canWinNim(self, n: int) -> bool:
+        return n % 4 != 0
