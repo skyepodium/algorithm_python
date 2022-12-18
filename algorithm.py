e744105df31770e75555a2361b1dc00bb4bfde96
skyepodium <skyepodium@gmail.com>
2022-12-18 17:18:07 +0900

    leetcode - Smallest Value After Replacing With Sum of Prime Factors

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,14 @@
-def gcd(a, b):
-    return a if b == 0 else gcd(b, a % b)
+from typing import List
+
+
+class Solution:
+    def isPossible(self, n: int, edges: List[List[int]]) -> bool:
+        # 1. init
+        d = [0] * (n+1)
+
+        # 2. count indegree
+        for s, e in edges:
+            d[s] += 1
+            d[e] += 1
+
+        # 3. sort
