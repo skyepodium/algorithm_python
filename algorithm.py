fc9de33cbebe4087b5df6deb3741f074cafb6d13
skyepodium <skyepodium@gmail.com>
2022-12-19 20:18:24 +0900

    leetcode - find-if-path-exists-in-graph

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,24 @@
+from typing import List
+
+
 class Solution:
-    def canWinNim(self, n: int) -> bool:
-        return n % 4 != 0
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # 1. init
+        check = [False] * (n+1)
+        v = [[] for _ in range(n+1)]
+
+        # 2. make graph
+        for s, e in edges:
+            v[s].append(e)
+            v[e].append(s)
+
+        # 3. dfs
+        def dfs(node):
+            check[node] = True
+            for next_node in v[node]:
+                if not check[next_node]:
+                    dfs(next_node)
+
+        dfs(source)
+
+        return check[destination]
