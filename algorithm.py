4cddf69157cd4f0d497b6ed346944baf67ea92b5
skyepodium <skyepodium@gmail.com>
2022-12-21 22:10:52 +0900

    leetcode - minimum-distance-to-the-target-element

--- algorithm.py
+++ algorithm.py
@@ -2,31 +2,13 @@ from typing import List
 
 
 class Solution:
-    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
         # 1. init
-        n = len(rooms)
-        v = [[] for _ in range(n+1)]
-        check = [False] * (n+1)
+        res = len(nums)
 
-        # 2. build graph
-        for room, keys in enumerate(rooms):
-            for key in keys:
-                v[room].append(key)
+        # 2. loop
+        for i, num in enumerate(nums):
+            if num == target:
+                res = min(res, abs(i - start))
 
-        # 3. dfs
-        def dfs(node):
-            check[node] = True
-            for n_node in v[node]:
-                if not check[n_node]:
-                    dfs(n_node)
-
-        dfs(0)
-
-        return sum(check) == n
-
-rooms = [[1],[2],[3],[]]
-rooms = [[1,3],[3,0,1],[2],[0]]
-sl = Solution()
-res = sl.canVisitAllRooms(rooms)
-
-print('res', res)
+        return res
