bed12e5437a1d9ea088a0ad7287ff4f9518a0d04
skyepodium <skyepodium@gmail.com>
2022-12-28 23:59:06 +0900

    leetcode - wo-sum

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,16 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 1. init
+        d = {}
+        res = []
+
+        # 2. loop
+        for idx, num in enumerate(nums):
+            remain = target - num
+
+            if remain in d:
+                return [d[remain], idx]
+
+            d[num] = idx
+
+        return res
