fef72a5bee53a69836e5b24738aaa6a1e19cc2bd
skyepodium <skyepodium@gmail.com>
2023-01-02 01:17:46 +0900

    leetcode - word-pattern

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,37 @@
 class Solution:
-    def distinctPrimeFactors(self, nums: List[int]) -> int:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        # 0. exception
+        if len(pattern) != len(s.split(" ")):
+            return False
+
+        # 1. init
+        d = {}
+        w = {}
+
+        # 2. loop
+        for p, c in zip(pattern, s.split(" ")):
+            if p in d and d[p] != c:
+                return False
+
+            if c in w and w[c] != p:
+                return False
+
+            d[p] = c
+            w[c] = p
+
+        return True
+
+sl = Solution()
+pattern = "abba"
+s = "dog cat cat dog"
+
+
+pattern = "abba"
+s = "dog dog dog dog"
+
+pattern = "aaa"
+s = "aa aa aa aa"
+
+res = sl.wordPattern(pattern, s)
+
+print('res', res)
