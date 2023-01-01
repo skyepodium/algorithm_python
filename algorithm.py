40096f27578a35b18f72f5c1b9576fb72e0e59b4
skyepodium <skyepodium@gmail.com>
2023-01-01 20:11:00 +0900

    leetcode - Count the Digits That Divide a Number

--- algorithm.py
+++ algorithm.py
@@ -1,20 +1,2 @@
-from typing import List
-  
-
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        # 1. init
-        n = len(nums)
-
-        # 2. binary search
-        l, r = 0, n - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
-                r = mid - 1
-
-        return -1
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
