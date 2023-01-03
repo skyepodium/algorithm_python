4dbc000418dc8b018b88f82aa908356e4ec01579
skyepodium <skyepodium@gmail.com>
2023-01-03 23:57:57 +0900

    gcd

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,5 @@
-class Solution:
-    def detectCapitalUse(self, word: str) -> bool:
-        if word == word.lower():
-            return True
-
-        if word == word.upper():
-            return True
-
-        if word[0] == word[0].upper() and word[1:] == word[1:].lower():
-            return True
-
-        return False
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
