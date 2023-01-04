6bd013cc05c03c714e1862fc832e5f314906b3c3
skyepodium <skyepodium@gmail.com>
2023-01-04 23:38:59 +0900

    leetcode - binary-tree-preorder-traversal

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,21 @@
-def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a%b)
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # 1. init
+        res = []
+
+        # 2. exception
+        if not root:
+            return res
+
+        # 3. pre_order
+        def pre_order(node):
+            if not node:
+                return
+
+            res.append(node.val)
+            pre_order(node.left)
+            pre_order(node.right)
+
+        pre_order(root)
+
+        return res
