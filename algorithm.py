ff883fcf460587fbe58710be53b817397e5bec62
skyepodium <skyepodium@gmail.com>
2023-01-06 23:13:52 +0900

    leetcode - count-negative-numbers-in-a-sorted-matrix

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,13 @@
+from typing import List
+
+
 class Solution:
-    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        # 1. init
-        d = {
-            "type": 0,
-            "color": 1,
-            "name": 2
-        }
+    def countNegatives(self, grid: List[List[int]]) -> int:
         res = 0
 
-        # 2. loop
-        for i in items:
-            if i[d[ruleKey]] == ruleValue:
-                res += 1
+        for g in grid:
+            for num in g:
+                if num < 0:
+                    res += 1
 
         return res
