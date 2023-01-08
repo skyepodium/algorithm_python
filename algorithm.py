b3950d2c6f3c77648d65d1efe3d6337d3712aec1
skyepodium <skyepodium@gmail.com>
2023-01-08 22:20:42 +0900

    leetcode - most-common-word

--- algorithm.py
+++ algorithm.py
@@ -1,15 +1,24 @@
 from typing import List
-
+from collections import Counter
+import re
 
 class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         # 1. init
-        res = 0
+        banned_set = set(banned)
+        paragraph = re.sub('[^a-zA-z ]', ' ', paragraph.lower())
 
         # 2. loop
-        for g in grid:
-            for num in g:
-                if num < 0:
-                    res += 1
+        c = Counter([p for p in paragraph.split() if p not in banned_set])
+        return c.most_common()[0][0]
 
-        return res
+
+sl = Solution()
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+
+paragraph = "a, a, a, a, b,b,b,c, c"
+banned = ["a"]
+res = sl.mostCommonWord(paragraph, banned)
+
+print('res', res)
