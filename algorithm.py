03fa5ad677b271e0e755628f932f54a367a0323d
skyepodium <skyepodium@gmail.com>
2023-01-09 20:48:09 +0900

    leetcode - fair-candy-swap

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,35 @@
+from typing import List
+
+
 class Solution:
-    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
         # 1. init
-        banned_set = set(banned)
-        paragraph = re.sub('[^a-zA-z ]', ' ', paragraph.lower())
+        sum_alice = sum(aliceSizes)
+        sum_bob = sum(bobSizes)
+        half_size = (sum_alice + sum_bob) // 2
+        s = set(aliceSizes)
 
         # 2. loop
-        c = Counter([p for p in paragraph.split() if p not in banned_set])
-        return c.most_common()[0][0]
+        for b in bobSizes:
+            remain = half_size - (sum_bob - b)
+            if remain in s:
+                return [remain, b]
+
+        return [-1, -1]
+
+
+sl = Solution()
+aliceSizes = [1,1]
+bobSizes = [2,2]
+
+aliceSizes = [1,2]
+bobSizes = [2,3]
+
+aliceSizes = [2]
+bobSizes = [1,3]
+
+aliceSizes = [2, 2]
+bobSizes = [1, 1]
+res = sl.fairCandySwap(aliceSizes, bobSizes)
+
+print('res', res)
