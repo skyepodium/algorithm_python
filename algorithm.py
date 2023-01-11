8e03734c9407880658f5e6a8306520927130fbda
skyepodium <skyepodium@gmail.com>
2023-01-11 21:36:38 +0900

    leetcode - substrings-of-size-three-with-distinct-characters

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,18 @@
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p and not q:
-            return False
+    def countGoodSubstrings(self, s: str) -> int:
+        # 1. init
+        res = 0
 
-        if not p and q:
-            return False
+        # 2. loop
+        for i in range(len(s) - 2):
+            if len(set(s[i:i+3])) >= 3:
+                res += 1
 
-        if not p and not q:
-            return True
+        return res
 
-        if p.val != q.val:
-            return False
+sl = Solution()
+s = "xyzzaz"
+s = "aababcabc"
+res = sl.countGoodSubstrings(s)
 
-        l = self.isSameTree(p.left, q.left)
-        r = self.isSameTree(p.right, q.right)
-
-        return l and r
+print('res', res)
