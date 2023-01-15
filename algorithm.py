e2f522586d89da007a227c752953a62bab3b4123
skyepodium <skyepodium@gmail.com>
2023-01-15 20:15:20 +0900

    leetcode - find-subarrays-with-equal-sum

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,18 @@
+from typing import List
+
+
 class Solution:
-    def truncateSentence(self, s: str, k: int) -> str:
-        return " ".join(s.split()[:k])
+    def findSubarrays(self, nums: List[int]) -> bool:
+        # 1. init
+        s = set()
+
+        # 2. loop
+        for i in range(len(nums) - 1):
+            val = nums[i] + nums[i+1]
+            if val in s:
+                return True
+
+            s.add(val)
+
+        return False
+
