8b11da4cc6a4ffdbd13788973327e0ad8beaab00
skyepodium <skyepodium@gmail.com>
2023-01-17 21:58:38 +0900

    leetcode - find-closest-number-to-zero

--- algorithm.py
+++ algorithm.py
@@ -2,5 +2,19 @@ from typing import List
 
 
 class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        return len([(a, b) for a, b in zip(heights, sorted(heights)) if a != b])
+    def findClosestNumber(self, nums: List[int]) -> int:
+        # 1. init
+        res = -float(1e6)
+        min_dist = float(1e6)
+
+        # 2. loop
+        for num in nums:
+            abs_num = abs(num)
+            if abs_num < min_dist:
+                min_dist = abs_num
+                res = num
+            elif abs_num == min_dist:
+                if num > res:
+                    res = num
+
+        return res
