4a12132e4c496128600157fe1a3a135f78d73207
skyepodium <skyepodium@gmail.com>
2023-01-20 22:33:44 +0900

    leetcode - single-number

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,7 @@
+from functools import reduce
+from typing import List
+
+
 class Solution:
-    def findClosestNumber(self, nums: List[int]) -> int:
-        # 1. init
-        res = -float(1e6)
-        min_dist = float(1e6)
-
-        # 2. loop
-        for num in nums:
-            abs_num = abs(num)
-            if abs_num < min_dist:
-                min_dist = abs_num
-                res = num
-            elif abs_num == min_dist:
-                if num > res:
-                    res = num
-
-        return res
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda a, b: a ^ b, nums)
