54ee88f9b0d54071fb9f805b8b4f5e2934d21754
skyepodium <skyepodium@gmail.com>
2023-01-23 18:55:36 +0900

    atcode -Shift

--- algorithm.py
+++ algorithm.py
@@ -1,40 +1,11 @@
-import re
+from collections import deque
 
-class Solution:
-    def strongPasswordCheckerII(self, password: str) -> bool:
-        # 1. over 8 characters
-        if len(password) < 8:
-            return False
+n, m = map(int, input().split())
+l = list(map(int, input().split()))
+q = deque(l)
 
-        # 2. one lowercase letter
-        if password.upper() == password:
-            return False
+for _ in range(m):
+    q.popleft()
+    q.append(0)
 
-        # 3. one uppercase letter
-        if password.lower() == password:
-            return False
-
-        # 4. one digit
-        if len(re.sub("[0-9]", '', password)) == len(password):
-            return False
-
-        # 5. one special character
-        if len(re.sub("[!@#$%^&*()\\-+]", '', password)) == len(password):
-            return False
-
-        # 6. same character
-        for i in range(len(password) - 1):
-            if password[i] == password[i+1]:
-                return False
-
-        return True
-
-sl = Solution()
-password = "IloveLe3tcode!"
-password = "Me+You--IsMyDream"
-password = "1aB!"
-password = "aabab12!Aab"
-password = "-Aa1a1a1"
-res = sl.strongPasswordCheckerII(password)
-
-print('res', res)
+print(*list(q))
