a44977f0339e1579f8c83d2b663b943c97966d94
skyepodium <skyepodium@gmail.com>
2023-01-24 21:28:49 +0900

    leetcode - next-greater-element-i

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,26 @@
-from collections import deque
+from typing import List
 
-n, m = map(int, input().split())
-l = list(map(int, input().split()))
-q = deque(l)
 
-for _ in range(m):
-    q.popleft()
-    q.append(0)
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # 1. init
+        n = len(nums2)
+        next_big = [-1] * n
+        index_dic = {}
+        res = []
 
-print(*list(q))
+        # 2. loop
+        for i in range(n):
+            cur = nums2[i]
+            index_dic[cur] = i
+            for j in range(i+1, n):
+                next_nums = nums2[j]
+                if next_nums > cur:
+                    if next_big[i] == -1:
+                        next_big[i] = next_nums
+
+        # 3. search
+        for num in nums1:
+            res.append(next_big[index_dic[num]])
+
+        return res
