d1151d89fddce9f73971197160ab2029ee368f67
skyepodium <skyepodium@gmail.com>
2023-01-28 21:54:29 +0900

    gcd

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,5 @@
-class Solution:
-    def permute(self, nums: list[int]) -> list[list[int]]:
-
-        result = []
-        size = len(nums)
-        check = [False for _ in range(21)]
-
-        def go(idx, stack):
-            if idx >= size:
-                result.append(list(stack))
-                return
-
-            for num in nums:
-                if not check[num+10]:
-                    check[num+10] = True
-                    stack.append(num)
-
-                    go(idx + 1, stack)
-
-                    check[num+10] = False
-                    stack.pop()
-
-        go(0, [])
-
-        return result
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
