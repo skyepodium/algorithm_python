ea55317888b87d0e545fef7c0b44a81ee72deccf
skyepodium <skyepodium@gmail.com>
2023-01-29 21:02:10 +0900

    binary search

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,12 @@
-def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a%b)
+def binary_search(array, target):
+    left = 0
+    right = len(array) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if array[middle] == target:
+            return middle
+        elif array[middle] < target:
+            left = middle + 1
+        else:
+            right = middle - 1
+    return -1
