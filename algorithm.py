8ba7fcc2a923b914de89beed91269bc10c133445
skyepodium <skyepodium@gmail.com>
2023-01-30 00:26:24 +0900

    binary search recursive

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,10 @@
-def binary_search(array, target):
-    left = 0
-    right = len(array) - 1
-    while left <= right:
-        middle = (left + right) // 2
-        if array[middle] == target:
-            return middle
-        elif array[middle] < target:
-            left = middle + 1
-        else:
-            right = middle - 1
-    return -1
+def binary_search_recursive(array, target, start, end):
+    if start > end:
+        return False
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return True
+    elif array[mid] > target:
+        return binary_search_recursive(array, target, start, mid - 1)
+    else:
+        return binary_search_recursive(array, target, mid + 1, end)
