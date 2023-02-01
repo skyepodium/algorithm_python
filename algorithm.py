cf766590f11d6b14464cd88b323de6057eb348e5
skyepodium <skyepodium@gmail.com>
2023-02-01 22:15:22 +0900

    gcd

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,7 @@
-def dijkstra(graph, start, end):
-    # initialize
-    dist = {start: 0}
-    prev = {}
-    queue = []
-    for node in graph:
-        if node != start:
-            dist[node] = float('inf')
-        heapq.heappush(queue, (dist[node], node))
-    # loop
-    while queue:
-        u = heapq.heappop(queue)[1]
-        if u == end:
-            break
-        for v in graph[u]:
-            alt = dist[u] + graph[u][v]
-            if alt < dist[v]:
-                dist[v] = alt
-                prev[v] = u
-    # return
-    return dist, prev
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+    
