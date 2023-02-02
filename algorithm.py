18ccee35c57a8c60f0fa024ce66169f6c2f3db2b
skyepodium <skyepodium@gmail.com>
2023-02-02 23:08:02 +0900

    leetcode -combination-sum

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,21 @@
-def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a % b)
+class Solution:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
 
-    
+        result = []
+
+        size = len(candidates)
+
+        def go(idx, sumVal, stack):
+            if sumVal >= target:
+                if sumVal == target:
+                    result.append(stack[:])
+                return
+
+            for i in range(idx, size):
+                stack.append(candidates[i])
+                go(i, sumVal + candidates[i], stack)
+                stack.pop()
+
+        go(0, 0, [])
+
+        return result
