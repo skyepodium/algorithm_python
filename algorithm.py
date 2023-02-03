9193a373fb0225889116e85faf77e1618d6e9ede
skyepodium <skyepodium@gmail.com>
2023-02-03 22:59:36 +0900

    leetcode - word-search

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,41 @@
 class Solution:
-    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+    def exist(self, board: List[List[str]], word: str) -> bool:
 
-        result = []
+        # 1. init
+        n, m = len(board), len(board[0])
+        check = [[False for _ in range(m)] for _ in range(n)]
+        start_nodes = []
+        d = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        self.is_exist = False
 
-        size = len(candidates)
+        # 2. start_nodes
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == word[0]:
+                    start_nodes.append((i, j))
 
-        def go(idx, sumVal, stack):
-            if sumVal >= target:
-                if sumVal == target:
-                    result.append(stack[:])
+        # 3. DFS
+        def dfs(x, y, cnt, s):
+            if cnt >= len(word):
+                if s == word:
+                    self.is_exist = True
                 return
 
-            for i in range(idx, size):
-                stack.append(candidates[i])
-                go(i, sumVal + candidates[i], stack)
-                stack.pop()
+            for dx, dy in d:
+                nx, ny = x + dx, y + dy
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if not check[nx][ny] and board[nx][ny] == word[cnt]:
+                    check[nx][ny] = True
+                    dfs(nx, ny, cnt + 1, s + board[nx][ny])
+                    check[nx][ny] = False
 
-        go(0, 0, [])
+        # 4. loop
+        for x, y in start_nodes:
+            check[x][y] = True
+            dfs(x, y, 1, f"{board[x][y]}")
+            if self.is_exist:
+                return True
+            check[x][y] = False
 
-        return result
+        return False
