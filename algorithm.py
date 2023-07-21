162faa054838ca0378a213d90acca43cdee359c2
skyepodium <skyepodium@gmail.com>
2023-07-21 23:59:03 +0900

    [leet code] contains-duplicate
    [leet code] contains-duplicate

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,6 @@
-fruits = [
-    {
-        'id': 1,
-        'name': 'banana',
-    },
-    {
-        'id': 2,
-        'name': 'apple',
-    },
-    {
-        'id': 3,
-        'name': 'orange',
-    }
-]
+from typing import List
 
-selected_fruits = []
 
-for fruit in fruits:
-    if (fruit['name'] == 'banana') == 0:
-        selected_fruits.append(fruit)
-
-print(selected_fruits)
-# name이 banana인 row 제외하고 조회
-# [{'id': 2, 'name': 'apple'}, {'id': 3, 'name': 'orange'}]
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
