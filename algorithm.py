16a4c851e083e7d8e24d63baa13fa34d26c71315
skyepodium <skyepodium@gmail.com>
2023-07-22 23:08:52 +0900

    [leetcode] maximum-depth-of-binary-tree
    [leetcode] maximum-depth-of-binary-tree

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,27 @@
-from typing import List
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+    def __init__(self):
+        self.max = 0
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, depth):
+            self.max = max(self.max, depth)
+            if not node:
+                return depth
+
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+
+        dfs(root, 0)
+
+        return self.max
