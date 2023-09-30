53f3387c807e07f324ff7b5ea693585a460f04b7
skyepodium <skyepodium@gmail.com>
2023-10-01 01:35:10 +0900

    leetcode - toeplitz-matrix

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,33 @@
-from collections import Counter
 from typing import List
 
 
 class Solution:
-    def findLucky(self, arr: List[int]) -> int:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         # 1. init
-        c = Counter(arr)
+        n, m = len(matrix), len(matrix[0])
 
-        # 2. find lucky
-        lucky = -1
-        for k, v in c.items():
-            if k == v:
-                lucky = max(lucky, k)
+        def check(x, y):
+            val = matrix[x][y]
+            while x < n and y < m:
+                if matrix[x][y] != val:
+                    return False
+                x += 1
+                y += 1
 
-        return lucky
+            return True
+
+        # 2. check
+        for i in range(n):
+            if not check(i, 0):
+                return False
+
+        for j in range(m):
+            if not check(0, j):
+                return False
+
+        return True
+
+
+sl = Solution()
+matrix = [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]
+print(sl.isToeplitzMatrix(matrix))
