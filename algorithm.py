fbef8031c6e03e1bcad9c02dac7301b1282434d1
skyepodium <skyepodium@gmail.com>
2023-10-02 12:46:55 +0900

    leetcode - reverse-only-letters

--- algorithm.py
+++ algorithm.py
@@ -1,20 +1,26 @@
-from typing import List
-
-
 class Solution:
-    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+    def reverseOnlyLetters(self, s: str) -> str:
         # 1. init
-        c = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
-             ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
-        l = []
+        i, j = 0, len(s) - 1
+        l = list(s)
 
-        for word in words:
-            code = ""
-            for w in word:
-                code += c[ord(w) - 97]
+        # 2. loop
+        while i < j:
+            if not l[i].isalpha():
+                i += 1
+            elif not l[j].isalpha():
+                j -= 1
+            else:
+                l[i], l[j] = l[j], l[i]
+                i += 1
+                j -= 1
 
-            if code not in l:
-                l.append(code)
+        # 3. return
+        return ''.join(l)
 
-        return len(l)
 
+sl = Solution()
+s = "ab-cd"
+s = "a-bC-dEf-ghIj"
+s = "Test1ng-Leet=code-Q!"
+print(sl.reverseOnlyLetters(s))
