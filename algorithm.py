75025fb96446326ee01c9a4f609d80e25936da4d
skyepodium <skyepodium@gmail.com>
2023-10-06 01:34:10 +0900

    programmer - sub

--- algorithm.py
+++ algorithm.py
@@ -1,20 +1,2 @@
-from typing import List
-
-
-class Solution:
-    def stringMatching(self, words: List[str]) -> List[str]:
-        # 1. init
-        res = []
-        n = len(words)
-
-        # 2. loop
-        for i in range(n):
-            for j in range(n):
-                if i == j:
-                    continue
-
-                if words[i] in words[j]:
-                    res.append(words[i])
-                    break
-
-        return res
+def solution(num1, num2):
+    return num1 - num2
