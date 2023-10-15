7be242f4ba26d9b00edcc2325f96b349e8d2abbc
skyepodium <skyepodium@gmail.com>
2023-10-15 15:31:02 +0900

    programmers - 첫 번째로 나오는 음수

--- algorithm.py
+++ algorithm.py
@@ -1,30 +1,8 @@
-# Definition for a binary tree node.
-from typing import Optional
+def solution(num_list):
+    result = -1
 
+    for idx, num in enumerate(num_list):
+        if num < 0:
+            return idx
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Solution:
-    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        # 1. init
-        self.res = 0
-
-        # 2. dfs
-        def dfs(node: TreeNode, path_sum: str):
-            if not node:
-                return
-
-            if not node.left and not node.right:
-                self.res += int(f"{path_sum}{node.val}", 2)
-
-            dfs(node.left, f"{path_sum}{node.val}")
-            dfs(node.right, f"{path_sum}{node.val}")
-
-        dfs(root, "")
-
-        return self.res
+    return result
