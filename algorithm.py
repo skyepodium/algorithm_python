1d8f69c14fbce0fb27bdec264ec41e48182965ff
skyepodium <skyepodium@gmail.com>
2023-11-16 23:40:13 +0900

    leetcode - unique-email-addresses

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,28 @@
-def solution(num_list):
-    result = -1
+from typing import List
 
-    for idx, num in enumerate(num_list):
-        if num < 0:
-            return idx
 
-    return result
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        # 1. init
+        unique_emails = set()
+
+        # 2. loop
+        for email in emails:
+            local, domain = email.split("@")
+
+            if "+" in local:
+                local = local[:local.index("+")]
+
+            local = local.replace(".", "")
+
+            email_result = f"{local}@{domain}"
+            if email_result not in unique_emails:
+                unique_emails.add(email_result)
+
+        return len(unique_emails)
+
+
+emails = ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]
+emails = ["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"]
+sl = Solution()
+print(sl.numUniqueEmails(emails))
