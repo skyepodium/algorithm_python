f6476ad1f6f21ef00593daa2450166f640a4ac7b
skyepodium <skyepodium@gmail.com>
2023-11-17 23:22:13 +0900

    leetcode - remove-outermost-parentheses

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,13 @@
-from typing import List
-
-
 class Solution:
-    def numUniqueEmails(self, emails: List[str]) -> int:
-        # 1. init
-        unique_emails = set()
-
-        # 2. loop
-        for email in emails:
-            local, domain = email.split("@")
-
-            if "+" in local:
-                local = local[:local.index("+")]
-
-            local = local.replace(".", "")
-
-            email_result = f"{local}@{domain}"
-            if email_result not in unique_emails:
-                unique_emails.add(email_result)
-
-        return len(unique_emails)
-
-
-emails = ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]
-emails = ["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"]
-sl = Solution()
-print(sl.numUniqueEmails(emails))
+    def removeOuterParentheses(self, s: str) -> str:
+        stack = []
+        result = ""
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                if len(stack) == 1:
+                    result += s[stack.pop() + 1:i]
+                else:
+                    stack.pop()
+        return result
