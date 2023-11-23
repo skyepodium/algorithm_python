72aebe96bfc948c363754a5f16ce3c40d42d74ef
skyepodium <skyepodium@gmail.com>
2023-11-23 23:53:22 +0900

    leetcode - single number

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,3 @@
 class Solution:
-    def removeOuterParentheses(self, s: str) -> str:
-        stack = []
-        result = ""
-        for i in range(len(s)):
-            if s[i] == "(":
-                stack.append(i)
-            else:
-                if len(stack) == 1:
-                    result += s[stack.pop() + 1:i]
-                else:
-                    stack.pop()
-        return result
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda a, b: a ^ b, nums)
