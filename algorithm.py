a9224d700dd0b6baa3601677eb709cfeffff9b8d
skyepodium <skyepodium@gmail.com>
2023-12-04 22:23:19 +0900

    url encoding

--- algorithm.py
+++ algorithm.py
@@ -1,31 +1,17 @@
-from typing import List
+url = "shopping.naver.com/luxury/home"
 
 
-class Solution:
-    def findRelativeRanks(self, score: List[int]) -> List[str]:
-        # 1. init
-        medals = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
+def encode_url(url: str) -> str:
+    res = ""
 
-        # 2. sort
-        sorted_score = sorted(score, reverse=True)
+    for c in url:
+        if c in ["/", ":"]:
+            res += c
+        else:
+            res += f"%{hex(ord(c))[2:]}"
 
-        # 3. rank
-        rank = {}
-        for i, s in enumerate(sorted_score):
-            if i < 3:
-                rank[s] = medals[i]
-            else:
-                rank[s] = str(i + 1)
-
-        # 4. return
-        return [rank[s] for s in score]
+    return res
 
 
-sl = Solution()
-
-score = [5, 4, 3, 2, 1]
-score = [10, 3, 8, 9, 4]
-
-res = sl.findRelativeRanks(score)
-
-print('res', res)
+res = "https://" + encode_url(url)
+print(res)
