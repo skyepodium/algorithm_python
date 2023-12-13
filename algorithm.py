14625e4e78cbec7519eef4c2e6ee2cceaf8e9dd1
skyepodium <skyepodium@gmail.com>
2023-12-13 22:52:48 +0900

    leetcode - sorting-the-sentence

--- algorithm.py
+++ algorithm.py
@@ -1,31 +1,10 @@
 class Solution:
-    def interpret(self, command: str) -> str:
-        # 1. init
-        s = []
-
-        # 2. loop
-        for c in command:
-            if c == "(":
-                s.append(c)
-            elif c == ")":
-                if s[-1] == "(":
-                    s.pop()
-                    s.append("o")
-            else:
-                if len(s) < 1:
-                    s.append(c)
-                elif s[-1] == "(":
-                    s.pop()
-                    s.append(c)
-                else:
-                    s.append(c)
-
-        # 3. return
-        return "".join(s)
+    def sortSentence(self, s: str) -> str:
+        s = s.split()
+        s.sort(key=lambda x: x[-1])
+        return ' '.join([i[:-1] for i in s])
 
 
-command = "G()(al)"
-command = "G()()()()(al)"
-command = "(al)G(al)()()G"
 sl = Solution()
-print(sl.interpret(command))
+s = "is2 sentence4 This1 a3"
+print(sl.sortSentence(s))
