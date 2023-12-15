1f5ad277236f1e1580a56bdc48f2decf9cc19f42
skyepodium <skyepodium@gmail.com>
2023-12-15 23:13:59 +0900

    leetcode - longest-common-prefix

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,13 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        # roman to integer
-        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        # result
-        result = 0
-        # loop through the string
-        for i in range(len(s)):
-            # if the current string is less than the next string, then it is a subtraction
-            if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i + 1]]:
-                result -= roman_dict[s[i]]
-            else:
-                result += roman_dict[s[i]]
-        return result
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+        for i in range(len(first)):
+            if first[i] != last[i]:
+                return first[:i]
+        return first
